30b73d02ea40511bbe7f45c0a143225a2ef5c80c
Jason Miller <jason@developit.ca>
2015-09-14 07:44:01 -0400

    - Add basic export sanity tests and some preliminary tests for `h()`.

---
+++ test/shared/exports.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { default as preact, h, render, Component, hooks, options } from '../../preact';
+
+describe('preact', () => {
+	it('should be available as a default export', () => {
+		expect(preact).to.be.an('object');
+		expect(preact).to.have.property('h', h);
+		expect(preact).to.have.property('render', render);
+		expect(preact).to.have.property('Component', Component);
+		expect(preact).to.have.property('hooks', hooks);
+		expect(preact).to.have.property('options', options);
+	});
+
+	it('should be available as named exports', () => {
+		expect(h).to.be.a('function');
+		expect(render).to.be.a('function');
+		expect(Component).to.be.a('function');
+		expect(hooks).to.exist.and.be.an('object');
+		expect(options).to.exist.and.be.an('object');
+	});
+});
---
+++ test/shared/h.js
@@ -0,0 +1,105 @@
+import { expect } from 'chai';
+import { h, VNode } from '../../preact';
+
+/** @jsx h */
+
+let flatten = obj => JSON.parse(JSON.stringify(obj));
+
+describe('h()', () => {
+	it('should return a VNode', () => {
+		var r;
+		expect( () => r = h('foo') ).not.to.throw();
+		expect(r).to.be.an('object');
+		expect(r).to.be.an.instanceof(VNode);
+		expect(r).to.have.property('nodeName', 'foo');
+		expect(r).to.have.property('attributes', undefined);
+		expect(r).to.have.property('children', undefined);
+	});
+
+	it('should perserve raw attributes', () => {
+		let attrs = { foo:'bar', baz:10, func:()=>{} },
+			r = h('foo', attrs);
+		expect(r).to.be.an('object')
+			.with.property('attributes')
+			.that.deep.equals(attrs);
+	});
+
+	it('should support element children', () => {
+		let r = h(
+			'foo',
+			null,
+			h('bar'),
+			h('baz')
+		);
+
+		expect(r).to.be.an('object')
+			.with.property('children')
+			.that.deep.equals([
+				new VNode('bar'),
+				new VNode('baz')
+			]);
+	});
+
+	it('should support element children', () => {
+		let r = h(
+			'foo',
+			null,
+			h('bar'),
+			h('baz', null, h('test'))
+		);
+
+		r = flatten(r);
+
+		expect(r).to.be.an('object')
+			.with.property('children')
+			.that.deep.equals([
+				{ nodeName:'bar' },
+				{ nodeName:'baz', children:[
+					{ nodeName:'test' }
+				]}
+			]);
+	});
+
+	it('should support text children', () => {
+		let r = h(
+			'foo',
+			null,
+			'textstuff'
+		);
+
+		expect(r).to.be.an('object')
+			.with.property('children')
+			.with.length(1)
+			.with.property('0')
+			.that.equals('textstuff');
+	});
+
+	it('should merge adjacent text children', () => {
+		let r = h(
+			'foo',
+			null,
+			'one',
+			'two',
+			h('bar'),
+			'three',
+			h('baz'),
+			h('baz'),
+			'four',
+			'five',
+			'six'
+		);
+
+		r = flatten(r);
+
+		expect(r).to.be.an('object')
+			.with.property('children')
+			.that.deep.equals([
+				'onetwo',
+				{ nodeName:'bar' },
+				'three',
+				{ nodeName:'baz' },
+				{ nodeName:'baz' },
+				'fourfivesix'
+			]);
+	});
+});
