bca59b7e027c046214642b68e29bf13807ab8496
Jason Miller <jason@developit.ca>
2015-10-17 17:33:45 -0400

    Add some basic tests

---
+++ server/test/index.js
@@ -0,0 +1,125 @@
+import render from '../src';
+import { h, Component } from 'preact';
+import { expect, use } from 'chai';
+import { spy, match } from 'sinon';
+import sinonChai from 'sinon-chai';
+use(sinonChai);
+
+describe('render-to-string', () => {
+	describe('Basic JSX', () => {
+		it('should render JSX', () => {
+			let rendered = render(<div class="foo">bar</div>),
+				expected = `<div class="foo">bar</div>`;
+
+			expect(rendered).to.equal(expected);
+		});
+	});
+
+	describe('Functional Components', () => {
+		it('should render functional components', () => {
+			let Test = spy( ({ foo, children }) => <div foo={foo}>{ children }</div> );
+
+			let rendered = render(<Test foo="test">content</Test>);
+
+			expect(rendered)
+				.to.equal(`<div foo="test">content</div>`);
+
+			expect(Test)
+				.to.have.been.calledOnce
+				.and.calledWithExactly(
+					match({
+						foo: 'test',
+						children: ['content']
+					})
+				);
+		});
+
+		it('should render functional components within JSX', () => {
+			let Test = spy( ({ foo, children }) => <div foo={foo}>{ children }</div> );
+
+			let rendered = render(
+				<section>
+					<Test foo={1}><span>asdf</span></Test>
+				</section>
+			);
+
+			expect(rendered)
+				.to.equal(`<section><div foo="1"><span>asdf</span></div></section>`);
+
+			expect(Test)
+				.to.have.been.calledOnce
+				.and.calledWithExactly(
+					match({
+						foo: 1,
+						children: [
+							match({ nodeName:'span', children:['asdf'] })
+						]
+					})
+				);
+		});
+	});
+
+	describe('Classical Components', () => {
+		it('should render classical components', () => {
+			class Test extends Component {
+				render({ foo, children }, state) {
+					return <div foo={foo}>{ children }</div>;
+				}
+			}
+
+			Test = spy(Test);
+			spy(Test.prototype, 'render');
+
+			let rendered = render(<Test foo="test">content</Test>);
+
+			expect(rendered)
+				.to.equal(`<div foo="test">content</div>`);
+
+			expect(Test).to.have.been.calledOnce;
+
+			expect(Test.prototype.render)
+				.to.have.been.calledOnce
+				.and.calledWithExactly(
+					match({
+						foo: 'test',
+						children: ['content']
+					}),
+					match({})	// empty state
+				);
+		});
+
+		it('should render classical components within JSX', () => {
+			class Test extends Component {
+				render({ foo, children }, state) {
+					return <div foo={foo}>{ children }</div>;
+				}
+			}
+
+			Test = spy(Test);
+			spy(Test.prototype, 'render');
+
+			let rendered = render(
+				<section>
+					<Test foo={1}><span>asdf</span></Test>
+				</section>
+			);
+
+			expect(rendered)
+				.to.equal(`<section><div foo="1"><span>asdf</span></div></section>`);
+
+			expect(Test).to.have.been.calledOnce;
+
+			expect(Test.prototype.render)
+				.to.have.been.calledOnce
+				.and.calledWithExactly(
+					match({
+						foo: 1,
+						children: [
+							match({ nodeName:'span', children:['asdf'] })
+						]
+					}),
+					match({})	// empty state
+				);
+		});
+	});
+});
