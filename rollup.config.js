15fe432a7c2df790eb1cc045b928af98e4152534
Jason Miller <jasonmiller@synacor.com>
2015-12-03 13:54:12 -0500

    Fix rollup sourceMap file references (waiting on rollup/rollup#344).

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import babel from 'rollup-plugin-babel';
+import path from 'path';
+import fs from 'fs';
 
 export default {
+	sourceMapFile: path.resolve(JSON.parse(fs.readFileSync('./package.json')).main),
 	plugins: [
 		babel({
 			sourceMap: true
