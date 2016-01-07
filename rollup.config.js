c63f6c8a17d8362ae83d0c9f493ff798dce27ada
Jason Miller <jason@developit.ca>
2016-01-06 21:06:58 -0500

    Fix broken sourceMaps build from rollup patch bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import babel from 'rollup-plugin-babel';
-import path from 'path';
-import fs from 'fs';
 
 export default {
-	sourceMapFile: path.resolve(JSON.parse(fs.readFileSync('./package.json')).main),
 	plugins: [
 		babel({
-			sourceMap: true
+			sourceMap: true,
+			exclude: 'node_modules/**'
 		})
 	]
 };
