184e893eec71a5aeb959c6ce45b2b3ba16c87b70
Artem Artemyev <aartemev@naumen.ru>
2016-02-21 01:30:08 +0500

    fixed linting errors

--- test/browser/lifecycle.js
+++ test/browser/lifecycle.js
@@ -18,171 +18,167 @@ describe('Lifecycle methods', () => {
 		scratch = null;
 	});
 
-  describe('#componentWillUpdate', function () {
-    it('should NOT be called on initial render', function () {
-      class ReceivePropsComponent extends Component {
-  			componentWillUpdate() {}
-  			render() {
-  				return <div />;
-  			}
-  		}
-      sinon.spy(ReceivePropsComponent.prototype, 'componentWillUpdate');
-      render(<ReceivePropsComponent />, scratch);
-      expect(ReceivePropsComponent.prototype.componentWillUpdate)
-  			.not.to.have.been.called;
-    });
-    it('should be called when rerender with new props from parent', function () {
-      let doRender;
-      class Outer extends Component {
-        constructor() {
-          super();
-          this.state = { i: 0 };
-        }
-  			componentDidMount() {
-  				doRender = () => this.setState({ i: this.state.i + 1 });
-  			}
-  			render(props, { i }) {
-  				return <Inner i={i} {...props} />;
-  			}
-  		}
-      class Inner extends Component {
-        componentWillUpdate(nextProps, nextState) {
-          expect(nextProps).to.be.deep.equal({i: 1});
-          expect(nextState).to.be.deep.equal({});
-        }
-  			render() {
-          return <div />;
-  			}
-  		}
-      sinon.spy(Inner.prototype, 'componentWillUpdate');
+
+	describe('#componentWillUpdate', () => {
+		it('should NOT be called on initial render', () => {
+			class ReceivePropsComponent extends Component {
+				componentWillUpdate() {}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(ReceivePropsComponent.prototype, 'componentWillUpdate');
+			render(<ReceivePropsComponent />, scratch);
+			expect(ReceivePropsComponent.prototype.componentWillUpdate).not.to.have.been.called;
+		});
+		it('should be called when rerender with new props from parent', () => {
+			let doRender;
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { i: 0 };
+				}
+				componentDidMount() {
+					doRender = () => this.setState({ i: this.state.i + 1 });
+				}
+				render(props, { i }) {
+					return <Inner i={i} {...props} />;
+				}
+			}
+			class Inner extends Component {
+				componentWillUpdate(nextProps, nextState) {
+					expect(nextProps).to.be.deep.equal({i: 1});
+					expect(nextState).to.be.deep.equal({});
+				}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(Inner.prototype, 'componentWillUpdate');
+			sinon.spy(Outer.prototype, 'componentDidMount');
+
+			// Initial render
+			render(<Outer />, scratch);
+			expect(Inner.prototype.componentWillUpdate).not.to.have.been.called;
+
+			// Rerender inner with new props
+			doRender();
+			rerender();
+			expect(Inner.prototype.componentWillUpdate).to.have.been.called;
+		});
+		it('should be called on new state', () => {
+			let doRender;
+			class ReceivePropsComponent extends Component {
+				componentWillUpdate() {}
+				componentDidMount() {
+					doRender = () => this.setState({ i: this.state.i + 1 });
+				}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(ReceivePropsComponent.prototype, 'componentWillUpdate');
+			render(<ReceivePropsComponent />, scratch);
+			expect(ReceivePropsComponent.prototype.componentWillUpdate).not.to.have.been.called;
+
+			doRender();
+			rerender();
+			expect(ReceivePropsComponent.prototype.componentWillUpdate).to.have.been.called;
+		});
+	});
+
+	describe('#componentWillReceiveProps', () => {
+		it('should NOT be called on initial render', () => {
+			class ReceivePropsComponent extends Component {
+				componentWillReceiveProps() {}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(ReceivePropsComponent.prototype, 'componentWillReceiveProps');
+			render(<ReceivePropsComponent />, scratch);
+			expect(ReceivePropsComponent.prototype.componentWillReceiveProps).not.to.have.been.called;
+		});
+		it('should be called when rerender with new props from parent', () => {
+			let doRender;
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { i: 0 };
+				}
+				componentDidMount() {
+					doRender = () => this.setState({ i: this.state.i + 1 });
+				}
+				render(props, { i }) {
+					return <Inner i={i} {...props} />;
+				}
+			}
+			class Inner extends Component {
+				componentWillMount() {
+					expect(this.props.i).to.be.equal(0);
+				}
+				componentWillReceiveProps(nextProps) {
+					expect(nextProps.i).to.be.equal(1);
+				}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(Inner.prototype, 'componentWillReceiveProps');
+			sinon.spy(Outer.prototype, 'componentDidMount');
 
       // Initial render
-      render(<Outer />, scratch);
-      expect(Inner.prototype.componentWillUpdate)
-  			.not.to.have.been.called;
+			render(<Outer />, scratch);
+			expect(Inner.prototype.componentWillReceiveProps).not.to.have.been.called;
 
-      // Rerender inner with new props
-      doRender();
-      rerender();
-      expect(Inner.prototype.componentWillUpdate)
-        .to.have.been.called;
-    });
-    it('should be called on new state', function () {
-      let doRender;
-      class ReceivePropsComponent extends Component {
-        componentWillUpdate() {}
-        componentDidMount() {
-          doRender = () => this.setState({ i: this.state.i + 1 });
-        }
-        render() {
-          return <div />;
-        }
-      }
-      sinon.spy(ReceivePropsComponent.prototype, 'componentWillUpdate');
-      render(<ReceivePropsComponent />, scratch);
-      expect(ReceivePropsComponent.prototype.componentWillUpdate)
-        .not.to.have.been.called;
+			// Rerender inner with new props
+			doRender();
+			rerender();
+			expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
+		});
+		it('should be called in right execution order', () => {
+			let doRender;
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { i: 0 };
+				}
+				componentDidMount() {
+					doRender = () => this.setState({ i: this.state.i + 1 });
+				}
+				render(props, { i }) {
+					return <Inner i={i} {...props} />;
+				}
+			}
+			class Inner extends Component {
+				componentDidUpdate() {
+					expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
+					expect(Inner.prototype.componentWillUpdate).to.have.been.called;
+				}
+				componentWillReceiveProps() {
+					expect(Inner.prototype.componentWillUpdate).not.to.have.been.called;
+					expect(Inner.prototype.componentDidUpdate).not.to.have.been.called;
+				}
+				componentWillUpdate() {
+					expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
+					expect(Inner.prototype.componentDidUpdate).not.to.have.been.called;
+				}
+				render() {
+					return <div />;
+				}
+			}
+			sinon.spy(Inner.prototype, 'componentWillReceiveProps');
+			sinon.spy(Inner.prototype, 'componentDidUpdate');
+			sinon.spy(Inner.prototype, 'componentWillUpdate');
+			sinon.spy(Outer.prototype, 'componentDidMount');
 
-      doRender();
-      rerender();
-      expect(ReceivePropsComponent.prototype.componentWillUpdate)
-        .to.have.been.called;
-    });
-  });
+			render(<Outer />, scratch);
+			doRender();
+			rerender();
 
-  describe('#componentWillReceiveProps', function () {
-    it('should NOT be called on initial render', function () {
-      class ReceivePropsComponent extends Component {
-  			componentWillReceiveProps() {}
-  			render() {
-  				return <div />;
-  			}
-  		}
-      sinon.spy(ReceivePropsComponent.prototype, 'componentWillReceiveProps');
-      render(<ReceivePropsComponent />, scratch);
-      expect(ReceivePropsComponent.prototype.componentWillReceiveProps)
-  			.not.to.have.been.called;
-    });
-    it('should be called when rerender with new props from parent', function () {
-      let doRender;
-      class Outer extends Component {
-        constructor() {
-          super();
-          this.state = { i: 0 };
-        }
-  			componentDidMount() {
-  				doRender = () => this.setState({ i: this.state.i + 1 });
-  			}
-  			render(props, { i }) {
-  				return <Inner i={i} {...props} />;
-  			}
-  		}
-      class Inner extends Component {
-        componentWillMount() {
-          expect(this.props.i).to.be.equal(0);
-        }
-  			componentWillReceiveProps(nextProps) {
-          expect(nextProps.i).to.be.equal(1);
-        }
-  			render() {
-          return <div />;
-  			}
-  		}
-      sinon.spy(Inner.prototype, 'componentWillReceiveProps');
-
-      // Initial render
-      render(<Outer />, scratch);
-      expect(Inner.prototype.componentWillReceiveProps)
-  			.not.to.have.been.called;
-
-      // Rerender inner with new props
-      doRender();
-      rerender();
-      expect(Inner.prototype.componentWillReceiveProps)
-        .to.have.been.called;
-    });
-    it('should be called in right execution order', function () {
-      let doRender;
-      class Outer extends Component {
-        constructor() {
-          super();
-          this.state = { i: 0 };
-        }
-  			componentDidMount() {
-  				doRender = () => this.setState({ i: this.state.i + 1 });
-  			}
-  			render(props, { i }) {
-  				return <Inner i={i} {...props} />;
-  			}
-  		}
-      class Inner extends Component {
-        componentDidUpdate() {
-          expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
-          expect(Inner.prototype.componentWillUpdate).to.have.been.called;
-        }
-  			componentWillReceiveProps(nextProps) {
-          expect(Inner.prototype.componentWillUpdate).not.to.have.been.called;
-          expect(Inner.prototype.componentDidUpdate).not.to.have.been.called;
-        }
-        componentWillUpdate() {
-          expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
-          expect(Inner.prototype.componentDidUpdate).not.to.have.been.called;
-        }
-  			render() {
-          return <div />;
-  			}
-  		}
-      sinon.spy(Inner.prototype, 'componentWillReceiveProps');
-      sinon.spy(Inner.prototype, 'componentDidUpdate');
-      sinon.spy(Inner.prototype, 'componentWillUpdate');
-
-      render(<Outer />, scratch);
-      doRender();
-      rerender();
-
-      expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
-      expect(Inner.prototype.componentWillUpdate).to.have.been.called;
-      expect(Inner.prototype.componentDidUpdate).to.have.been.called;
-    });
-  });
+			expect(Inner.prototype.componentWillReceiveProps).to.have.been.called;
+			expect(Inner.prototype.componentWillUpdate).to.have.been.called;
+			expect(Inner.prototype.componentDidUpdate).to.have.been.called;
+		});
+	});
 });
