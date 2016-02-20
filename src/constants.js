003397aef2af0f4220aaffa1c5db5b40cbc1b465
Jason Miller <jason@developit.ca>
2016-02-20 17:35:50 -0500

    Rename `<x-undefined-element>` to `<undefined>`

--- src/constants.js
+++ src/constants.js
@@ -12,11 +12,11 @@ export const TEXT_CONTENT = !HAS_DOM || 'textContent' in document ? 'textContent
 
 export const ATTR_KEY = typeof Symbol!=='undefined' ? Symbol('preactattr') : '__preactattr_';
 
-export const UNDEFINED_ELEMENT = 'x-undefined-element';
+export const UNDEFINED_ELEMENT = 'undefined';
 
 // DOM properties that should NOT have "px" added when numeric
 export const NON_DIMENSION_PROPS = {
-	boxFlex:1,boxFlexGroup:1,columnCount:1,fillOpacity:1,flex:1,flexGrow:1,
-	flexPositive:1,flexShrink:1,flexNegative:1,fontWeight:1,lineClamp:1,lineHeight:1,
-	opacity:1,order:1,orphans:1,strokeOpacity:1,widows:1,zIndex:1,zoom:1
+	boxFlex:1, boxFlexGroup:1, columnCount:1, fillOpacity:1, flex:1, flexGrow:1,
+	flexPositive:1, flexShrink:1, flexNegative:1, fontWeight:1, lineClamp:1, lineHeight:1,
+	opacity:1, order:1, orphans:1, strokeOpacity:1, widows:1, zIndex:1, zoom:1
 };
