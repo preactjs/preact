47688ae0d5be7cd1a59cd189f967ff613fb22c86
Artem Artemyev <aartemev@naumen.ru>
2016-02-21 02:32:53 +0500

    added support for getDefaultProps and defaultProps #59

--- src/vdom/index.js
+++ src/vdom/index.js
@@ -1,4 +1,4 @@
-import { clone, toLowerCase, isFunction, isString } from '../util';
+import { clone, toLowerCase, isFunction, isString, extend, hasOwnProperty } from '../util';
 import { isFunctionalComponent } from './functional-component';
 
 
@@ -16,6 +16,36 @@ export function isSameNodeType(node, vnode) {
 }
 
 
+/**
+ * Update given props object with props from default props.
+ * If props is exists in default (is not undefined) and is not
+ * exists in given props object (is undefined) then prop from
+ * defaults will be set in given prop object
+ * @param {Object} component
+ * @param {Object} props
+ */
+export function setDefaultProps(nodeName, props) {
+	if (!isString(nodeName)) {
+		// Get default props from cache or set a cache
+		let defaultProps = nodeName.__defaultPropsCache;
+		if (!defaultProps) {
+			defaultProps = extend({}, nodeName.defaultProps || {});
+			const defaultPropsGenerator = nodeName.prototype.getDefaultProps;
+			if (defaultPropsGenerator) {
+				extend(defaultProps, defaultPropsGenerator() || {});
+			}
+			nodeName.__defaultPropsCache = defaultProps;
+		}
+
+		// Merge props with given props object
+		for (let k in defaultProps) {
+			if (hasOwnProperty.call(defaultProps, k) && props[k] === undefined) {
+				props[k] = defaultProps[k];
+			}
+		}
+	}
+}
+
 
 /** Reconstruct Component-style `props` from a VNode
  *	@todo: determine if it would be acceptible to drop the extend() clone here for speed
@@ -26,5 +56,6 @@ export function getNodeProps(vnode) {
 	if (vnode.children) {
 		props.children = vnode.children;
 	}
+	setDefaultProps(vnode.nodeName, props);
 	return props;
 }
