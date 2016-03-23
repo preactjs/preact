6903fdac0a67de74379a5ae0efb47261fa20089b
aimwhy <aimwhy@foxmail.com>
2016-03-23 16:02:41 +0800

    Update diff.js

--- src/vdom/diff.js
+++ src/vdom/diff.js
@@ -189,7 +189,7 @@ export function recollectNodeTree(node, unmountOnly) {
 
 	let component = node._component;
 	if (component) {
-		unmountComponent(node, component, !unmountOnly);
+		unmountComponent(component, !unmountOnly);
 	}
 	else {
 		if (!unmountOnly) {
