7edd5e90550ea1c1c58e637a1574800a7c431cb2
Jason Miller <jason@developit.ca>
2016-04-26 16:24:36 -0400

    Fix props.children being ignored in certain cases. Fixes developit/preact-compat#44 (thanks @ld0rman!)

--- src/h.js
+++ src/h.js
@@ -16,37 +16,42 @@ const SHARED_TEMP_ARRAY = [];
  */
 export default function h(nodeName, attributes) {
 	let len = arguments.length,
+		attributeChildren = attributes && attributes.children,
 		children, arr, lastSimple;
 
-	if (len>2) {
-		children = [];
-		for (let i=2; i<len; i++) {
-			let p = arguments[i];
-			if (falsey(p)) continue;
-			if (p.join) {
-				arr = p;
-			}
-			else {
-				arr = SHARED_TEMP_ARRAY;
-				arr[0] = p;
-			}
-			for (let j=0; j<arr.length; j++) {
-				let child = arr[j],
-					simple = !falsey(child) && !(child instanceof VNode);
-				if (simple) child = String(child);
-				if (simple && lastSimple) {
-					children[children.length-1] += child;
-				}
-				else if (!falsey(child)) {
-					children.push(child);
-				}
-				lastSimple = simple;
-			}
-		}
+	if (attributeChildren) {
+		delete attributes.children;
+
+		// if (len<3) {
+		// 	unfilteredChildren = attributeChildren;
+		// 	start = 0;
+		// }
+		if (len<3) return h(nodeName, attributes, attributeChildren);
 	}
 
-	if (attributes && attributes.children) {
-		delete attributes.children;
+	for (let i=2; i<len; i++) {
+		let p = arguments[i];
+		if (falsey(p)) continue;
+		if (!children) children = [];
+		if (p.join) {
+			arr = p;
+		}
+		else {
+			arr = SHARED_TEMP_ARRAY;
+			arr[0] = p;
+		}
+		for (let j=0; j<arr.length; j++) {
+			let child = arr[j],
+				simple = !falsey(child) && !(child instanceof VNode);
+			if (simple) child = String(child);
+			if (simple && lastSimple) {
+				children[children.length-1] += child;
+			}
+			else if (!falsey(child)) {
+				children.push(child);
+			}
+			lastSimple = simple;
+		}
 	}
 
 	let p = new VNode(nodeName, attributes || undefined, children || undefined);
