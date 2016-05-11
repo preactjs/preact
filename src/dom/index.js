329d823dfc069bf192f5e4fb893c408f4b9281fc
Akeem McLennon <akeem@mclennon.com>
2016-05-11 00:39:47 -0500

    Throw an exception if event handler is not a function

--- src/dom/index.js
+++ src/dom/index.js
@@ -106,7 +106,10 @@ function setComplexAccessor(node, name, value) {
  */
 function eventProxy(e) {
 	let fn = this._listeners[normalizeEventName(e.type)];
-	if (fn && typeof(fn) === "function") return fn.call(this, optionsHook('event', e) || e);
+	if (fn && typeof(fn) !== "function"){
+		throw new Error(`Expecting ${e.type} event handler to be a function`);
+	}
+	if (fn) return fn.call(this, optionsHook('event', e) || e);
 }
 
 
