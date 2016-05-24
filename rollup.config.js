f74a2c8215227517ef359aed7a31a3cb69dcfec7
Jason Miller <jason@developit.ca>
2016-05-24 08:26:20 -0400

    Use `rollup-plugin-memory` to exclude default export from the CJS build (drops the file size a bunch, but the ES Modules source retains both named and default exports).

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import npm from 'rollup-plugin-npm';
 import babel from 'rollup-plugin-babel';
+import memory from 'rollup-plugin-memory';
 
 export default {
+	exports: 'named',
 	plugins: [
+		memory({
+			path: 'src/preact',
+			contents: "export { h, Component, render, rerender, options } from './preact';"
+		}),
 		npm({
-			// for index.js:
 			main: true
 		}),
 		babel({
