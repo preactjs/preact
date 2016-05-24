ae8f12fe22fe3c144337e55b491d54629cb2ce65
Dan Zajdband <dan.zajdband@gmail.com>
2016-05-23 22:45:07 -0400

    Fixed named export for using with rollup and meet the js modules spec (#150)

--- src/preact.js
+++ src/preact.js
@@ -1,14 +1,21 @@
-import h from './h';
-import Component from './component';
-import render from './render';
-import { rerender } from './render-queue';
-import options from './options';
+import _h from './h';
+import _Component from './component';
+import _render from './render';
+import { rerender as _rerender } from './render-queue';
+import _options from './options';
 
 export default {
-	h,
-	Component,
-	render,
-	rerender,
-	options,
-	hooks: options
+	h: _h,
+	Component: _Component,
+	render: _render,
+	rerender: _rerender,
+	options: _options,
+	hooks: _options
 };
+
+export const h = _h;
+export const Component = _Component;
+export const render = _render;
+export const rerender = _rerender;
+export const options = _options;
+export const hooks = _options;
