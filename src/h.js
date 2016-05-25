fb19ac1b1f4e72151abc21d483f7b2d7f8424a84
Jason Miller <jason@developit.ca>
2016-05-24 20:36:50 -0400

    Small optimization for dealing with `props.children` within `h()`

--- src/h.js
+++ src/h.js
@@ -16,53 +16,48 @@ const SHARED_TEMP_ARRAY = [];
  */
 export function h(nodeName, attributes, firstChild) {
 	let len = arguments.length,
-		attributeChildren = attributes && attributes.children,
 		children, arr, lastSimple;
 
-	if (attributeChildren) {
-		delete attributes.children;
 
-		// if (len<3) {
-		// 	unfilteredChildren = attributeChildren;
-		// 	start = 0;
-		// }
-		if (len<3) return h(nodeName, attributes, attributeChildren);
-	}
-
-	let type = typeof firstChild;
-	if (len===3 && type!=='object' && type!=='function') {
-		if (!falsey(firstChild)) {
-			children = [String(firstChild)];
+	if (len>2) {
+		let type = typeof firstChild;
+		if (len===3 && type!=='object' && type!=='function') {
+			if (!falsey(firstChild)) {
+				children = [String(firstChild)];
+			}
 		}
-	}
-	else if (len>2) {
-		children = [];
-		for (let i=2; i<len; i++) {
-			let p = arguments[i];
-			if (falsey(p)) continue;
-			if (p.join) {
-				arr = p;
-			}
-			else {
-				arr = SHARED_TEMP_ARRAY;
-				arr[0] = p;
-			}
-			for (let j=0; j<arr.length; j++) {
-				let child = arr[j],
-					simple = !falsey(child) && !(child instanceof VNode);
-				if (simple && !isString(child)) child = String(child);
-				if (simple && lastSimple) {
-					children[children.length-1] += child;
+		else {
+			children = [];
+			for (let i=2; i<len; i++) {
+				let p = arguments[i];
+				if (falsey(p)) continue;
+				if (p.join) {
+					arr = p;
 				}
-				else if (!falsey(child)) {
-					children.push(child);
+				else {
+					arr = SHARED_TEMP_ARRAY;
+					arr[0] = p;
 				}
-				lastSimple = simple;
+				for (let j=0; j<arr.length; j++) {
+					let child = arr[j],
+						simple = !falsey(child) && !(child instanceof VNode);
+					if (simple && !isString(child)) child = String(child);
+					if (simple && lastSimple) {
+						children[children.length-1] += child;
+					}
+					else if (!falsey(child)) {
+						children.push(child);
+					}
+					lastSimple = simple;
+				}
 			}
 		}
 	}
+	else if (attributes && attributes.children) {
+		return h(nodeName, attributes, attributes.children);
+	}
 
-	let p = new VNode(nodeName, attributes || undefined, children || undefined);
+	let p = new VNode(nodeName, attributes || undefined, children);
 	optionsHook('vnode', p);
 	return p;
 }
