1baf1a06327e5bc6c570fbaa38bfa2632185ca7b
Jason Miller <jason@developit.ca>
2016-05-30 13:55:28 -0400

    Small performance tweak to sinon deepEqual() fix

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 // patch Sinon 1.x's deepEqual to support objects with no prototype.
 // @see https://github.com/sinonjs/sinon/pull/1050
+
+let needsPatch = false;
 try {
 	let foo = Object.create(null),
 		bar = Object.create(null);
@@ -8,13 +10,18 @@ try {
 	sinon.deepEqual(foo, bar);
 } catch (err) {
 	// console.log('Detected Sinon reliance on hasOwnProperty: patching deepEqual. '+err);  // eslint-disable-line
-	let eq = sinon.deepEqual;
-	let isPrototypeless = obj => obj && typeof obj==='object' && (!obj.prototype || !obj.prototype.hasOwnProperty);
+	needsPatch = true;
+}
+
+if (needsPatch) {
+	let isPrototypeless = obj => !obj.prototype || !obj.prototype.hasOwnProperty,
+		hasOwnProperty = Object.prototype.hasOwnProperty;
+	sinon._originalDeepEqual = sinon.deepEqual;
 	sinon.deepEqual = (a, b) => {
-		if (!sinon.match || !sinon.match.isMatcher(a)) {
-			if (isPrototypeless(a)) a = { ...a };
-			if (isPrototypeless(b)) b = { ...b };
+		if (!sinon.match.isMatcher(a)) {
+			if (a && typeof a==='object' && isPrototypeless(a)) a.hasOwnProperty = hasOwnProperty;
+			if (b && typeof b==='object' && isPrototypeless(b)) b.hasOwnProperty = hasOwnProperty;
 		}
-		return eq.call(sinon, a, b);
+		return sinon._originalDeepEqual(a, b);
 	};
 }
