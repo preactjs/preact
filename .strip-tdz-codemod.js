a238ae5e621dee591211f0f8c72655e447cc845a
Jason Miller <jason@developit.ca>
2016-06-20 19:20:47 -0400

    Fix overly optimistic codemod that was breaking child resolution in `5.0.1-beta.14`.  The codemod is now more intelligent and will only remove pointless initialization to `undefined` if not done within a conditionally executable block.

--- .strip-tdz-codemod.js
+++ .strip-tdz-codemod.js
@@ -6,28 +6,57 @@ module.exports = function(file, api, options) {
 
 	var found = 0;
 
+	// parent node types that we don't want to remove pointless initializations from (because it breaks hoisting)
+	var blocked = ['ForStatement', 'WhileStatement', 'IfStatement', 'SwtichStatement'];
+
 	var code = api.jscodeshift(file.source)
 		.find(jscodeshift.VariableDeclaration)
-		.forEach(function(decl) {
-			decl.value.declarations.forEach(handleDeclaration);
-		});
+		.forEach(handleDeclaration);
 
-	function handleDeclaration(node) {
-		var init = node.init,
-			isPointless = false;
+	function handleDeclaration(decl) {
+		var p = decl,
+			remove = true,
+			fname;
+
+		while (p = p.parentPath) {
+			if (!blocked.indexOf(p.value.type)) {
+				remove = false;
+				break
+			}
+
+			if (p.value.type && p.value.type.match(/Function/)) {
+				fname = p.value.id && p.value.id.name || '<anon>';
+				break;
+			}
+		}
+
+		decl.value.declarations.filter(isPointless).forEach(function(node) {
+			if (remove===false) {
+				console.log('> Skipping removal of undefined init for "'+node.id.name+'": within '+p.value.type);
+			}
+			else {
+				removeNodeInitialization(node, fname);
+			}
+		});
+	}
+
+	function removeNodeInitialization(node, fname) {
+		// console.log('Removing undefined init for "'+node.id.name+'" within '+fname+'()');
+		node.init = null;
+		found++;
+	}
+
+	function isPointless(node) {
+		var init = node.init;
 		if (init) {
 			if (init.type==='UnaryExpression' && init.operator==='void' && init.argument.value==0) {
-				isPointless = true;
+				return true;
 			}
 			if (init.type==='Identifier' && init.name==='undefined') {
-				isPointless = true;
+				return true;
 			}
 		}
-
-		if (isPointless) {
-			node.init = null;
-			found++;
-		}
+		return false;
 	}
 
 	return found ? code.toSource({ quote: 'single' }) : null;
