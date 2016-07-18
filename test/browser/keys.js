a3cf22dfa6f6d39b29171491bf4c0d01580096ae
Jason Miller <jason@developit.ca>
2016-07-18 19:13:06 -0400

    Add test for #232

--- test/browser/keys.js
+++ test/browser/keys.js
@@ -1,4 +1,4 @@
-import { h, render } from '../../src/preact';
+import { h, Component, render } from '../../src/preact';
 /** @jsx h */
 
 describe('keys', () => {
@@ -41,5 +41,44 @@ describe('keys', () => {
 		expect(<div />).to.have.property('key').that.is.empty;
 		expect(<div a="a" />).to.have.property('key').that.is.empty;
 		expect(<div key="1" />).to.have.property('key', '1');
+	});
+
+	it('should remove keyed nodes (#232)', () => {
+		class App extends Component {
+			componentDidMount() {
+				setTimeout(() => this.setState({opened: true,loading: true}), 10);
+				setTimeout(() => this.setState({opened: true,loading: false}), 20);
+			}
+
+			render({ opened, loading }) {
+				return (
+					<BusyIndicator id="app" busy={loading}>
+						<div>This div needs to be here for this to break</div>
+						{ opened && !loading && <div>{[]}</div> }
+					</BusyIndicator>
+				);
+			}
+		}
+
+		class BusyIndicator extends Component {
+			render({ children, busy }) {
+				return <div class={busy ? "busy" : ""}>
+					{ children && children.length ? children : <div class="busy-placeholder"></div> }
+					<div class="indicator">
+						<div>indicator</div>
+						<div>indicator</div>
+						<div>indicator</div>
+					</div>
+				</div>;
+			}
+		}
+
+		let root;
+
+		root = render(<App />, scratch, root);
+		root = render(<App opened loading />, scratch, root);
+		root = render(<App opened />, scratch, root);
+
+		expect(root).to.have.property('innerHTML', '<div>This div needs to be here for this to break</div><div class=""></div><div class="indicator"><div>indicator</div><div>indicator</div><div>indicator</div></div>');
 	});
 });
