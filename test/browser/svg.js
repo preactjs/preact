preactjs/preact

commit e7c6ab672a5f18e67dec3cb7e7feabc7f404b337

Move both className to class normalization and hashed class stringification out of `h()` and into the DOM renderer. Tiny possible breakage here:  if you were creating VNodes using `className` and expecting them to have `attributes.class` instead of `attributes.className`, that is no longer the case.

src/dom/index.js
@@ -1,6 +1,6 @@
 import { ATTR_KEY, NON_DIMENSION_PROPS, NON_BUBBLING_EVENTS } from '../constants';
 import options from '../options';
-import { toLowerCase, empty, falsey, isString, isFunction } from '../util';
+import { toLowerCase, empty, falsey, isString, isFunction, hashToClassName } from '../util';
 
 
 export function ensureNodeData(node, data) {
@@ -28,8 +28,8 @@ export function setAccessor(node, name, value, old, isSvg) {
 
 	if (name==='key' || name==='children' || name==='innerHTML') return;
 
-	if (name==='class' && !isSvg) {
-		node.className = value || '';
+	if ((name==='class' || name==='className') && !isSvg) {
+		node.className = value && typeof value==='object' ? hashToClassName(value) : value || '';
 	}
 	else if (name==='style') {
 		if (!value || isString(value) || isString(old)) {

src/h.js
@@ -1,6 +1,6 @@
 import { VNode } from './vnode';
 import options from './options';
-import { falsey, isFunction, isString, hashToClassName } from './util';
+import { falsey, isFunction, isString } from './util';
 
 
 // tracks whether the previously normalized child was a "simple" vnode (ie, converts to a String)
@@ -53,18 +53,6 @@ export function h(nodeName, attributes) {
 			delete attributes.children;
 		}
 
-		if (!isFunction(nodeName)) {
-			// normalize className to class.
-			if ('className' in attributes) {
-				attributes.class = attributes.className;
-				delete attributes.className;
-			}
-
-			lastSimple = attributes.class;
-			if (lastSimple && !isString(lastSimple)) {
-				attributes.class = hashToClassName(lastSimple);
-			}
-		}
 	}
 
 	let p = new VNode(nodeName, attributes || undefined, children);

commit 81754b8c11b18e48a59efb1f20a6f49f446a2c45

Fix serializing class for SVG (test included)
e7c6ab6 broke serializing `class={{}}` for SVG elements.  This fixes
that regression and does a little cleans up the logic a little bit.

I've also added a test verifying the proper behavior for SVG.

src/dom/index.js
@@ -28,11 +28,15 @@ export function setAccessor(node, name, value, old, isSvg) {
 
 	if (name==='className') name = 'class';
 
+	if (name==='class') {
+		value = value && typeof value==='object' ? hashToClassName(value) : value;
+	}
+
 	if (name==='key' || name==='children' || name==='innerHTML') {
 		// skip these
 	}
 	else if (name==='class' && !isSvg) {
-		node.className = value && typeof value==='object' ? hashToClassName(value) : value || '';
+		node.className = value || '';
 	}
 	else if (name==='style') {
 		if (!value || isString(value) || isString(old)) {

test/browser/svg.js
@@ -86,6 +86,14 @@ describe('svg', () => {
 		expect(scratch.innerHTML).to.contain(` class="foo bar"`);
 	});
 
+	it('should serialize class', () => {
+		render((
+			<svg viewBox="0 0 1 1" class={{ foo: true, bar: false, other: 'hello' }} />
+		), scratch);
+
+		expect(scratch.innerHTML).to.contain(` class="foo other"`);
+	});
+
 	it('should switch back to HTML for <foreignObject>', () => {
 		render((
 			<svg>