a1912c32e4c30b489f97b9539af3e92ee09a3bfa
Bill Neff <bneff@synacor.com>
2017-03-16 18:33:11 -0400

    protectOlderBrowsers Protect against instantiating Map in browsers that don't support it in devtools

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -132,7 +132,7 @@ function createReactCompositeComponent(component) {
  * The same React*Component instance must be used when notifying devtools
  * about the initial mount of a component and subsequent updates.
  */
-let instanceMap = new Map();
+let instanceMap = typeof Map==='function' && new Map();
 
 /**
  * Update (and create if necessary) the ReactDOMComponent|ReactCompositeComponent-like
@@ -177,7 +177,7 @@ function findRoots(node, roots) {
 /**
  * Map of functional component name -> wrapper class.
  */
-let functionalComponentWrappers = new Map();
+let functionalComponentWrappers = typeof Map==='function' && new Map();
 
 /**
  * Wrap a functional component with a stateful component.
