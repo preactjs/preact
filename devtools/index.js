6c3304764bfce33ff1031e5f702ba779a67c718c
Siqi Tian <Siqi.Tian@minted.com>
2017-03-20 16:56:46 -0700

    use process.env

--- devtools/index.js
+++ devtools/index.js
@@ -1,4 +1,4 @@
-if (NODE_ENV !== 'production') {
+if (typeof process !== 'undefined' && process.env && process.env.NODE_ENV !== 'production') {
 	const initDevTools = require('./devtools').initDevTools;
 	initDevTools();
 }
