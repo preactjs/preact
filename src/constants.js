727f0360c72a933eb46f16db73e7ef9217302d1d
Jason Miller <jason@developit.ca>
2017-04-05 22:30:39 -0400

    [breaking] Remove whitelisted capturing events and instead add support for `onXxxxCapture`. Switching from using `_listeners` to detect addition/removal very likely fixes #616.

--- src/constants.js
+++ src/constants.js
@@ -12,5 +12,3 @@ export const ATTR_KEY = '__preactattr_';
 // DOM properties that should NOT have "px" added when numeric
 export const IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|ows|mnc|ntw|ine[ch]|zoo|^ord/i;
 
-// DOM event types that do not bubble and should be attached via useCapture
-export const NON_BUBBLING_EVENTS = { blur:1, error:1, focus:1, load:1, resize:1, scroll:1 };
