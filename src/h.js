e2bbc85194680331c83431eff43bb778dad346de
Hassan Bazzi <contact@hassanbazzi.com>
2017-04-06 17:42:27 -0700

    Move boolean check out of component check

--- src/h.js
+++ src/h.js
@@ -25,10 +25,12 @@ export function h(nodeName, attributes) {
 			for (i=child.length; i--; ) stack.push(child[i]);
 		}
 		else {
+			if (child===true || child===false) child = null;
+
 			if ((simple = typeof nodeName!=='function')) {
-				if (typeof child==='number') child = String(child);
-				else if (child===true || child===false || child==null) child = '';
-				else if (typeof child!=='string') simple = false;
+			    if (child==null) child = '';
+			    else if (typeof child==='number') child = String(child);
+			    else if (typeof child!=='string') simple = false;
 			}
 
 			if (simple && lastSimple) {
