3b3d2576adc236e98ec3bd4c3acda221be7dd744
Hassan Bazzi <contact@hassanbazzi.com>
2017-04-06 17:49:39 -0700

    Move boolean check out of component check (#629)
    * Move boolean check out of component check
    
    * weird spacing
    
    * weird spacing

--- src/h.js
+++ src/h.js
@@ -25,9 +25,11 @@ export function h(nodeName, attributes) {
 			for (i=child.length; i--; ) stack.push(child[i]);
 		}
 		else {
+			if (child===true || child===false) child = null;
+
 			if ((simple = typeof nodeName!=='function')) {
-				if (typeof child==='number') child = String(child);
-				else if (child===true || child===false || child==null) child = '';
+				if (child==null) child = '';
+				else if (typeof child==='number') child = String(child);
 				else if (typeof child!=='string') simple = false;
 			}
 
