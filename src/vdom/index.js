1fe668ab67eadb2970009e718bd0b48805fd5e4c
Jason Miller <jason@developit.ca>
2017-04-06 21:03:10 -0400

    Add explanatory comments, remove unnecessary comments.

--- src/vdom/index.js
+++ src/vdom/index.js
@@ -8,24 +8,21 @@ import { extend } from '../util';
  */
 export function isSameNodeType(node, vnode, hydrating) {
 	if (typeof vnode==='string' || typeof vnode==='number') {
-		// return node instanceof Text;
 		return node.splitText!==undefined;
-		// return typeof node.data!=='undefined';
 	}
 	if (typeof vnode.nodeName==='string') {
-	// if (isString(vnode.nodeName)) {
 		return !node._componentConstructor && isNamedNode(node, vnode.nodeName);
-		// return !node._componentConstructor && (node.normalizedNodeName || node.nodeName.toLowerCase())===vnode.nodeName;
 	}
 	return hydrating || node._componentConstructor===vnode.nodeName;
 }
 
 
+/** Check if an Element has a given normalized name.
+*	@param {Element} node
+*	@param {String} nodeName
+ */
 export function isNamedNode(node, nodeName) {
-	// return node.normalizedNodeName===nodeName || toLowerCase(node.nodeName)===toLowerCase(nodeName);
-	// return node.normalizedNodeName!==undefined ? node.normalizedNodeName===nodeName : node.nodeName.toLowerCase()===nodeName.toLowerCase();
 	return node.normalizedNodeName===nodeName || node.nodeName.toLowerCase()===nodeName.toLowerCase();
-	// return node.nodeName===nodeName.toUpperCase();
 }
 
 
@@ -40,10 +37,6 @@ export function getNodeProps(vnode) {
 	let props = extend({}, vnode.attributes);
 	props.children = vnode.children;
 
-	// let props = { children: vnode.children };
-	// extend(props, vnode.attributes);
-	// for (let i in vnode.attributes) props[i] = vnode.attributes[i];
-
 	let defaultProps = vnode.nodeName.defaultProps;
 	if (defaultProps!==undefined) {
 		for (let i in defaultProps) {
@@ -54,30 +47,4 @@ export function getNodeProps(vnode) {
 	}
 
 	return props;
-
-
-	/*
-	// let props = clone(vnode.attributes);
-	let props = extend({}, vnode.attributes);
-	props.children = vnode.children;
-
-	let defaultProps = vnode.nodeName.defaultProps;
-	if (defaultProps) {
-		for (let i in defaultProps) {
-			if (props[i]===undefined) {
-				props[i] = defaultProps[i];
-			}
-		}
-	}
-
-	// if (vnode.nodeName.defaultProps!=null) {
-	// 	for (let i in vnode.nodeName.defaultProps) {
-	// 		if (props[i]===undefined) {
-	// 			props[i] = vnode.nodeName.defaultProps[i];
-	// 		}
-	// 	}
-	// }
-
-	return props;
-	*/
 }
