7956ee3c9520203e7601e516afd40506beb094f8
Arthur Stolyar <nekr.fabula@gmail.com>
2017-04-15 02:54:18 +0300

    preact/debug report error on duplicate keys

--- debug.js
+++ debug.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 		options.vnode = function (vnode) {
 			var nodeName = vnode.nodeName;
 			var attributes = vnode.attributes;
+			var children = vnode.children;
 
 			if (nodeName === void 0) {
 				throw new Error('Undefined component passed to preact.h()');
@@ -20,7 +21,66 @@ if (process.env.NODE_ENV === 'development') {
 				throw new Error('Component\'s "ref" property should be a function,' + (' but [' + typeof attributes.ref + '] passed'));
 			}
 
+			{
+				(function () {
+					var keys = {};
+
+					inspectChildren(children, function (deepChild) {
+						if (!deepChild) return;
+
+						// In Preact, all keys are stored as object values, i.e. being strings
+						var key = deepChild.key + '';
+
+						if (key in keys) {
+							/* eslint-disable no-console */
+							console.error('Following component has two or more children with the ' + 'same "key" attribute. This may cause glitches and misbehavior ' + 'in rendering process. Component: \n\n' + serializeVNode(vnode) + '\n\n');
+
+							// Return early to not spam the console
+							return true;
+						}
+
+						keys[key] = true;
+					});
+				})();
+			}
+
 			return oldVnodeOption.call(this, vnode);
+		};
+
+		var inspectChildren = function inspectChildren(children, inspect) {
+			return children.some(function (child, i) {
+				if (Array.isArray(child)) {
+					return inspectChildren(child, inspect);
+				}
+
+				return inspect(child, i);
+			});
+		};
+
+		var serializeVNode = function serializeVNode(_ref) {
+			var nodeName = _ref.nodeName;
+			var attributes = _ref.attributes;
+
+			var name = undefined;
+			var props = undefined;
+
+			if (typeof nodeName === 'function') {
+				name = nodeName.name || nodeName.displayName;
+			} else {
+				name = nodeName;
+			}
+
+			if (attributes) {
+				props = Object.keys(attributes).map(function (attr) {
+					return attr + '=' + JSON.stringify(attributes[attr] + '');
+				});
+			}
+
+			if (!props) {
+				return '<' + name + ' />';
+			}
+
+			return '<' + name + ' ' + props.join(' ') + ' />';
 		};
 
 		require('preact/devtools');
--- debug/index.js
+++ debug/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
 	const oldVnodeOption = options.vnode;
 
 	options.vnode = function(vnode) {
-		const { nodeName, attributes } = vnode;
+		const { nodeName, attributes, children } = vnode;
 
 		if (nodeName === void 0) {
 			throw new Error('Undefined component passed to preact.h()');
@@ -19,7 +19,66 @@ if (process.env.NODE_ENV === 'development') {
 			);
 		}
 
+		{
+			const keys = {};
+
+			inspectChildren(children, (deepChild) => {
+				if (!deepChild) return;
+
+				// In Preact, all keys are stored as object values, i.e. being strings
+				const key = deepChild.key + '';
+
+				if (key in keys) {
+					/* eslint-disable no-console */
+					console.error(
+						'Following component has two or more children with the ' +
+						'same "key" attribute. This may cause glitches and misbehavior ' +
+						'in rendering process. Component: \n\n' +
+						serializeVNode(vnode) + '\n\n'
+					);
+
+					// Return early to not spam the console
+					return true;
+				}
+
+				keys[key] = true;
+			});
+		}
+
 		return oldVnodeOption.call(this, vnode);
+	};
+
+	const inspectChildren = (children, inspect) => {
+		return children.some((child, i) => {
+			if (Array.isArray(child)) {
+				return inspectChildren(child, inspect);
+			}
+
+			return inspect(child, i);
+		});
+	};
+
+	const serializeVNode = ({ nodeName, attributes }) => {
+		let name;
+		let props;
+
+		if (typeof nodeName === 'function') {
+			name = nodeName.name || nodeName.displayName;
+		} else {
+			name = nodeName;
+		}
+
+		if (attributes) {
+			props = Object.keys(attributes).map(attr => {
+				return `${attr}=${JSON.stringify(attributes[attr] + '')}`;
+			});
+		}
+
+		if (!props) {
+			return `<${name} />`;
+		}
+
+		return `<${name} ${props.join(' ')} />`;
 	};
 
 	require('preact/devtools');
