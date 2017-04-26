d4f55b129f01aaf0dbcc7ac1a93df3eea1ec2250
Denis Bardadym <bardadymchik@gmail.com>
2017-04-26 11:31:09 +0300

    Use build code from preact-compat

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,28 +1,41 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import memory from 'rollup-plugin-memory';
+import fs from "fs";
+import memory from "rollup-plugin-memory";
+import buble from "rollup-plugin-buble";
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+
+let pkg = JSON.parse(fs.readFileSync("./package.json"));
+
+let external = Object.keys(pkg.peerDependencies || {}).concat(Object.keys(pkg.dependencies || {}));
+
+let format = process.env.FORMAT === "es" ? "es" : "umd";
 
 export default {
-	useStrict: true,
-	format: 'iife',
-	entry: 'src/preact.js',
-	plugins: [
-		memory({
-			path: 'src/preact.js',
-			contents: `
-				import preact from './preact';
-				if (typeof module!='undefined') module.exports = preact;
-				else self.preact = preact;
-			`
-		}),
-		nodeResolve({
-			main: true
-		}),
-		babel({
-			sourceMap: true,
-			loose: 'all',
-			blacklist: ['es6.tailCall'],
-			exclude: 'node_modules/**'
-		})
-	]
+  entry: "src/preact.js",
+  sourceMap: true,
+  exports: format === "es" ? null : "default",
+  dest: format === "es" ? pkg.module : pkg.main,
+  format,
+  external,
+  useStrict: false,
+  plugins: [
+    format === "umd" &&
+      memory({
+        path: "src/preact.js",
+        contents: "export { default } from './preact';"
+      }),
+    buble({
+      objectAssign: "extend",
+      namedFunctionExpressions: false
+    }),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      skip: external
+    }),
+    commonjs({
+      include: "node_modules/**",
+      exclude: "**/*.css"
+    })
+  ].filter(Boolean)
 };
