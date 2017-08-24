a42390d8d56e51de1d7034d8abc62da16431a458
Jason Miller <jason@developit.ca>
2017-08-24 12:25:52 -0400

    Update rollup and modify configs accordingly (fixes #800)

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,9 +3,14 @@ import babel from 'rollup-plugin-babel';
 import memory from 'rollup-plugin-memory';
 
 export default {
-	useStrict: true,
-	format: 'iife',
-	entry: 'src/preact.js',
+	strict: true,
+	input: 'src/preact.js',
+	output: {
+		format: 'iife',
+		file: 'dist/preact.dev.js',
+		name: 'preact',
+		sourcemap: true
+	},
 	plugins: [
 		memory({
 			path: 'src/preact.js',
