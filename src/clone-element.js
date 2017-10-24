05c4b2e43cdf1616fcb9292300a2051e3c13a037
Jason Miller <developit@users.noreply.github.com>
2017-10-24 08:42:51 -0700

    Update clone-element.js

--- src/clone-element.js
+++ src/clone-element.js
@@ -2,19 +2,10 @@ import { extend } from './util';
 import { h } from './h';
 
 /**
- * The method is used to clone a given virtual node with
- * the given properties.
- * 
- * @param {VNode} vnode the virutal node to clone
- *
- * @param props the attributes/properties to add to the
- * clone node
- *
- * Note: the function accepts additional arguments that are
- * added as the children of the cloned node if present. If no
- * additional arguments to the method call are provided, any
- * children to the provided virtual node are added to the cloned
- * node.
+ * Clones the given VNode, optionally adding attributes/props and replacing its children.
+ * @param {VNode} vnode		The virutal DOM element to clone
+ * @param {Object} props	Attributes/props to add when cloning
+ * @param {VNode} rest		Any additional arguments will be used as replacement children.
  */
 export function cloneElement(vnode, props) {
 	return h(
