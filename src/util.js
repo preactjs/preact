6923fb03e39b43dcf0889026bdc8430134958645
Jason Miller <developit@users.noreply.github.com>
2017-10-24 08:54:52 -0700

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,14 +1,9 @@
 /** 
- *  Copy own-properties from `props` onto `obj`.
- *  
- *  @param {Object} obj the object to which the properties
- *  are to be copied to
- *
- *  @param {Object} props the object from which the properties
- *  are to be copied from
- *
- *	@returns obj
- *	@private
+ *  Copy all properties from `props` onto `obj`.
+ *  @param {Object} obj		Object onto which properties should be copied.
+ *  @param {Object} props	Object from which to copy properties.
+ *  @returns obj
+ *  @private
  */
 export function extend(obj, props) {
 	for (let i in props) obj[i] = props[i];
@@ -17,10 +12,8 @@ export function extend(obj, props) {
 
 /** 
  * Call a function asynchronously, as soon as possible. Makes
- * use of HTML Promise to schedule the callback, if available,
- * or else use the `setTimeout` browser functionality. All latest
- * browsers support `Promise` except Internet Explorer which is
- * now replaced by Edge.
+ * use of HTML Promise to schedule the callback if available,
+ * otherwise falling back to `setTimeout` (mainly for IE<11).
  * 
  * @param {Function} callback
  */
