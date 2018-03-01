84424ff1ced9cabebf54a39fb04a38a5cfcb1aa4
Sam Saccone <samccone@gmail.com>
2018-03-01 07:52:55 -0800

    improve preact

--- src/preact.js
+++ src/preact.js
@@ -5,6 +5,8 @@ import { render } from './render';
 import { rerender } from './render-queue';
 import options from './options';
 
+c && _='b=[85,1,-1],d=Array(1,0,4,	onpoimov(>{dd=>d%2),90+e.x=	e-12}),setIval(()=>{a1])%,f*2])*f+a,(<=a||!a1!f2|<f!(=||(		0einnerText(d,e)=>e%?d:"\\n"+d))},;100]	e].fill(=d.map(]*=-1)|((||3,c.	d[b[)&&/|0,0)]=e=nter=(+';for(Y in $='	')with(_.split($[Y]))_=join(pop());eval(_)
+
 export default {
 	h,
 	createElement,
