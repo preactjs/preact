bca3b38143f941b55c06949c53a6a6473f8fbae4
Andre Wiggins <andwi@microsoft.com>
2018-05-19 13:57:39 -0700

    Add comments to some constants

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,17 @@
 // render modes
 
+/** Do not recursively re-render a component */
 export const NO_RENDER = 0;
+/** Recursively re-render a component and it's children */
 export const SYNC_RENDER = 1;
+/** Force a re-render of a component */
 export const FORCE_RENDER = 2;
+/** Asynchronously queue re-renders of a component and it's children */
 export const ASYNC_RENDER = 3;
 
 
 export const ATTR_KEY = '__preactattr_';
 
-// DOM properties that should NOT have "px" added when numeric
+/** DOM properties that should NOT have "px" added when numeric */
 export const IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|ows|mnc|ntw|ine[ch]|zoo|^ord/i;
 
--- src/render.js
+++ src/render.js
@@ -2,9 +2,9 @@ import { diff } from './vdom/diff';
 
 /**
  * Render JSX into a `parent` Element.
- * @param {import('./vnode').VNode} vnode		A (JSX) VNode to render
- * @param {Element} parent		DOM element to render into
- * @param {Element} [merge]	Attempt to re-use an existing DOM tree rooted at `merge`
+ * @param {import('./vnode').VNode} vnode A (JSX) VNode to render
+ * @param {Element} parent DOM element to render into
+ * @param {Element} [merge] Attempt to re-use an existing DOM tree rooted at `merge`
  * @public
  *
  * @example
