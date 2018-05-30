74fddde493e747b3e78d4bf9bbdfd1c96e2d1047
Ryan Petrich <rpetrich@gmail.com>
2018-05-30 15:32:22 -0400

    Remove error parameter in componentDidCatch (which triggered linter warning)

--- test/browser/lifecycle.js
+++ test/browser/lifecycle.js
@@ -1225,7 +1225,7 @@ describe('Lifecycle methods', () => {
 				}
 			}
 			class ErrorAdapterComponent extends Component {
-				componentDidCatch(error) {
+				componentDidCatch() {
 					// Ignore the error
 				}
 				render() {
