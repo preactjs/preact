ebd402759cdba6cfa59cb63c9f9229479b0eb0b8
Andre Wiggins <andwi@microsoft.com>
2018-05-29 21:24:31 -0700

    Undo using JS imports for JSDocs

--- src/clone-element.js
+++ src/clone-element.js
@@ -1,13 +1,12 @@
 import { extend } from './util';
 import { h } from './h';
-import { VNode } from './vnode';
 
 /**
  * Clones the given VNode, optionally adding attributes/props and replacing its
  * children.
- * @param {VNode} vnode The virtual DOM element to clone
+ * @param {import('./vnode').VNode} vnode The virtual DOM element to clone
  * @param {object} props Attributes/props to add when cloning
- * @param {Array<VNode>} [rest] Any additional arguments will be used as replacement
+ * @param {Array<import('./vnode').VNode>} [rest] Any additional arguments will be used as replacement
  *  children.
  */
 export function cloneElement(vnode, props) {
