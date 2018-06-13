2fd478f12df068a8923abe6e0008f780a088e7a6
Jason Miller <jason@developit.ca>
2018-06-12 21:23:54 -0400

    Update tests to reflect changes between preact and ceviche

--- test/shared/exports.js
+++ test/shared/exports.js
@@ -1,10 +1,10 @@
-import preact, { createElement as h, Component, render, rerender, options } from '../../src/index';
+import preact, { createElement, Component, render, rerender, options } from '../../src/index';
 import { expect } from 'chai';
 
 describe('preact', () => {
 	it.skip('should be available as a default export', () => {
 		expect(preact).to.be.an('object');
-		expect(preact).to.have.property('h', h);
+		expect(preact).to.have.property('h', createElement);
 		expect(preact).to.have.property('Component', Component);
 		expect(preact).to.have.property('render', render);
 		expect(preact).to.have.property('rerender', rerender);
@@ -12,7 +12,7 @@ describe('preact', () => {
 	});
 
 	it('should be available as named exports', () => {
-		expect(h).to.be.a('function');
+		expect(createElement).to.be.a('function');
 		expect(Component).to.be.a('function');
 		expect(render).to.be.a('function');
 		// expect(rerender).to.be.a('function');
