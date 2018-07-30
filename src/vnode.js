a2be0581965b7ff25784195ce4d46e1b24313fdd
Robert Biggs <rbiggs@ymail.com>
2018-07-29 19:54:09 -0700

    Fixed union type for VNode children.
    1. Cannot use union inside brackets. This was showing children as type `string | VNode[]` when it should have been `string[] | VNode[]`.

--- src/vnode.js
+++ src/vnode.js
@@ -4,7 +4,7 @@
  * Virtual DOM Node
  * @typedef VNode
  * @property {string | function} nodeName The string of the DOM node to create or Component constructor to render
- * @property {Array<VNode | string>} children The children of node
+ * @property {VNode[] | string[]} children The children of node
  * @property {string | number | undefined} key The key used to identify this VNode in a list
  * @property {Object.<string, any>} attributes The properties of this VNode
  */
