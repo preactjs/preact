7eea322931fc99c203c4526e59df1ec273589f3f
Andre Wiggins <andrewiggins@live.com>
2018-08-03 17:42:57 -0700

    Add benchmark for startsWith
    From developit/preact#275

--- src/diff/props.js
+++ src/diff/props.js
@@ -105,6 +105,7 @@ function setProperty(node, name, value, oldValue, isSvg) {
 
 		// }
 	}
+	// Benchmark for comparison: https://esbench.com/bench/574c954bdb965b9a00965ac6
 	// else if (name.charCodeAt(0)===111 && name.charCodeAt(1)===110) {
 	else if (name[0]==='o' && name[1]==='n') {
 		let useCapture = name !== (name=name.replace(/Capture$/, ''));
