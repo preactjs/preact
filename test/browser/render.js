f1e30cf6480ec316815b29750324072a830fdfe7
Andre Wiggins <andrewiggins@live.com>
2018-08-03 17:00:09 -0700

    Move DOM attribute test to render suite

--- test/browser/render.js
+++ test/browser/render.js
@@ -235,6 +235,12 @@ describe('render()', () => {
 		expect(scratch).to.have.property('innerHTML', '<div><input><table></table></div>', 'for undefined');
 	});
 
+	// Test for #651
+	it('should set enumerable boolean attribute', () => {
+		render(<input spellcheck={false} />, scratch);
+		expect(scratch.firstChild.spellcheck).to.equal(false);
+	});
+
 	it('should apply string attributes', () => {
 		render(<div foo="bar" data-foo="databar" />, scratch);
 
