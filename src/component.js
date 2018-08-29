eaa31557892722aa81632379358e961e57c54882
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-08-29 22:24:40 +0200

    Remove commented out code

--- src/component.js
+++ src/component.js
@@ -1,6 +1,5 @@
 import { assign } from './util';
 import { diff, flushMounts } from './diff/index';
-// import { diff, diffLevel } from './diff/index';
 
 /**
  * Base Component class. Provides `setState()` and `forceUpdate()`, which
@@ -37,10 +36,6 @@ export function Component(props, context) {
  * updated
  */
 Component.prototype.setState = function(update, callback) {
-	// if (this.prevState==null) this.prevState = assign({}, this.state);
-	// let s = this._nextState || this.state;
-	// let s = this.state;
-	// assign(this.state, update);
 
 	// only clone state when copying to nextState the first time.
 	let s = this._nextState || (this._nextState = assign({}, this.state));
@@ -53,16 +48,10 @@ Component.prototype.setState = function(update, callback) {
 	// Skip update if updater function returned null
 	if (update==null) return;
 
-	// let s = this._nextState || this.state;
-	// this._nextState = assign(assign({}, s), typeof update==='function' && update(s, this.props) || s);
-
-	// console.log(update, this._nextState);
-	// this._nextState = Object.assign({}, s, update);
 	if (callback!=null) this._renderCallbacks.push(callback);
 
 	this._force = false;
 	enqueueRender(this);
-	// if (!this._dirty && (this._dirty = true) && q.push(this)===1) (0, Component.debounce)(process);
 };
 
 /**
@@ -105,93 +94,26 @@ Component.prototype.render = function () {};
  */
 let q = [];
 
-// const defer = typeof Promise=='function' ? Promise.resolve().then.bind(Promise.resolve()) : setTimeout;
-// Component.debounce = typeof Promise=='function' ? Promise.resolve().then.bind(Promise.resolve()) : setTimeout;
-
 /**
  * Asynchronously schedule a callback
  * @type {(cb) => void}
  */
 const defer = typeof Promise=='function' ? Promise.resolve().then.bind(Promise.resolve()) : setTimeout;
-// Component.debounce = setTimeout;
 
 /**
  * Enqueue a rerender of a component
  * @param {import('./internal').Component} c The component to rerender
  */
 export function enqueueRender(c) {
-	// console.log('enqueueRender', c.id, q.length===0, c._dirty);
 	if (!c._dirty && (c._dirty = true) && q.push(c) === 1) {
-		// (0, Component.debounce)(process);
 		(Component.debounce || defer)(process);
-		// (Component.debounce || setTimeout)(process);
-		// defer(process);
-		// (Component.debounce || setTimeout)(process);
-		// (0, Component.debounce || defer)(process);
 	}
-
-	// if (!c._dirty) {
-	// 	c._dirty = true;
-	// 	if (diffLevel!==0) {
-	// 		q.push(c);
-	// 	}
-	// 	else {
-	// 		diff(c.base, c.base.parentNode, c._vnode, c._vnode, c.context);
-	// 	}
-	// }
-
-	// if (!c._dirty && (c._dirty = true) && q.push(c) === 1) {
-	// 	requestAnimationFrame(process);
-	// }
-	// if (q.push(c) === 1) setTimeout(process);
 }
 
 /** Flush the render queue by rerendering all queued components */
 function process() {
-	// console.log('process queue', q.map(c => c.context.__depth)+' ');
-
-	// requestIdleCallback( (c) => {
-	// 	let p;
-	// 	while (c.timeRemaining()>0 && (p=q.pop())) {
-	// 		if (p._dirty) p.forceUpdate();
-	// 	}
-	// 	if (q.length) process();
-	// });
-
 	let p;
 	while ((p=q.pop())) {
 		if (p._dirty) p.forceUpdate();
 	}
-
-	// let time = Date.now(), len=q.length, i=len;
-	// while (i--) {
-	// 	if (Date.now() - time > 2) break;
-	// 	if (q[i]._dirty) q[i].forceUpdate();
-	// }
-	// q.splice(i, len-i);
-	// if (i>=0) (0, Component.debounce)(process);
-
-	// while ( Date.now() - time < 5 && (p=q.pop())) {
-	// 	if (p._dirty) p.forceUpdate();
-	// }
-	// if (q.length) (0, Component.debounce)(process);
-
-	// let current = q;
-	// q = [];
-	// for (let i=current.length; i--; ) {
-	// 	if (current[i]._dirty) {
-	// 		current[i].forceUpdate();
-	// 	}
-	// }
-
-	// let current = q, p;
-	// q = [];
-	// while ((p = current.pop())) {
-	// 	if (p._dirty) {
-	// 		p.forceUpdate();
-	// 		// diff(p.base, p.base.parentNode, p._vnode, p._vnode, p.context);
-	// 	}
-	// }
 }
-
-// export { process as processQueue };
--- src/diff/props.js
+++ src/diff/props.js
@@ -9,45 +9,17 @@ import { IS_NON_DIMENSIONAL } from '../constants';
  * @param {boolean} isSvg Whether or not this node is an SVG node
  */
 export function diffProps(node, props, oldProps, isSvg) {
-	// if (props!=null) {
 	for (let i in props) {
 		if (i!=='children' && i!=='key' && (!oldProps || oldProps[i]!=props[i])) {
-		// if (i!=='children' && i!=='key' && (oldProps==null || oldProps[i]!=props[i])) {
-		// if (oldProps==null || oldProps[i]!=props[i]) {
 			setProperty(node, i, props[i], oldProps[i], isSvg);
-			// node[i] = props[i];
 		}
 	}
-	// if (oldProps!=null) {
 	for (let i in oldProps) {
 		if (i!=='children' && i!=='key' && (!props || !(i in props))) {
-		// if (i!=='children' && i!=='key' && (props==null || !(i in props))) {
-		// if (props==null || !(i in props)) {
-			// delete node[i];
 			setProperty(node, i, null, oldProps[i], isSvg);
 		}
 	}
 }
-
-// const DIMENSION_PROPS = {};
-
-// function isDimensionalProp(name) {
-// 	return DIMENSION_PROPS[name] || (DIMENSION_PROPS[name] = (IS_NON_DIMENSIONAL.test(name) === false));
-// }
-
-// let isNonDimensional = IS_NON_DIMENSIONAL.test.bind(IS_NON_DIMENSIONAL);
-
-// function setStyle(node, value, oldValue) {
-// 	for (let i in oldValue) {
-// 		if (value==null || !(i in value)) node.style[i] = '';
-// 	}
-// 	for (let i in value) {
-// 		let v = value[i];
-// 		if (oldValue==null || v!==oldValue[i]) {
-// 			node.style[i] = typeof v==='number' && IS_NON_DIMENSIONAL.test(i)===false ? (v + 'px') : v;
-// 		}
-// 	}
-// }
 
 /**
  * Set a property value on a DOM node
@@ -58,34 +30,15 @@ export function diffProps(node, props, oldProps, isSvg) {
  * @param {boolean} isSvg Whether or not this DOM node is an SVG node or not
  */
 function setProperty(node, name, value, oldValue, isSvg) {
-	// if (name==='children') return;
-
-	// let name = iname==='class' ? 'className' : iname;
 	if (name==='class' || name==='className') name = isSvg ? 'class' : 'className';
-	// let isProperty = !isSvg && name in node;
 
-	// let obj = node[name];
-
-	// if (name==='children' || name==='key') {}
-	// else
 	if (name==='style') {
-		// setStyle(node, value, oldValue);
 
 		/* Possible golfing activities for setting styles:
 		 *   - we could just drop String style values. They're not supported in other VDOM libs.
 		 *   - assigning to .style sets .style.cssText - TODO: benchmark this, might not be worth the bytes.
 		 *   - assigning also casts to String, and ignores invalid values. This means assigning an Object clears all styles.
 		 */
-		// if (typeof value==='string') {
-		// 	// node.style.cssText = value;
-		// 	node.style = value;
-		// }
-		// else {
-		// 	if (typeof oldValue==='string') {
-		// 		// node.style.cssText = '';
-		// 		node.style = '';
-		// 	}
-
 		let s = node.style;
 
 		if (typeof value==='string') return s.cssText = value;
@@ -101,27 +54,13 @@ function setProperty(node, name, value, oldValue, isSvg) {
 			let v = value[i];
 			if (oldValue==null || v!==oldValue[i]) {
 				s[i] = typeof v==='number' && IS_NON_DIMENSIONAL.test(i)===false ? (v + 'px') : v;
-				// obj[i] = v;
-
-				// if (typeof v==='number' && IS_NON_DIMENSIONAL.test(i)===false) v += 'px';
-				// obj[i] = v;
-
-				// node.style[i] = typeof v==='number' && isDimensionalProp(i) ? `${v}px` : v;
-				// node.style[i] = v;
-
-				// if (typeof v==='number' && (DIMENSION_PROPS[i] === true || (DIMENSION_PROPS[i] = IS_NON_DIMENSIONAL.test(i)===false))) {
-				// 	v += 'px';
-				// }
-				// node.style[i] = v;
 			}
 		}
-		// }
 	}
 	else if (name==='dangerouslySetInnerHTML') {
 		node.innerHTML = value && value.__html || '';
 	}
 	// Benchmark for comparison: https://esbench.com/bench/574c954bdb965b9a00965ac6
-	// else if (name.charCodeAt(0)===111 && name.charCodeAt(1)===110) {
 	else if (name[0]==='o' && name[1]==='n') {
 		let useCapture = name !== (name=name.replace(/Capture$/, ''));
 		let nameLower = name.toLowerCase();
@@ -130,41 +69,13 @@ function setProperty(node, name, value, oldValue, isSvg) {
 		node.removeEventListener(name, oldValue, useCapture);
 		node.addEventListener(name, value, useCapture);
 	}
-	// else if (isProperty===true) {
-	// else if (name in node) {
 	else if (name!=='list' && !isSvg && (name in node)) {
 		node[name] = value==null ? '' : value;
-		// if (value==null || value===false) node.removeAttribute(name);
 	}
-	// else if (name in node && (isSvg===false || obj!=null && obj.baseVal!=null && obj.baseVal.valueAsString!=null)) {
-	// 	if (isSvg===true) {
-	// 		// console.log(name, value, node[name].baseVal.unitType);
-	// 		obj.baseVal.valueAsString = typeof value==='number' ? (value + 'px') : value;
-	// 	}
-	// 	else {
-	// 		node[name] = value==null ? '' : value;
-	// 	}
-	// 	if (value==null || value===false) node.removeAttribute(name);
-	// }
 	else if (value==null || value===false) {
 		node.removeAttribute(name);
 	}
 	else if (typeof value!=='function') {
 		node.setAttribute(name, value);
 	}
-	// @TODO handle this implicitly in set?
-	// else if (value==null) {
-	// 	if (isProperty===true) {
-	// 		delete node[name];
-	// 	}
-	// 	else {
-	// 		node.removeAttribute(name);
-	// 	}
-	// }
-	// else if (isProperty===true) {
-	// 	node[name] = value;
-	// }
-	// else {
-	// 	node.setAttribute(name, value);
-	// }
 }
