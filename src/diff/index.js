5db6d2d34ddbd1b8a5dcfd8181909ae449f86995
Ryan Petrich <rpetrich@gmail.com>
2018-08-29 06:16:25 -0400

    Add comment regarding the anchor text node that is created to allow rerendering failed components

--- src/diff/index.js
+++ src/diff/index.js
@@ -344,6 +344,7 @@ export function diff(dom, parent, newTree, oldTree, context, isSvg, append, exce
 	} catch (e) {
 		if (c && !dom) {
 			// Create an "anchor" into which we can rerender upon recovery even though the component doesn't have a proper tree to render
+			// This is required because forceUpdate doesn't diff if c.base is null
 			parent.appendChild(c.base = dom = document.createTextNode(''));
 		}
 		catchErrorInComponent(e, ancestorComponent);
