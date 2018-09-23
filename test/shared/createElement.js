8c675029eba700837b7926adf9aedcb367b1098a
Andre Wiggins <andrewiggins@live.com>
2018-09-22 23:23:32 -0700

    Rename h tests to createElement & combine createElement tests

--- test/shared/createElement.js
+++ test/shared/createElement.js
@@ -13,7 +13,7 @@ import { expect } from 'chai';
 // 	key: attributes && attributes.key
 // });
 
-describe('h(jsx)', () => {
+describe('createElement(jsx)', () => {
 	it('should return a VNode', () => {
 		let r;
 		expect( () => r = h('foo') ).not.to.throw();
@@ -22,6 +22,40 @@ describe('h(jsx)', () => {
 		expect(r).to.have.property('tag', 'foo');
 		expect(r).to.have.property('props').that.eql({});
 		// expect(r).to.have.deep.property('props.children').that.eql(null);
+	});
+
+	it('should set VNode#tag property', () => {
+		expect(<div />).to.have.property('tag', 'div');
+		function Test() {
+			return <div />;
+		}
+		expect(<Test />).to.have.property('tag', Test);
+	});
+
+	it('should set VNode#props property', () => {
+		const props = {};
+		expect(h('div', props)).to.have.property('props', props);
+	});
+
+	it('should set VNode#text property', () => {
+		expect(<div />).to.have.property('text', null);
+	});
+
+	it('should set VNode#key property', () => {
+		expect(<div />).to.have.property('key').that.is.undefined;
+		expect(<div a="a" />).to.have.property('key').that.is.undefined;
+		expect(<div key="1" />).to.have.property('key', '1');
+	});
+
+	it('should set VNode#ref property', () => {
+		expect(<div />).to.have.property('ref').that.is.undefined;
+		expect(<div a="a" />).to.have.property('ref').that.is.undefined;
+		const emptyFunction = () => {};
+		expect(<div ref={emptyFunction} />).to.have.property('ref', emptyFunction);
+	});
+
+	it('should have ordered VNode properties', () => {
+		expect(Object.keys(<div />).filter(key => !/^_/.test(key))).to.deep.equal(['tag', 'props', 'text', 'key', 'ref']);
 	});
 
 	it('should preserve raw props', () => {
