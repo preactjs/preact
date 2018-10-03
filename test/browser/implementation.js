f96d3d2c5d6825d05594907ae7b4587c5831a910
Kevin Doughty <kvndy@protonmail.com>
2018-10-03 16:35:48 -0400

    props and context should only be set once failing tests

--- test/browser/implementation.js
+++ test/browser/implementation.js
@@ -147,4 +147,239 @@ describe('implementation', () => {
 			expect(Inner.prototype._setPrivateState).to.have.been.calledTwice;
 		});
 	});
+
+	describe('props', () => {
+
+		let scratch;
+
+		before( () => {
+			scratch = document.createElement('div');
+			(document.body || document.documentElement).appendChild(scratch);
+		});
+
+		beforeEach( () => {
+			let c = scratch.firstElementChild;
+			if (c) render(<Empty />, scratch, c);
+			scratch.innerHTML = '';
+		});
+
+		after( () => {
+			scratch.parentNode.removeChild(scratch);
+			scratch = null;
+		});
+
+		it('after setState, should only set props once, not set then unset for lifecycle methods then set again', (done) => {
+			let instance;
+
+			class Inner extends Component {
+				constructor(props) {
+					super(props);
+					this._privateProps = props;
+					Object.defineProperty(this, 'props', {
+						get: this._getPrivateProps,
+						set: this._setPrivateProps,
+						enumerable: true,
+						configurable: false
+					});
+				}
+				_setPrivateProps(props) {
+					this._privateProps = props;
+				}
+				_getPrivateProps() {
+					return this._privateProps;
+				}
+				render(props) {
+					return <div {...props} />;
+				}
+			}
+
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { value: 0 };
+				}
+				componentDidUpdate() {
+					expect(Inner.prototype._setPrivateProps).to.have.been.calledTwice;
+					done();
+				}
+				render(_,state) {
+					return (
+						<Inner {...state} />
+					);
+				}
+			}
+			sinon.spy(Inner.prototype, '_setPrivateProps');
+			render(<Outer ref={c=>instance=c} />, scratch);
+			instance.setState({ value: instance.state.value + 1 });
+		});
+
+		it('after forced render, should only set props once, not set then unset for lifecycle methods then set again', () => {
+			let instance;
+
+			class Inner extends Component {
+				constructor(props) {
+					super(props);
+					this._privateProps = props;
+					Object.defineProperty(this, 'props', {
+						get: this._getPrivateProps,
+						set: this._setPrivateProps,
+						enumerable: true,
+						configurable: false
+					});
+				}
+				_setPrivateProps(props) {
+					this._privateProps = props;
+				}
+				_getPrivateProps() {
+					return this._privateProps;
+				}
+				render(props) {
+					return <div {...props} />;
+				}
+			}
+
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { value: 0 };
+				}
+				render(_,state) {
+					return (
+						<Inner {...state} />
+					);
+				}
+			}
+
+			sinon.spy(Inner.prototype, '_setPrivateProps');
+			render(<Outer ref={c=>instance=c} />, scratch);
+
+			instance.setState({ value: instance.state.value + 1 });
+			instance.forceUpdate();
+
+			expect(Inner.prototype._setPrivateProps).to.have.been.calledTwice;
+		});
+
+	});
+
+	describe('context', () => {
+
+		let scratch;
+
+		before( () => {
+			scratch = document.createElement('div');
+			(document.body || document.documentElement).appendChild(scratch);
+		});
+
+		beforeEach( () => {
+			let c = scratch.firstElementChild;
+			if (c) render(<Empty />, scratch, c);
+			scratch.innerHTML = '';
+		});
+
+		after( () => {
+			scratch.parentNode.removeChild(scratch);
+			scratch = null;
+		});
+
+		it('after setState, should only set context once, not set then unset for lifecycle methods then set again', (done) => {
+			let instance;
+			const CONTEXT = { a:'a' };
+
+			class Inner extends Component {
+				constructor(props, context) {
+					super(props, context);
+					this._privateContext = context;
+					Object.defineProperty(this, 'context', {
+						get: this._getPrivateContext,
+						set: this._setPrivateContext,
+						enumerable: true,
+						configurable: false
+					});
+				}
+				_setPrivateContext(context) {
+					this._privateContext = context;
+				}
+				_getPrivateContext() {
+					return this._privateContext;
+				}
+				render(props) {
+					return <div {...props} />;
+				}
+			}
+
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { value: 0 };
+				}
+				getChildContext() {
+					return CONTEXT;
+				}
+				componentDidUpdate() {
+					expect(Inner.prototype._setPrivateContext).to.have.been.calledTwice;
+					done();
+				}
+				render(_,state) {
+					return (
+						<Inner {...state} />
+					);
+				}
+			}
+			sinon.spy(Inner.prototype, '_setPrivateContext');
+			render(<Outer ref={c=>instance=c} />, scratch);
+			instance.setState({ value: instance.state.value + 1 });
+		});
+
+		it('after forced render, should only set context once, not set then unset for lifecycle methods then set again', () => {
+			let instance;
+			const CONTEXT = { a:'a' };
+
+			class Inner extends Component {
+				constructor(props,context) {
+					super(props,context);
+					this._privateContext = context;
+					Object.defineProperty(this, 'context', {
+						get: this._getPrivateContext,
+						set: this._setPrivateContext,
+						enumerable: true,
+						configurable: false
+					});
+				}
+				_setPrivateContext(context) {
+					this._privateContext = context;
+				}
+				_getPrivateContext() {
+					return this._privateContext;
+				}
+				render(props) {
+					return <div {...props} />;
+				}
+			}
+
+			class Outer extends Component {
+				constructor() {
+					super();
+					this.state = { value: 0 };
+				}
+				getChildContext() {
+					return CONTEXT;
+				}
+				render(_,state) {
+					return (
+						<Inner {...state} />
+					);
+				}
+			}
+
+			sinon.spy(Inner.prototype, '_setPrivateContext');
+			render(<Outer ref={c=>instance=c} />, scratch);
+
+			instance.setState({ value: instance.state.value + 1 });
+			instance.forceUpdate();
+
+			expect(Inner.prototype._setPrivateContext).to.have.been.calledTwice;
+		});
+
+	});
+
 });
