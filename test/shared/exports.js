ef5fbb7943e3890204d913ab91fb9bfe54d7fad4
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-10-27 13:49:43 +0200

    Remove unused imports

--- test/shared/exports.js
+++ test/shared/exports.js
@@ -1,19 +1,7 @@
-import preact, { createElement, Component, Fragment, render, hydrate, cloneElement } from '../../src/index';
+import { createElement, Component, Fragment, render, hydrate, cloneElement } from '../../src/index';
 import { expect } from 'chai';
 
 describe('preact', () => {
-	it('should be available as a default export', () => {
-		expect(preact).to.be.an('object');
-		expect(preact).to.have.property('createElement', createElement);
-		expect(preact).to.have.property('Component', Component);
-		expect(preact).to.have.property('Fragment', Fragment);
-		expect(preact).to.have.property('render', render);
-		expect(preact).to.have.property('hydrate', hydrate);
-		expect(preact).to.have.property('cloneElement', cloneElement);
-		// expect(preact).to.have.property('rerender', rerender);
-		// expect(preact).to.have.property('options', options);
-	});
-
 	it('should be available as named exports', () => {
 		expect(createElement).to.be.a('function');
 		expect(Component).to.be.a('function');
