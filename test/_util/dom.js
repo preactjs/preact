a1c2438683242d47d055b7ecfeeeff5cb541e14e
Andre Wiggins <andrewiggins@live.com>
2018-11-09 09:18:58 -0800

    Add hydrate tests

---
+++ test/_util/dom.js
@@ -0,0 +1,23 @@
+/**
+ * A helper to generate innerHTML validation strings containing spans
+ * @param {string} contents The contents of the span, as a string
+ */
+export const span = contents => `<span>${contents}</span>`;
+
+/**
+ * A helper to generate innerHTML validation strings containing divs
+ * @param {string} contents The contents of the div, as a string
+ */
+export const div = contents => `<div>${contents}</div>`;
+
+/**
+ * A helper to generate innerHTML validation strings containing uls
+ * @param {string} contents The contents of the ul, as a string
+ */
+export const ul = contents => `<ul>${contents}</ul>`;
+
+/**
+ * A helper to generate innerHTML validation strings containing uls
+ * @param {string} contents The contents of the ul, as a string
+ */
+export const li = contents => `<li>${contents}</li>`;
