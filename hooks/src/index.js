cbdda0d77dc0a723ea7ee95288887a0782d8e40b
Andre Wiggins <andrewiggins@live.com>
2019-01-16 19:30:26 -0800

    Fix SSR to include MessageChannel & avoid serialization when using postMessage (810 B)

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,8 +1,5 @@
 import { options } from 'preact';
 
-const hasWindow = typeof window !== 'undefined';
-const mc = new MessageChannel();
-
 /** @type {number} */
 let currentIndex;
 
@@ -217,21 +214,21 @@ export const useReducer = createHook((hook, component, reducer, initialState, in
 });
 
 // eslint-disable-next-line arrow-body-style
-export const useEffect = hasWindow ? createHook((hook, component) => {
+export const useEffect = createHook((hook, component) => {
 	return callback => {
 		component.__hooks._pendingEffects.push(hook);
 		afterPaint(component);
 		return callback;
 	};
-}, propsChanged) : noop;
+}, propsChanged);
 
 // eslint-disable-next-line arrow-body-style
-export const useLayoutEffect = hasWindow ? createHook((hook, component) => {
+export const useLayoutEffect = createHook((hook, component) => {
 	return callback => {
 		component.__hooks._pendingLayoutEffects.push(hook);
 		return callback;
 	};
-}, propsChanged) : noop;
+}, propsChanged);
 
 export const useRef = createHook((hook, component, initialValue) => {
 	const ref = { current: initialValue };
@@ -247,82 +244,93 @@ export const useCallback = createHook(() => callback => callback, propsChanged);
 // Perhaps this is not such a big deal.
 /**
  * Invoke a component's pending effects after the next frame renders
- * @param {import('./internal').Component} args
+ * @type {(component: import('./internal').Component) => void}
  */
-function afterPaint(args) {
-	if (afterPaintEffects.push(args) === 1) {
-		requestAnimationFrame(onPaint);
-	}
-}
+let afterPaint = () => {};
+
+/** @type {MessageChannel} */
+let mc;
 
 function onPaint() {
-	mc.port1.postMessage(0);
+	mc.port1.postMessage(undefined);
 }
 
-mc.port2.onmessage = () => {
-	// !Drain Queue
+if (typeof window !== 'undefined') {
+	mc = new MessageChannel();
 
-	// ORIGINAL: 874 B
-	// afterPaintEffects.splice(0, afterPaintEffects.length).forEach(component => {
-	// 	if (!component._parentDom) return;
-	// 	const effects = component.__hooks._pendingEffects;
-	// 	// !Drain Queue 2
-	// 	effects.splice(0, effects.length).forEach(invokeEffect);
-	// });
+	afterPaint = (component) => {
+		// TODO: Consider ways to avoid queuing a component multiple times
+		// due to multiple `useEffect`s
+		if (afterPaintEffects.push(component) === 1) {
+			requestAnimationFrame(onPaint);
+		}
+	};
 
-	// WHILE (SHIFT): 855 B  ❌ if we don't support sync Component.debounce. See 852 B comment below.
-	// let c, effect;
-	// while (c = afterPaintEffects.shift()) {
-	// 	if (!c._parentDom) continue;
-	// 	while (effect = c.__hooks._pendingEffects.shift()) {
-	// 		invokeEffect(effect);
-	// 	}
-	// }
+	mc.port2.onmessage = () => {
+		// !Drain Queue
 
-	// WHILE (POP): 853 B  ❌ if we don't support sync Component.debounce. See 852 B comment below.
-	// let c, effect;
-	// while (c = afterPaintEffects.pop()) {
-	// 	if (!c._parentDom) continue;
-	// 	while (effect = c.__hooks._pendingEffects.pop()) {
-	// 		invokeEffect(effect);
-	// 	}
-	// }
+		// ORIGINAL: 874 B
+		// afterPaintEffects.splice(0, afterPaintEffects.length).forEach(component => {
+		// 	if (!component._parentDom) return;
+		// 	const effects = component.__hooks._pendingEffects;
+		// 	// !Drain Queue 2
+		// 	effects.splice(0, effects.length).forEach(invokeEffect);
+		// });
 
-	// FOR (TEMP): 886 B
-	// let list1 = afterPaintEffects; afterPaintEffects = [];
-	// for (j = 0; j < list1.length; j++) {
-	// 	currentComponent = list1[j];
-	// 	if (!currentComponent._parentDom) continue;
-	// 	let list2 = currentComponent.__hooks._pendingEffects; currentComponent.__hooks._pendingEffects = [];
-	// 	for (i = 0; i < list2.length; i++) {
-	// 		invokeEffect(list2[i]);
-	// 	}
-	// }
+		// WHILE (SHIFT): 855 B  ❌ if we don't support sync Component.debounce. See 852 B comment below.
+		// let c, effect;
+		// while (c = afterPaintEffects.shift()) {
+		// 	if (!c._parentDom) continue;
+		// 	while (effect = c.__hooks._pendingEffects.shift()) {
+		// 		invokeEffect(effect);
+		// 	}
+		// }
 
-	// FOREACH (SPLICE): 837 B
-	// afterPaintEffects.splice(0, afterPaintEffects.length).forEach(component => {
-	// 	if (!component._parentDom) return;
-	// 	component.__hooks._pendingEffects.splice(0, component.__hooks._pendingEffects.length).forEach(invokeEffect);
-	// });
+		// WHILE (POP): 853 B  ❌ if we don't support sync Component.debounce. See 852 B comment below.
+		// let c, effect;
+		// while (c = afterPaintEffects.pop()) {
+		// 	if (!c._parentDom) continue;
+		// 	while (effect = c.__hooks._pendingEffects.pop()) {
+		// 		invokeEffect(effect);
+		// 	}
+		// }
 
-	// FOREACH (TEMP): 832 B
-	afterPaintEffects.forEach(component => {
-		if (!component._parentDom) return;
-		let effects = component.__hooks._pendingEffects; component.__hooks._pendingEffects = [];
-		effects.forEach(invokeEffect);
-	});
-	afterPaintEffects = [];
+		// FOR (TEMP): 886 B
+		// let list1 = afterPaintEffects; afterPaintEffects = [];
+		// for (j = 0; j < list1.length; j++) {
+		// 	currentComponent = list1[j];
+		// 	if (!currentComponent._parentDom) continue;
+		// 	let list2 = currentComponent.__hooks._pendingEffects; currentComponent.__hooks._pendingEffects = [];
+		// 	for (i = 0; i < list2.length; i++) {
+		// 		invokeEffect(list2[i]);
+		// 	}
+		// }
 
-	// FOREACH (RESET AFTER): 829 B ❌
-	// Doesn't pass sync debounce test cuz we need to clear the _pendingEffects
-	// list before traversing it to avoid re-entering effects in the beforeRender loop
-	// afterPaintEffects.forEach(component => {
-	// 	if (!component._parentDom) return;
-	// 	component.__hooks._pendingEffects.forEach(invokeEffect);
-	// 	component.__hooks._pendingEffects = [];
-	// });
-	// afterPaintEffects = [];
-};
+		// FOREACH (SPLICE): 837 B
+		// afterPaintEffects.splice(0, afterPaintEffects.length).forEach(component => {
+		// 	if (!component._parentDom) return;
+		// 	component.__hooks._pendingEffects.splice(0, component.__hooks._pendingEffects.length).forEach(invokeEffect);
+		// });
+
+		// FOREACH (TEMP): 832 B
+		afterPaintEffects.forEach(component => {
+			if (!component._parentDom) return;
+			let effects = component.__hooks._pendingEffects; component.__hooks._pendingEffects = [];
+			effects.forEach(invokeEffect);
+		});
+		afterPaintEffects = [];
+
+		// FOREACH (RESET AFTER): 829 B ❌
+		// Doesn't pass sync debounce test cuz we need to clear the _pendingEffects
+		// list before traversing it to avoid re-entering effects in the beforeRender loop
+		// afterPaintEffects.forEach(component => {
+		// 	if (!component._parentDom) return;
+		// 	component.__hooks._pendingEffects.forEach(invokeEffect);
+		// 	component.__hooks._pendingEffects = [];
+		// });
+		// afterPaintEffects = [];
+	};
+}
 
 /**
  * Invoke a Hook's effect
@@ -382,5 +390,3 @@ function memoChanged(oldArgs, newArgs) {
 function invokeOrReturn(arg, f) {
 	return typeof f === 'function' ? f(arg) : f;
 }
-
-function noop() {}
