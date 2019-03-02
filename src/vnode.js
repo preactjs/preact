bcd323cdeb169ea2af16131f01876301e6bdb895
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-03-02 22:33:46 +0100

    Remove 8.x code

--- src/vnode.js
+++
@@ -1,9 +0,0 @@
-/**
- * Virtual DOM Node
- * @typedef VNode
- * @property {string | function} nodeName The string of the DOM node to create or Component constructor to render
- * @property {Array<VNode | string>} children The children of node
- * @property {string | number | undefined} key The key used to identify this VNode in a list
- * @property {object} attributes The properties of this VNode
- */
-export const VNode = function VNode() {};
