e6f8e0c59ac299127d316e5537f25007e15eb8fd
Jovi De Croock <jovi@codifly.be>
2019-03-10 15:41:41 +0100

    finished feature

--- test-utils/test/shared/act.js
+++ test-utils/test/shared/act.js
@@ -1,9 +1,22 @@
-import { options } from 'preact';
+import { options, createElement as h, render } from 'preact';
 import { useState } from 'preact/hooks';
 
+import { setupScratch, teardown } from '../../../test/_util/helpers';
 import { act } from '../../src';
 
+/** @jsx h */
 describe('act', () => {
+
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	beforeEach(() => {
+		scratch = setupScratch();
+	});
+
+	afterEach(() => {
+		teardown(scratch);
+	});
 
 	it('should reset options after act finishes', () => {
 		expect(options.afterPaint).to.equal(undefined);
@@ -14,6 +27,21 @@ describe('act', () => {
 	});
 
 	it('should drain the queue of hooks', () => {
-		expect(true).to.be.true();
+		function StateContainer() {
+			const [count, setCount] = useState(0);
+			return (<div>
+				<p>Count: {count}</p>
+				<button onClick={() => setCount(c => c + 11)} />
+			</div>);
+		}
+
+		render(<StateContainer />, scratch);
+		expect(scratch.textContent).to.include('Count: 0');
+		act(() => {
+			const button = scratch.querySelector('button');
+			button.click();
+			expect(scratch.textContent).to.include('Count: 0');
+		});
+		expect(scratch.textContent).to.include('Count: 1');
 	});
 });
