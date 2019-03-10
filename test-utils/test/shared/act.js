6f8b0294ad60d69d415d030ea456b7b25cd8a484
Jovi De Croock <jovi@codifly.be>
2019-03-10 15:21:37 +0100

    progress

--- test-utils/test/shared/act.js
+++ test-utils/test/shared/act.js
@@ -5,7 +5,11 @@ import { act } from '../../src';
 describe('act', () => {
 
 	it('should reset options after act finishes', () => {
-		expect(options.afterPaint).to.be.undefined();
+		expect(options.afterPaint).to.equal(undefined);
+		act(() => {
+			console.log('testing');
+		});
+		expect(options.afterPaint).to.equal(undefined);
 	});
 
 });
