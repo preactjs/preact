43354a986530b8e3e6194561173930ed91c08f27
Jovi De Croock <jovi@codifly.be>
2019-03-11 07:15:59 +0100

    test the flush callback

--- test-utils/test/shared/act.test.js
+++ test-utils/test/shared/act.test.js
@@ -1,9 +1,9 @@
 import { options, createElement as h, render } from 'preact';
 import sinon from 'sinon';
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 
 import { setupScratch, teardown } from '../../../test/_util/helpers';
-import { act, setupRerender } from '../../src';
+import { act } from '../../src';
 
 /** @jsx h */
 describe('act', () => {
@@ -44,17 +44,15 @@ describe('act', () => {
 		expect(scratch.textContent).to.include('Count: 1');
 	});
 
-	it('should call flush pending effects', () => {
+	it('should flush pending effects', () => {
+		let spy = sinon.spy();
 		function StateContainer() {
-			const [count, setCount] = useState(0);
-			return (<div>
-				<p>Count: {count}</p>
-				<button onClick={() => setCount(c => c + 11)} />
-			</div>);
+			useEffect(spy);
+			return <div />;
 		}
 
 		act(() => render(<StateContainer />, scratch));
-		expect(scratch.textContent).to.include('Count: 0');
+		expect(spy).to.be.calledOnce;
 	});
 
 	it('should restore options.requestAnimationFrame', () => {
