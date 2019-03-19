e79d5d46420fdedba60cba85c7f3367d030d4c13
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-03-19 15:54:17 +0100

    Add array find + includes polyfill

--- test/polyfills.js
+++ test/polyfills.js
@@ -3,6 +3,8 @@ import 'core-js/es6/map';
 import 'core-js/es6/promise';
 import 'core-js/fn/array/fill';
 import 'core-js/fn/array/from';
+import 'core-js/fn/array/find';
+import 'core-js/fn/array/includes';
 import 'core-js/fn/object/assign';
 
 // Fix Function#name on browsers that do not support it (IE).
