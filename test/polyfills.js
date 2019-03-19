f2688064c91bbf7d0750394530622ee215a43ff8
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-03-19 15:19:17 +0100

    Add Promise polyfill for IE11

--- test/polyfills.js
+++ test/polyfills.js
@@ -1,5 +1,6 @@
 // ES2015 APIs used by developer tools integration
 import 'core-js/es6/map';
+import 'core-js/es6/promise';
 import 'core-js/fn/array/fill';
 import 'core-js/fn/array/from';
 import 'core-js/fn/object/assign';
