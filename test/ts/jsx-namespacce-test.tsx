f24669ff884f903875ed69c98a7bc822a4d43cc4
just-boris <just-boris@hotmail.com>
2019-03-24 14:58:49 +0100

    move JSX namespace into preact one
    closes #1036

---
+++ test/ts/jsx-namespacce-test.tsx
@@ -0,0 +1,16 @@
+import { createElement, Component } from "../../src";
+
+// declare global JSX types that should not be mixed with preact's internal types
+declare global {
+	namespace JSX {
+		interface Element {
+			unknownProperty: string
+		}
+	}
+}
+
+class SimpleComponent extends Component {
+	render() {
+		return <div>It works</div>;
+	}
+}
