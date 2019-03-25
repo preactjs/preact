15ecabc2d3bed89c2263a5ea657f10443ac2fd65
jdecroock <decroockjovi@gmail.com>
2019-03-25 17:15:42 +0100

    add failing test

--- test/browser/svg.test.js
+++ test/browser/svg.test.js
@@ -16,14 +16,14 @@ describe('svg', () => {
 
 	it('should render SVG to string', () => {
 		render((
-			<svg viewBox="0 0 360 360">
+			<svg viewBox="0 0 360 360" xlinkHref="www.preact.com">
 				<path stroke="white" fill="black" d="M 347.1 357.9 L 183.3 256.5 L 13 357.9 V 1.7 h 334.1 v 356.2 Z M 58.5 47.2 v 231.4 l 124.8 -74.1 l 118.3 72.8 V 47.2 H 58.5 Z" />
 			</svg>
 		), scratch);
 
 		let html = sortAttributes(String(scratch.innerHTML).replace(' xmlns="http://www.w3.org/2000/svg"', ''));
 		expect(html).to.equal(sortAttributes(`
-			<svg viewBox="0 0 360 360">
+			<svg viewBox="0 0 360 360" xlink:href="www.preact.com">
 				<path d="M 347.1 357.9 L 183.3 256.5 L 13 357.9 V 1.7 h 334.1 v 356.2 Z M 58.5 47.2 v 231.4 l 124.8 -74.1 l 118.3 72.8 V 47.2 H 58.5 Z" fill="black" stroke="white"></path>
 			</svg>
 		`.replace(/[\n\t]+/g,'')));
