8b2f24aad18e65ee1c3d8d1b3911fd9e3701046d
38elements <mh19820223@gmail.com>
2019-03-26 00:45:33 +0900

    Fix componentDidUpdate()

--- test/browser/lifecycle.test.js
+++ test/browser/lifecycle.test.js
@@ -1102,6 +1102,7 @@ describe('Lifecycle methods', () => {
 
 			let prevPropsArg;
 			let prevStateArg;
+			let snapshotArg;
 			let curProps;
 			let curState;
 
@@ -1122,11 +1123,12 @@ describe('Lifecycle methods', () => {
 						value: state.value + 1
 					};
 				}
-				componentDidUpdate(prevProps, prevState) {
+				componentDidUpdate(prevProps, prevState, snapshot) {
 					// These object references might be updated later so copy
 					// object so we can assert their values at this snapshot in time
 					prevPropsArg = { ...prevProps };
 					prevStateArg = { ...prevState };
+					snapshotArg = snapshot;
 
 					curProps = { ...this.props };
 					curState = { ...this.state };
@@ -1148,6 +1150,7 @@ describe('Lifecycle methods', () => {
 			expect(scratch.firstChild.textContent).to.be.equal('1');
 			expect(prevPropsArg).to.be.undefined;
 			expect(prevStateArg).to.be.undefined;
+			expect(snapshotArg).to.be.undefined;
 			expect(curProps).to.be.undefined;
 			expect(curState).to.be.undefined;
 
@@ -1157,6 +1160,7 @@ describe('Lifecycle methods', () => {
 			expect(scratch.firstChild.textContent).to.be.equal('2');
 			expect(prevPropsArg).to.deep.equal({ foo: 'foo' });
 			expect(prevStateArg).to.deep.equal({ value: 1 });
+			expect(snapshotArg).to.be.undefined;
 			expect(curProps).to.deep.equal({ foo: 'bar' });
 			expect(curState).to.deep.equal({ value: 2 });
 
@@ -1167,6 +1171,7 @@ describe('Lifecycle methods', () => {
 			expect(scratch.firstChild.textContent).to.be.equal('4');
 			expect(prevPropsArg).to.deep.equal({ foo: 'bar' });
 			expect(prevStateArg).to.deep.equal({ value: 2 });
+			expect(snapshotArg).to.be.undefined;
 			expect(curProps).to.deep.equal({ foo: 'bar' });
 			expect(curState).to.deep.equal({ value: 4 });
 		});
