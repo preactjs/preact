31237f15349dbbe8a35f4499514350a4f71af6db
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-03-31 21:17:58 +0200

    Fix stale dom when forceUpdate is called in cWRP

--- src/diff/index.js
+++ src/diff/index.js
@@ -64,6 +64,7 @@ export function diff(dom, parentDom, newVNode, oldVNode, context, isSvg, excessD
 			if (oldVNode._component) {
 				c = newVNode._component = oldVNode._component;
 				clearProcessingException = c._processingException;
+				newVNode._dom = oldVNode._dom;
 			}
 			else {
 				// Instantiate the new component
