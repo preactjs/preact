9dbbd64b42e94954c4697b39b9a34222749ac75a
Jovi De Croock <decroockjovi@gmail.com>
2019-04-02 16:49:26 +0200

    missing space

--- debug/test/browser/debug.test.js
+++ debug/test/browser/debug.test.js
@@ -126,7 +126,7 @@ describe('debug', () => {
 
 	it('should warn for useless useMemo calls', () => {
 		const App = () => {
-			const [people] = useState([40, 20,60, 80]);
+			const [people] = useState([40, 20, 60, 80]);
 			const retiredPeople = useMemo(() => people.filter(x => x >= 60));
 			const cb = useCallback(() => () => 'test');
 			return <p onClick={cb}>{retiredPeople.map(x => x)}</p>;
