d01e2dbd00816ff4fc3cb13b166ae1abcd9be87f
Jovi De Croock <decroockjovi@gmail.com>
2019-04-03 12:12:36 +0200

    refactor: don't pass vnode for now

--- src/index.d.ts
+++ src/index.d.ts
@@ -181,7 +181,7 @@ declare namespace preact {
 		/** Attach a hook that is invoked before a vnode has rendered. */
 		render?(vnode: VNode): void;
 		/** Attach a hook that is invoked after a vnode has rendered. */
-		afterRender?(vnode: VNode): void;
+		afterRender?(): void;
 		/** Attach a hook that is invoked before a hook's state is queried. */
 		preHook?(component: VNode): void;
 		/** Attach a hook that is invoked after a vnode has rendered. */
