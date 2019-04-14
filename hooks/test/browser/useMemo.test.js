a74a466f2426cc68ab9c1bcf755a92dcd1018df2
jdecroock <decroockjovi@gmail.com>
2019-04-14 13:44:07 +0200

    tests: test for equal on useMemo

--- hooks/test/browser/useMemo.test.js
+++ hooks/test/browser/useMemo.test.js
@@ -1,7 +1,7 @@
 import { setupRerender } from 'preact/test-utils';
 import { createElement as h, render } from 'preact';
 import { setupScratch, teardown } from '../../../test/_util/helpers';
-import { useMemo } from '../../src';
+import { useMemo, useState } from '../../src';
 
 /** @jsx h */
 
@@ -9,10 +9,11 @@ import { useMemo } from '../../src';
 describe('useMemo', () => {
 
 	/** @type {HTMLDivElement} */
-	let scratch;
+	let scratch, rerender;
 
 	beforeEach(() => {
 		scratch = setupScratch();
+		rerender = setupRerender();
 	});
 
 	afterEach(() => {
@@ -40,6 +41,40 @@ describe('useMemo', () => {
 
 		expect(results).to.deep.equal([2, 2, 3, 3]);
 		expect(memoFunction).to.have.been.calledTwice;
+	});
+
+	it.skip('short circuits diffing for memoized components', () => {
+		let spy = sinon.spy();
+		let spy2 = sinon.spy();
+		const X = ({ count }) => {
+			spy();
+			return <p>{count}</p>;
+		};
+
+		const Y = ({ count }) => {
+			spy2();
+			return <p>{count}</p>;
+		};
+
+		const App = ({ x }) => {
+			const y = useMemo(() => <Y count={x} />, [x]);
+			return (
+				<div>
+					<X count={x} />
+					{y}
+				</div>
+			);
+		};
+
+		render(<App x={0} />, scratch);
+		expect(spy).to.be.calledOnce;
+		expect(spy2).to.be.calledOnce;
+		render(<App x={0} />, scratch);
+		expect(spy).to.be.calledTwice;
+		expect(spy2).to.be.calledOnce;
+		render(<App x={1} />, scratch);
+		expect(spy2).to.be.calledTwice;
+		expect(spy).to.be.calledThrice;
 	});
 
 });
