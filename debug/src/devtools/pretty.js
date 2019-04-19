bb7b5170c7559c4c25fa48de85e2507d5e7eaba8
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-04-19 12:55:58 +0200

    Pretty print props and state

--- debug/src/devtools/pretty.js
+++ debug/src/devtools/pretty.js
@@ -1,5 +1,26 @@
+import { getDisplayName } from "./custom";
+
+export function getType(data) {
+	switch (typeof data) {
+		case 'string': return 'string';
+		case 'function': return 'function';
+		case 'number': return 'number';
+		case 'boolean': return 'boolean';
+		case 'symbol': return 'symbol';
+		case 'object': {
+			if (data==null) return null;
+			if (data._dom!==undefined) return 'vnode';
+			if (typeof data.getMonth=='function') return 'date';
+			return 'object';
+		}
+		default:
+			if (Array.isArray(data)) return 'array';
+			return null;
+	}
+}
+
 export function prettify(data, cleaned, path, level) {
-	let type;
+	let type = getType(data);
 
 	switch (type) {
 		case 'function':
@@ -28,6 +49,12 @@ export function prettify(data, cleaned, path, level) {
 			}
 			return res;
 		}
+		case 'vnode':
+			cleaned.push(path);
+			return {
+				name: getDisplayName(data),
+				type: 'react_element'
+			};
 		default:
 			return data;
 	}
