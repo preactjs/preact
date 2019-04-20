f93ba79c965dc6e571de1140ee2efa2dcc652f52
Jovi De Croock <decroockjovi@gmail.com>
2019-04-20 22:44:23 +0200

    feat: useImperativeHandle

---
+++ hooks/test/browser/useImperativeHandle.test.js
@@ -0,0 +1,52 @@
+import { createElement as h, render } from 'preact';
+import { setupScratch, teardown } from '../../../test/_util/helpers';
+import { useImperativeHandle, useRef } from '../../src';
+
+/** @jsx h */
+
+
+describe('useImperativeHandle', () => {
+
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	beforeEach(() => {
+		scratch = setupScratch();
+	});
+
+	afterEach(() => {
+		teardown(scratch);
+	});
+
+	it('Mutates given ref', () => {
+		let ref;
+
+		function Comp() {
+			ref = useRef({});
+			useImperativeHandle(ref, () => ({ test: () => 'test' }), []);
+			return <p>Test</p>;
+		}
+
+		render(<Comp />, scratch);
+		expect(ref.current).to.have.property('test');
+		expect(ref.current.test()).to.equal('test');
+	});
+
+	it('Updates given ref with args', () => {
+		let ref;
+
+		function Comp({ a }) {
+			ref = useRef({});
+			useImperativeHandle(ref, () => ({ test: () => 'test' + a }), [a]);
+			return <p>Test</p>;
+		}
+
+		render(<Comp a={0} />, scratch);
+		expect(ref.current).to.have.property('test');
+		expect(ref.current.test()).to.equal('test0');
+
+		render(<Comp a={1} />, scratch);
+		expect(ref.current).to.have.property('test');
+		expect(ref.current.test()).to.equal('test1');
+	});
+});
