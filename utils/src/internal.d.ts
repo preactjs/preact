0380cea87cd63b9569c90c1ff71de4e202646e0e
jdecroock <decroockjovi@gmail.com>
2019-04-22 09:13:56 +0200

    remove types (tentative)

--- utils/src/internal.d.ts
+++
@@ -1,7 +0,0 @@
-import { FunctionalComponent } from "../../src/internal";
-
-function EqualityFunction(currentProps: object, nextProps: object): boolean;
-
-export function memo(component: FunctionalComponent, isEqual?: EqualityFunction): FunctionalComponent;
-
-// TODO: PureComponent and createPortal
