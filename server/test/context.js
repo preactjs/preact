df3f3957ae6322fdfe5ae40e9b8169d9d51ca8f8
Sven Tschui <github@sventschui.ch>
2019-05-01 20:36:55 +0200

    Support createContext of preact x

---
+++ server/test/context.js
@@ -0,0 +1,33 @@
+import render from '../src/jsx';
+import { h, createContext } from 'preact';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+// tag to remove leading whitespace from tagged template literal
+function dedent([str]) {
+	return str.split( '\n'+str.match(/^\n*(\s+)/)[1] ).join('\n').replace(/(^\n+|\n+\s*$)/g, '');
+}
+
+describe('context', () => {
+	let renderJsx = (jsx, opts) => render(jsx, null, opts).replace(/ {2}/g, '\t');
+
+	it('should support createContext', () => {
+		const { Provider, Consumer } = createContext();
+		let rendered = renderJsx(
+			<Provider value="correct">
+				<Consumer>
+					{(value) => (
+						<section>
+							value is: {value}
+						</section>
+					)}
+				</Consumer>
+			</Provider>
+		);
+
+		expect(rendered).to.equal(dedent`
+			<section>value is: correct</section>
+		`);
+	});
+});
