bb160012bf24c4dc688399ab144ebed934bd10b8
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-05-07 08:35:11 -0700

    Remove namespace

--- compat/src/index.d.ts
+++ compat/src/index.d.ts
@@ -1,50 +1,45 @@
-import * as _hooks from '../../hooks';
 import * as preact from '../../src';
 import { ForwardFn } from './internal';
 
-export = compat;
-export as namespace compat;
-declare namespace compat {
-  export import hooks = _hooks;
-  export import Component = preact.Component;
-  export import createContext = preact.createContext;
-  export import createRef = preact.createRef;
-  export import Fragment = preact.Fragment;
-  export import createElement = preact.createElement
-  export import cloneElement = preact.cloneElement
+export * from '../../hooks';
+export import Component = preact.Component;
+export import createContext = preact.createContext;
+export import createRef = preact.createRef;
+export import Fragment = preact.Fragment;
+export import createElement = preact.createElement
+export import cloneElement = preact.cloneElement
 
-  export const version: string;
+export const version: string;
 
-  export function createPortal(vnode: preact.VNode, container: Element): preact.VNode<any>;
+export function createPortal(vnode: preact.VNode, container: Element | Element): preact.VNode<any>;
 
-  function render(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
+export function render(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
 
-  function hydrate(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
+export function hydrate(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
 
-  function unmountComponentAtNode(container: Element | Document | ShadowRoot | DocumentFragment): boolean;
+export function unmountComponentAtNode(container: Element | Document | ShadowRoot | DocumentFragment): boolean;
 
-  function createFactory(type: preact.VNode["type"]): preact.VNode<{}>;
+export function createFactory(type: preact.VNode["type"]): preact.VNode<{}>;
 
-  function isValidElement(element: any): boolean;
+export function isValidElement(element: any): boolean;
 
-  function findDOMNode(component: preact.Component): Element | null;
+export function findDOMNode(component: preact.Component): Element | null;
 
-  interface PureComponent<P = {}, S = {}> extends preact.Component {
-    isPureReactComponenet: boolean;
-  }
+export interface PureComponent<P = {}, S = {}> extends preact.Component {
+  isPureReactComponenet: boolean;
+}
 
-  function memo<P = {}>(component: preact.FunctionalComponent<P>, comparer?: (prev: P, next: P) => boolean): preact.FunctionComponent<P>;
+export function memo<P = {}>(component: preact.FunctionalComponent<P>, comparer?: (prev: P, next: P) => boolean): preact.FunctionComponent<P>;
 
-  function forwardRef<P = {}>(fn: ForwardFn<P, any>): preact.FunctionalComponent<P>;
+export function forwardRef<P = {}>(fn: ForwardFn<P, any>): preact.FunctionalComponent<P>;
 
-  function unstable_batchedUpdates(callback: (arg?: any) => void, arg?: any): void;
+export function unstable_batchedUpdates(callback: (arg?: any) => void, arg?: any): void;
 
 
-  interface Children {
-    map<T extends preact.ComponentChild, R>(children: T | T[], fn: (child: T, i: number, array: T[]) => R): R[];
-    forEach<T extends preact.ComponentChild>(children: T | T[], fn: (child: T, i: number, array: T[]) => void): void;
-    count: (children: preact.ComponentChildren) => number;
-    only: (children: preact.ComponentChildren) => preact.ComponentChild;
-    toArray: (children: preact.ComponentChildren) => preact.VNode<{}>[];
-  }
+export interface Children {
+  map<T extends preact.ComponentChild, R>(children: T | T[], fn: (child: T, i: number, array: T[]) => R): R[];
+  forEach<T extends preact.ComponentChild>(children: T | T[], fn: (child: T, i: number, array: T[]) => void): void;
+  count: (children: preact.ComponentChildren) => number;
+  only: (children: preact.ComponentChildren) => preact.ComponentChild;
+  toArray: (children: preact.ComponentChildren) => preact.VNode<{}>[];
 }
