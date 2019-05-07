e7286df1c9e7479c27352c9f491c0a773fbefef8
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-05-07 13:06:04 -0700

    Add default export.

--- compat/src/index.d.ts
+++ compat/src/index.d.ts
@@ -1,3 +1,4 @@
+import * as _hooks from '../../hooks';
 import * as preact from '../../src';
 import { ForwardFn } from './internal';
 
@@ -9,37 +10,62 @@ export import Fragment = preact.Fragment;
 export import createElement = preact.createElement
 export import cloneElement = preact.cloneElement
 
-export const version: string;
+export declare const version: string;
 
-export function createPortal(vnode: preact.VNode, container: Element | Element): preact.VNode<any>;
+export declare function createPortal(vnode: preact.VNode, container: Element | Element): preact.VNode<any>;
 
-export function render(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
+export declare function render(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
 
-export function hydrate(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
+export declare function hydrate(vnode: preact.VNode<any>, parent: Element, callback?: () => void): Component | null;
 
-export function unmountComponentAtNode(container: Element | Document | ShadowRoot | DocumentFragment): boolean;
+export declare function unmountComponentAtNode(container: Element | Document | ShadowRoot | DocumentFragment): boolean;
 
-export function createFactory(type: preact.VNode["type"]): preact.VNode<{}>;
+export declare function createFactory(type: preact.VNode["type"]): preact.VNode<{}>;
 
-export function isValidElement(element: any): boolean;
+export declare function isValidElement(element: any): boolean;
 
-export function findDOMNode(component: preact.Component): Element | null;
+export declare function findDOMNode(component: preact.Component): Element | null;
 
-export interface PureComponent<P = {}, S = {}> extends preact.Component {
+export declare interface PureComponent<P = {}, S = {}> extends preact.Component {
   isPureReactComponenet: boolean;
 }
 
-export function memo<P = {}>(component: preact.FunctionalComponent<P>, comparer?: (prev: P, next: P) => boolean): preact.FunctionComponent<P>;
+export declare function memo<P = {}>(component: preact.FunctionalComponent<P>, comparer?: (prev: P, next: P) => boolean): preact.FunctionComponent<P>;
 
-export function forwardRef<P = {}>(fn: ForwardFn<P, any>): preact.FunctionalComponent<P>;
+export declare function forwardRef<P = {}>(fn: ForwardFn<P, any>): preact.FunctionalComponent<P>;
 
-export function unstable_batchedUpdates(callback: (arg?: any) => void, arg?: any): void;
+export declare function unstable_batchedUpdates(callback: (arg?: any) => void, arg?: any): void;
 
 
-export interface Children {
+export declare interface Children {
   map<T extends preact.ComponentChild, R>(children: T | T[], fn: (child: T, i: number, array: T[]) => R): R[];
   forEach<T extends preact.ComponentChild>(children: T | T[], fn: (child: T, i: number, array: T[]) => void): void;
   count: (children: preact.ComponentChildren) => number;
   only: (children: preact.ComponentChildren) => preact.ComponentChild;
   toArray: (children: preact.ComponentChildren) => preact.VNode<{}>[];
 }
+
+declare const _default: {
+  hooks: typeof _hooks,
+  Component: Component,
+  createContext: typeof createContext,
+  createRef: typeof createRef,
+  Fragment: typeof Fragment,
+  createElement: typeof createElement,
+  cloneElement: typeof cloneElement,
+  version: typeof version,
+  createPortal: typeof createPortal,
+  render: typeof render,
+  hydrate: typeof render,
+  unmountComponentAtNode: typeof unmountComponentAtNode,
+  createFactory: typeof createFactory,
+  isValidElement: typeof isValidElement,
+  findDOMNode: typeof findDOMNode,
+  PureComponent: PureComponent,
+  memo: typeof memo,
+  forwardRef: typeof forwardRef,
+  unstable_batchedUpdates: typeof unstable_batchedUpdates,
+  Children: Children,
+};
+
+export default _default;
