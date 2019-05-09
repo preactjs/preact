01f9b4b38b689a6de8a41a16aca944202eaeec81
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-05-09 13:37:53 -0700

    Fixed lazy/Suspense typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -232,15 +232,15 @@ declare namespace preact {
 
         function createContext<T>(defaultValue: T): Context<T>;
 
-        function lazy<T extends Component>(loader: () => Promise<{default: T}>): <P>(props: P) => VNode<any>;
+        function lazy<P>(loader: Promise<{default: AnyComponent<P>}>): (props: P) => VNode<any>;
 
         interface SuspenseProps {
-                children: ComponentChildren;
+                children?: ComponentChildren;
                 fallback: ComponentChildren;
         }
 
         interface SuspenseState {
                 _loading: boolean;
         }
-        abstract class Suspense<SuspenseProps, SuspenseState> extends Component {}
+        abstract class Suspense extends Component<SuspenseProps, SuspenseState> {}
 }
