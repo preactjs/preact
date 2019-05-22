4403681210a432a41f14fd6a633ba8d915dc2baa
Sven <sventschui@gmail.com>
2019-05-22 14:40:12 +0200

    Update src/diff/index.js

--- src/diff/index.js
+++ src/diff/index.js
@@ -333,7 +333,7 @@ export function unmount(vnode, ancestorComponent, skipRemove) {
 
 	vnode._dom = vnode._lastDomChild = null;
 
-	if ((r = vnode._component)) {
+	if (r = vnode._component) {
 		if (r.componentWillUnmount) {
 			try {
 				r.componentWillUnmount();
