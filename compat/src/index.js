633495f77b4a8181a021d91604d17d4c0923534a
jdecroock <decroockjovi@gmail.com>
2019-06-07 21:07:56 +0200

    fix: only hydrate when mounting

--- compat/src/index.js
+++ compat/src/index.js
@@ -78,8 +78,14 @@ class ContextProvider {
 function Portal(props) {
 	let wrap = h(ContextProvider, { context: this.context }, props.vnode);
 	let container = props.container;
-	hydrate('', container);
+	if (!this.mounted) {
+		hydrate('', container);
+	}
 	render(wrap, container);
+	this.componentDidMount = () => {
+		this.mounted = true;
+	};
+
 	this.componentWillUnmount = () => {
 		render(null, container);
 	};
