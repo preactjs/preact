b0a5ca71bcff41ed58c442379c520a10506fccf2
Jovi De Croock <decroockjovi@gmail.com>
2019-07-31 13:03:54 +0200

    (fix) - accidental change

--- test/browser/render.test.js
+++ test/browser/render.test.js
@@ -1077,7 +1077,7 @@ describe('render()', () => {
 		it('should not remove siblings of replaceNode', () => {
 			const childA = scratch.querySelector('#a');
 			render(<div id="a" />, scratch, childA);
-			expect(scratch.innerHTML).to.equal('<div id="a">b</div><div id="b"></div><div id="c"></div>');
+			expect(scratch.innerHTML).to.equal('<div id="a"></div><div id="b"></div><div id="c"></div>');
 		});
 
 		it('should notice prop changes on replaceNode', () => {
