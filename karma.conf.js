03918c79f918d2b8dcfc8d5f0257172acd205618
cristianbote <botecristian@yahoo.com>
2019-08-02 23:10:31 +0300

    remove the master flag for sauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,7 @@
 var coverage = String(process.env.COVERAGE) === 'true',
 	ci = String(process.env.CI).match(/^(1|true)$/gi),
 	pullRequest = !String(process.env.TRAVIS_PULL_REQUEST).match(/^(0|false|undefined)$/gi),
-	masterBranch = String(process.env.TRAVIS_BRANCH).match(/^master$/gi),
-	sauceLabs = ci && !pullRequest && masterBranch,
+	sauceLabs = ci && !pullRequest,
 	performance = !coverage && String(process.env.PERFORMANCE) !== 'false',
 	webpack = require('webpack'),
 	path = require('path');
