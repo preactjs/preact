f99e31abec1cd429275d0075c03c01b3feb07efa
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2019-09-05 21:37:31 +0100

    Support async

---
+++ demo/composition/async.js
@@ -0,0 +1,40 @@
+import { createElement } from "preact";
+import { createComponent, watch, ref } from "preact/composition";
+
+function getItems(length) {
+	return new Promise(resolve =>
+		setTimeout(
+			resolve,
+			300,
+			Array.from({ length }, (_, id) => ({ id, name: "Item #" + id }))
+		)
+	);
+}
+
+export default createComponent(() => {
+	const count = ref(1);
+
+	const increase = () => count.value++;
+	const decrease = () => count.value--;
+
+	const items = watch(count, getItems);
+
+	return () => {
+		const itemList = items.value || [];
+		return (
+			<div>
+				<div>
+					<button onClick={decrease}>decrease</button>
+					{count.value}
+					<button onClick={increase}>increase</button>
+				</div>
+				<p>Hello everyone</p>
+				<ul>
+					{itemList.map(item => (
+						<li key={item.id}>{item.name}</li>
+					))}
+				</ul>
+			</div>
+		);
+	};
+});
