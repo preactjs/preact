abbf7115235eba4de2e51d7ab9c4d89fac74c43d
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-10 12:41:43 +0200

    (chore) - update test to mangled props

--- test/browser/createElementProduction.test.js
+++ test/browser/createElementProduction.test.js
@@ -1,8 +1,10 @@
 import { h } from '../../dist/preact';
 
 describe('createElement production build', () => {
-	it('should not produce a circular data structure', () => {
+	it('should produce a correct structure', () => {
 		const el = h('div', null, h('span', null), h('br', null));
-		expect(JSON.stringify(el)).to.equal('{"type":"div","props":{"children":[{"type":"span","props":{},"text":null,"__k":null,"__e":null,"l":null,"__c":null},{"type":"br","props":{},"text":null,"__k":null,"__e":null,"l":null,"__c":null}]},"text":null,"__k":null,"__e":null,"l":null,"__c":null}');
+		expect(JSON.stringify(el)).to.equal(
+			'{"type":"div","props":{"children":[{"type":"span","props":{},"__k":null,"__p":null,"__b":0,"__e":null,"l":null,"__c":null},{"type":"br","props":{},"__k":null,"__p":null,"__b":0,"__e":null,"l":null,"__c":null}]},"__k":null,"__p":null,"__b":0,"__e":null,"l":null,"__c":null}'
+		);
 	});
 });
