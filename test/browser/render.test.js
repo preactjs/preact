f489fbdf205a7655f07d854207a0c97b8da921ea
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-11 17:11:02 +0200

    (fix) - allow for opacity 0

--- test/browser/render.test.js
+++ test/browser/render.test.js
@@ -422,6 +422,20 @@ describe('render()', () => {
 			}
 		});
 
+		it('should support opacity 0', () => {
+			render(<div style={{ opacity: 1 }}>Test</div>, scratch);
+			let style = scratch.firstChild.style;
+			expect(style)
+				.to.have.property('opacity')
+				.that.equals('1');
+
+			render(<div style={{ opacity: 0 }}>Test</div>, scratch);
+			style = scratch.firstChild.style;
+			expect(style)
+				.to.have.property('opacity')
+				.that.equals('0');
+		});
+
 		it('should replace previous style objects', () => {
 			render(<div style={{ display: 'inline' }}>test</div>, scratch);
 
