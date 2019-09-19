40e6af294d80db59f21c669c8ea46c64c7331023
Jessica Byrne <jessica.byrne@walmartlabs.com>
2019-09-19 09:24:32 -0400

    Add volume to HTMLAttributes interface

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -613,6 +613,7 @@ export namespace JSXInternal {
 		type?: string;
 		useMap?: string;
 		value?: string | string[] | number;
+		volume?: string | number;
 		width?: number | string;
 		wmode?: string;
 		wrap?: string;
@@ -750,7 +751,6 @@ export namespace JSXInternal {
 		ul: HTMLAttributes;
 		"var": HTMLAttributes;
 		video: HTMLAttributes;
-		volume: HTMLAttributes;
 		wbr: HTMLAttributes;
 
 		//SVG
