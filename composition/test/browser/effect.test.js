6dc97f35124ae430be7c4254f5a5011fbe75b993
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2019-09-22 16:26:36 +0100

    Add effect composition tests

---
+++ composition/test/browser/effect.test.js
@@ -0,0 +1,92 @@
+import { setupRerender } from 'preact/test-utils';
+import { createElement as h, render } from 'preact';
+import { setupScratch, teardown } from '../../../test/_util/helpers';
+import { createComponent, effect } from '../../src';
+
+/** @jsx h */
+
+describe('effect', () => {
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	/** @type {() => void} */
+	let rerender;
+
+	beforeEach(() => {
+		scratch = setupScratch();
+		rerender = setupRerender();
+	});
+
+	afterEach(() => {
+		teardown(scratch);
+	});
+
+	it('call effect', () => {
+		const spy = sinon.spy();
+		const Comp = createComponent(() => {
+			effect(spy);
+			return () => null;
+		});
+
+		render(<Comp />, scratch);
+		render(<Comp />, scratch);
+
+		expect(spy).to.be.calledTwice;
+	});
+
+	it('call effect with prop changes', () => {
+		const spy = sinon.spy();
+		const Comp = createComponent(() => {
+			effect(props => props.num, spy);
+			return () => null;
+		});
+
+		render(<Comp num={1} />, scratch);
+
+		expect(spy).to.be.calledWith(1);
+
+		render(<Comp num={2} />, scratch);
+
+		expect(spy).to.be.calledWith(2);
+		expect(spy).to.be.calledTwice;
+	});
+
+	it('dont call effect if prop is same', () => {
+		const spy = sinon.spy();
+		const Comp = createComponent(() => {
+			effect(props => props.num, spy);
+			return () => null;
+		});
+
+		render(<Comp num={1} />, scratch);
+		render(<Comp num={1} />, scratch);
+		expect(spy).to.be.calledOnce;
+	});
+
+	it('call cleanup with prop changes', () => {
+		const spy = sinon.spy();
+		const Comp = createComponent(() => {
+			effect(props => props.num, (n, o, onCleanup) => onCleanup(spy));
+			return () => null;
+		});
+
+		render(<Comp num={1} />, scratch);
+		expect(spy).to.not.be.called;
+
+		render(<Comp num={2} />, scratch);
+		expect(spy).to.be.calledOnce;
+	});
+
+	it('call effect with multiple prop changes', () => {
+		const spy = sinon.spy();
+		const Comp = createComponent(() => {
+			effect([props => props.x, props => props.y], spy);
+			return () => null;
+		});
+
+		render(<Comp x={1} y={10} />, scratch);
+		render(<Comp x={2} y={10} />, scratch);
+
+		expect(spy).to.be.calledWith([2, 10], [1, 10]);
+	});
+});
