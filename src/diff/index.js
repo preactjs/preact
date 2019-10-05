b81d22d687ddb83883548038cd3cc21a4d2d7ef4
jdecroock <decroockjovi@gmail.com>
2019-10-05 10:57:42 +0200

    (fix) - should reorder memoized children

--- src/diff/index.js
+++ src/diff/index.js
@@ -90,7 +90,7 @@ export function diff(parentDom, newVNode, oldVNode, context, isSvg, excessDomChi
 					c.state = c._nextState;
 					c._dirty = false;
 					c._vnode = newVNode;
-					newVNode._dom = oldDom!=null ? oldDom!==oldVNode._dom ? oldDom : oldVNode._dom : null;
+					newVNode._dom = oldVNode._dom;
 					newVNode._children = oldVNode._children;
 					for (tmp = 0; tmp < newVNode._children.length; tmp++) {
 						if (newVNode._children[tmp]) newVNode._children[tmp]._parent = newVNode;
