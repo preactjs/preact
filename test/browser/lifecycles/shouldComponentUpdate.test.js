233f4ad7551fa1adba2367cc445576c694ef0c82
Jovi De Croock <decroockjovi@gmail.com>
2019-10-07 22:12:24 +0200

    (fix) - should reorder memoized children (#1980)

--- test/browser/lifecycles/shouldComponentUpdate.test.js
+++ test/browser/lifecycles/shouldComponentUpdate.test.js
@@ -63,6 +63,42 @@ describe('Lifecycle methods', () => {
 			expect(ShouldNot.prototype.render).to.have.been.calledOnce;
 		});
 
+		it('should reorder non-updating children', () => {
+			const rows = [
+				{ id: '1', a: 5, b: 100 },
+				{ id: '2', a: 50, b: 10 },
+				{ id: '3', a: 25, b: 1000 }
+			];
+
+			class Row extends Component {
+				shouldComponentUpdate(nextProps) {
+					return nextProps.id !== this.props.id;
+				}
+
+				render() {
+					return this.props.id;
+				}
+			}
+
+			const App = ({ sortBy }) => (
+				<div>
+					<table>
+						{rows
+							.sort((a, b) => (a[sortBy] > b[sortBy] ? -1 : 1))
+							.map(row => (
+								<Row id={row.id} key={row.id} />
+							))}
+					</table>
+				</div>
+			);
+
+			render(<App sortBy="a" />, scratch);
+			expect(scratch.innerHTML).to.equal('<div><table>231</table></div>');
+
+			render(<App sortBy="b" />, scratch);
+			expect(scratch.innerHTML).to.equal('<div><table>312</table></div>');
+		});
+
 		it('should rerender when sCU returned false before', () => {
 			let c;
 			let spy = sinon.spy();
