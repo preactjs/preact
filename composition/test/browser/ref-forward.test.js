c85de5daeb8e6567e9a239494b976975dde31a31
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2019-10-12 17:53:39 +0100

    add tests to cover forwarding refs

---
+++ composition/test/browser/ref-forward.test.js
@@ -0,0 +1,54 @@
+import { h, render, createRef } from 'preact';
+import { setupScratch, teardown } from '../../../test/_util/helpers';
+import { createComponent } from '../../src';
+
+/** @jsx h */
+
+describe('ref-forward', () => {
+
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	beforeEach(() => {
+		scratch = setupScratch();
+	});
+
+	afterEach(() => {
+		teardown(scratch);
+	});
+
+	it('forwards ref', () => {
+		const r = createRef();
+
+		// eslint-disable-next-line react/display-name
+		const Comp = createComponent(() => (_, ref) => (
+			<div ref={ref}>Content</div>
+		));
+
+		render(<Comp ref={r} />, scratch);
+
+		expect(r.current).to.equal(scratch.firstChild);
+
+		render(<div />, scratch);
+
+		expect(r.current).to.be.null;
+	});
+
+	it('forwards function to ref', () => {
+		const spy = sinon.spy();
+
+		// eslint-disable-next-line react/display-name
+		const Comp = createComponent(() => (_, ref) => (
+			<div ref={ref}>Content</div>
+		));
+
+		render(<Comp ref={spy} />, scratch);
+
+		expect(spy).to.be.calledOnce;
+		expect(spy).to.be.calledWith(scratch.firstChild);
+
+		render(<div />, scratch);
+		expect(spy).to.be.calledTwice;
+		expect(spy).to.be.calledWith(null);
+	});
+});
