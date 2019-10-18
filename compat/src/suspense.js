bec82b2715f518adac42f41c09560ac5d56df15d
Andre Wiggins <andrewiggins@live.com>
2019-10-18 15:43:00 -0700

    Fix re-suspending bug (+25 B)

--- compat/src/suspense.js
+++ compat/src/suspense.js
@@ -1,4 +1,4 @@
-import { Component, createElement, _unmount as unmount, options } from 'preact';
+import { Component, createElement, _unmount as unmount, options, cloneElement } from 'preact';
 import { removeNode } from '../../src/util';
 
 const oldCatchError = options._catchError;
@@ -40,9 +40,10 @@ function detachDom(children) {
 }
 
 // having custom inheritance instead of a class here saves a lot of bytes
-export function Suspense() {
+export function Suspense(props) {
 	// we do not call super here to golf some bytes...
 	this._suspensions = [];
+	this.state = { _children: props.children };
 }
 
 // Things we do here to save some bytes but are not proper JS inheritance:
@@ -67,18 +68,22 @@ Suspense.prototype._childDidSuspend = function(promise) {
 		if (c._suspensions.length == 0) {
 			// If fallback is null, don't try to unmount it
 			// `unmount` expects a real VNode, not null values
-			if (c.props.fallback) {
-				unmount(c.props.fallback);
+			if (c.state._children) {
+				// Unmount current children (should be fallback)
+				unmount(c.state._children);
 			}
 			c._vnode._dom = null;
 
 			c._vnode._children = c.state._parkedChildren;
-			c.setState({ _parkedChildren: null });
+			c.setState({ _parkedChildren: null, _children: c.props.children });
 		}
 	};
 
 	if (c.state._parkedChildren == null) {
-		c.setState({ _parkedChildren: c._vnode._children });
+		c.setState({
+			_parkedChildren: c._vnode._children,
+			_children: c.props.fallback && cloneElement(c.props.fallback)
+		});
 		detachDom(c._vnode._children);
 		c._vnode._children = [];
 	}
@@ -87,7 +92,7 @@ Suspense.prototype._childDidSuspend = function(promise) {
 };
 
 Suspense.prototype.render = function(props, state) {
-	return state._parkedChildren ? props.fallback : props.children;
+	return state._children;
 };
 
 export function lazy(loader) {
