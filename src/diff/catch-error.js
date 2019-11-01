4cf0c1233d6378ff861a4296b373c7a51f3ea16b
Andre Wiggins <andrewiggins@live.com>
2019-10-31 18:33:54 -0700

    Inline catchError declaration into options (-2 B)

---
+++ src/diff/catch-error.js
@@ -0,0 +1,39 @@
+import { enqueueRender } from '../component';
+
+/**
+ * Find the closest error boundary to a thrown error and call it
+ * @param {object} error The thrown value
+ * @param {import('../internal').VNode} vnode The vnode that threw
+ * the error that was caught (except for unmounting when this parameter
+ * is the highest parent that was being unmounted)
+ * @param {import('../internal').VNode} [oldVNode] The oldVNode of the vnode
+ * that threw, if this VNode threw while diffing
+ */
+export function _catchError(error, vnode, oldVNode) {
+	/** @type {import('../internal').Component} */
+	let component;
+
+	for (; (vnode = vnode._parent); ) {
+		if ((component = vnode._component) && !component._processingException) {
+			try {
+				if (
+					component.constructor &&
+					component.constructor.getDerivedStateFromError != null
+				) {
+					component.setState(
+						component.constructor.getDerivedStateFromError(error)
+					);
+				} else if (component.componentDidCatch != null) {
+					component.componentDidCatch(error);
+				} else {
+					continue;
+				}
+				return enqueueRender((component._pendingError = component));
+			} catch (e) {
+				error = e;
+			}
+		}
+	}
+
+	throw error;
+}
