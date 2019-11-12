01b5f981dc9c4d686b3e0df7c645ced12c252c16
Prateek Bhatnagar <prateek89born@gmail.com>
2019-11-12 15:53:18 -0800

    reverting karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -114,7 +114,7 @@ module.exports = function(config) {
 			{
 				pattern:
 					config.grep ||
-					'{debug,hooks,compat,test-utils,}/test/{browser,shared}/**/suspense-list*.test.js',
+					'{debug,hooks,compat,test-utils,}/test/{browser,shared}/**/*.test.js',
 				watched: false
 			}
 		],
