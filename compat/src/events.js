1f11df42659ecd9ede0de7d797000b5ea3237b4f
Sven Tschui <github@sventschui.ch>
2019-11-16 09:08:49 +0100

    Always use lower cased touch events in compat. See #2113

--- compat/src/events.js
+++ compat/src/events.js
@@ -7,7 +7,7 @@ export function applyEventNormalization({ type, props }) {
 	let newProps = {};
 
 	for (let i in props) {
-		if (/^on(Ani|Tra)/.test(i)) {
+		if (/^on(Ani|Tra|Tou)/.test(i)) {
 			props[i.toLowerCase()] = props[i];
 			delete props[i];
 		}
