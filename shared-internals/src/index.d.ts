6a81ab55466cca265054b1921fbdf649c18e762c
Marvin Hagemeister <hello@marvinh.dev>
2019-11-29 11:46:18 +0100

    Add options type to shared-internals

--- shared-internals/src/index.d.ts
+++ shared-internals/src/index.d.ts
@@ -8,3 +8,31 @@ export function getChildren(
 	vnode: preact.VNode
 ): Array<preact.VNode | null | undefined>;
 export function getComponentVNode(component: preact.Component): preact.VNode;
+
+//
+// Option hooks
+//
+// These are using the public types to avoid conflicts. Downside of that
+// is that the return types need to be declared manually here.
+type OptionsDiff = (vnode: preact.VNode) => void;
+type OptionsCommit = (
+	vnode: preact.VNode,
+	commitQueue: preact.Component[]
+) => void;
+type OptionsRoot = (
+	vnode: preact.ComponentChild,
+	parent: Element | Document | ShadowRoot | DocumentFragment
+) => void;
+
+// Getters
+export function getOptionsDiff(options: preact.Options): OptionsDiff;
+export function getOptionsCommit(options: preact.Options): OptionsCommit;
+export function getOptionsRoot(options: preact.Options): OptionsRoot;
+
+// Setters
+export function setOptionsDiff(options: preact.Options, fn: OptionsDiff): void;
+export function setOptionsCommit(
+	options: preact.Options,
+	fn: OptionsCommit
+): void;
+export function setOptionsRoot(options: preact.Options, fn: OptionsRoot): void;
