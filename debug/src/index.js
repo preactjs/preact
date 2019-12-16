a617244889e26f7fffcbacb70e443ae791a3b6e4
Marvin Hagemeister <hello@marvinh.dev>
2019-12-16 20:00:56 +0100

    Ignore debug env check in coverage

--- debug/src/index.js
+++ debug/src/index.js
@@ -1,6 +1,7 @@
 import { initDebug } from './debug';
 import { initDevTools } from './devtools';
 
+/* istanbul ignore else */
 if (process.env.NODE_ENV === 'development') {
 	initDebug();
 	initDevTools();
