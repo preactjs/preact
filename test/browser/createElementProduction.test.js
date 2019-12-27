841519090c068d592e6084a8d6ed60c681c535a4
Jason Miller <developit@google.com>
2019-12-26 22:24:06 -0500

    fix false-negative cyclical reference test

--- test/browser/createElementProduction.test.js
+++ test/browser/createElementProduction.test.js
@@ -3,6 +3,10 @@ import { h } from 'preact';
 describe('createElement production build', () => {
 	it('should produce a correct structure', () => {
 		const el = h('div', null, h('span', null), h('br', null));
+		// fix: this test was intended to surface cyclical references, but false-negatives for self-references (which are allowed).
+		delete el._original;
+		delete el.props.children[0]._original;
+		delete el.props.children[1]._original;
 		expect(() => JSON.stringify(el)).to.not.throw();
 	});
 });
