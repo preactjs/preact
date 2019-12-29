47d46a00a4f23efa13c4bf97f003f249e48975e1
jdecroock <decroockjovi@gmail.com>
2019-12-29 17:07:07 +0100

    don't assign null when the ref isn't assigned to the current vdom, this occurs when it has already been unmounted

--- src/diff/index.js
+++ src/diff/index.js
@@ -406,7 +406,8 @@ export function unmount(vnode, parentVNode, skipRemove) {
 	if (options.unmount) options.unmount(vnode);
 
 	if ((r = vnode.ref)) {
-		applyRef(r, null, parentVNode);
+		if (!vnode.ref.current || vnode.ref.current === vnode._dom)
+			applyRef(r, null, parentVNode);
 	}
 
 	let dom;
