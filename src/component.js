8ea9c5416e428a9449ddbf604ecf17112ea540cc
Andre Wiggins <andrewiggins@live.com>
2020-02-02 18:20:25 -0800

    Add back a useful istanbul ignore

--- src/component.js
+++ src/component.js
@@ -166,6 +166,7 @@ let q = [];
  * Asynchronously schedule a callback
  * @type {(cb: () => void) => void}
  */
+/* istanbul ignore next */
 // Note the following line isn't tree-shaken by rollup cuz of rollup/rollup#2566
 const defer =
 	typeof Promise == 'function'
