3cbf797d3cdbd20c0dea214b6b3fcbaad13de88e
Brian Vella <bvella@users.noreply.github.com>
2020-02-11 12:15:33 +0100

    fixed whitespace

--- compat/src/index.d.ts
+++ compat/src/index.d.ts
@@ -31,12 +31,12 @@ declare namespace React {
 
 	// Preact Defaults
 	export type Component<P = {}, S = {}, SS = any> = preact.Component<P, S>;
-    export type ReactElement<P = {}, S = any> = preact.VNode<P>;
-    export import ComponentClass = preact.ComponentClass;
-    export import ComponentType = preact.ComponentType;
-    export import Context = preact.Context;
-    export import ReactNode = preact.ComponentChildren;
-    export type CSSProperties = string | {[key: string]: string | number};
+	export type ReactElement<P = {}, S = any> = preact.VNode<P>;
+	export import ComponentClass = preact.ComponentClass;
+	export import ComponentType = preact.ComponentType;
+	export import Context = preact.Context;
+	export import ReactNode = preact.ComponentChildren;
+	export type CSSProperties = string | {[key: string]: string | number};
 	export type AllHTMLAttributes<T> = JSX.HTMLAttributes;
 	export import FunctionComponent = preact.FunctionComponent;
 	export import FC = preact.FunctionComponent;
