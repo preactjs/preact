1560707e41dfad9f6db3d64c7fae7b9a952b674b
jdecroock <decroockjovi@gmail.com>
2020-03-02 14:53:41 +0100

    delete mangled variant

--- test/browser/createElementProduction.test.js
+++ test/browser/createElementProduction.test.js
@@ -6,6 +6,9 @@ describe('createElement production build', () => {
 		delete el._original;
 		delete el.props.children[0]._original;
 		delete el.props.children[1]._original;
+		delete el.__v;
+		delete el.props.children[0].__v;
+		delete el.props.children[1].__v;
 		expect(() => JSON.stringify(el)).to.not.throw();
 	});
 });
