0b419a53b16d008a30c4e64d4f9710e7bc455bf6
Marvin Hagemeister <hello@marvinh.dev>
2020-05-18 23:52:31 +0200

    Add MobX demo page

--- demo/index.js
+++ demo/index.js
@@ -23,6 +23,7 @@ import TextFields from './textFields';
 import ReduxBug from './reduxUpdate';
 import SuspenseRouterBug from './suspense-router';
 import NestedSuspenseBug from './nested-suspense';
+import { MobXDemo } from './mobx';
 
 let isBenchmark = /(\/spiral|\/pythagoras|[#&]bench)/g.test(
 	window.location.href
@@ -112,6 +113,9 @@ class App extends Component {
 						<Link href="/redux" activeClassName="active">
 							Redux
 						</Link>
+						<Link href="/mobx" activeClassName="active">
+							MobX
+						</Link>
 						<Link href="/suspense" activeClassName="active">
 							Suspense / lazy
 						</Link>
@@ -151,6 +155,7 @@ class App extends Component {
 						<PeopleBrowser path="/people/:user?" />
 						<StyledComp path="/styled-components" />
 						<Redux path="/redux" />
+						<MobXDemo path="/mobx" />
 						<TextFields path="/textfields" />
 						<ReduxBug path="/reduxBug/:start" />
 						<SuspenseRouterBug path="/suspense-router" />
