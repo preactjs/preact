1834cd70adf5758541d6167ba8c2c42778443d04
Jason Miller <developit@users.noreply.github.com>
2020-05-18 23:22:53 +0000

    10.4.4

--- devtools/src/devtools.js
+++ devtools/src/devtools.js
@@ -2,7 +2,7 @@ import { options, Fragment, Component } from 'preact';
 
 export function initDevTools() {
 	if (typeof window != 'undefined' && window.__PREACT_DEVTOOLS__) {
-		window.__PREACT_DEVTOOLS__.attachPreact('10.4.3', options, {
+		window.__PREACT_DEVTOOLS__.attachPreact('10.4.4', options, {
 			Fragment,
 			Component
 		});
