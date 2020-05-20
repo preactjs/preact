2e4c643b59df406ca8bfcb4f99fb0f96fd1f104d
Andre Wiggins <andrewiggins@live.com>
2020-05-19 20:30:31 -0700

    Replace aria regex with substr

--- src/diff/props.js
+++ src/diff/props.js
@@ -146,7 +146,7 @@ export function setProperty(dom, name, value, oldValue, isSvg) {
 				// ARIA-attributes we could treat false as a removal, but the
 				// amount of exceptions would cost us too many bytes. On top of
 				// that other VDOM frameworks also always stringify `false`.
-				!/^ar/.test(name))
+				name.substr(0, 2) !== 'ar')
 		) {
 			dom.removeAttribute(name);
 		} else {
