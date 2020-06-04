f86c0e7da0d6fd13041215e8bf1b8326ddee2b11
me <bhavesh.pareek36@gmail.com>
2020-06-05 00:05:10 +0530

    pass ownerDocument to diff function

---
+++ test/browser/getOwnerDocument.test.js
@@ -0,0 +1,28 @@
+import { createElement, render } from 'preact';
+import { setupScratch, teardown } from '../_util/helpers';
+
+/** @jsx createElement */
+
+describe('parentDom.ownerDocument', () => {
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	before(() => {
+		scratch = setupScratch();
+	});
+
+	after(() => {
+		teardown(scratch);
+	});
+
+	it('should reference the correct document from the parent node', () => {
+		let spy = sinon.spy(scratch, 'ownerDocument', ['get']);
+
+		function Foo() {
+			return <div>A</div>;
+		}
+
+		render(<Foo />, scratch);
+		expect(spy).to.be.calledOnce;
+	});
+});
