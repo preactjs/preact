db8ec2b50d31fe9cd78aee170ce7cc1fe483c83f
me <bhavesh.pareek36@gmail.com>
2020-06-05 01:12:13 +0530

    remove unused variable

--- test/browser/getOwnerDocument.test.js
+++ test/browser/getOwnerDocument.test.js
@@ -16,8 +16,6 @@ describe('parentDom.ownerDocument', () => {
 	});
 
 	it('should reference the correct document from the parent node', () => {
-		let spy = sinon.spy(scratch, 'ownerDocument', ['get']);
-
 		function Foo() {
 			return <div>A</div>;
 		}
