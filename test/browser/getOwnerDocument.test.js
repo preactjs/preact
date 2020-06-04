4d8a40ed9c606683b83db0455f10bb5b556b4398
me <bhavesh.pareek36@gmail.com>
2020-06-05 00:11:29 +0530

    check spy.get

--- test/browser/getOwnerDocument.test.js
+++ test/browser/getOwnerDocument.test.js
@@ -23,7 +23,7 @@ describe('parentDom.ownerDocument', () => {
 		}
 
 		render(<Foo />, scratch);
-		expect(spy).to.be.calledOnce;
+		expect(spy.get).to.be.calledOnce;
 		expect(scratch.ownerDocument).to.equal(document);
 	});
 });
