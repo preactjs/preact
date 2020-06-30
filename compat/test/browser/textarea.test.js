dc0a5f0137f7b1eeeca05e71a7ef4cfc86606eca
Andre Wiggins <andrewiggins@live.com>
2020-06-30 12:51:58 -0700

    Add more tests for textarea compat

--- compat/test/browser/textarea.test.js
+++ compat/test/browser/textarea.test.js
@@ -2,6 +2,8 @@ import React, { render } from 'preact/compat';
 import { setupScratch, teardown } from '../../../test/_util/helpers';
 
 describe('Textarea', () => {
+	// React textarea: https://codesandbox.io/s/react-textarea-py5c6
+
 	let scratch;
 
 	beforeEach(() => {
@@ -22,5 +24,28 @@ describe('Textarea', () => {
 		render(<textarea defaultValue="foo" />, scratch);
 
 		expect(scratch.innerHTML).to.equal('<textarea>foo</textarea>');
+	});
+
+	it('should allow using children text if no value or defaultValue is provided', () => {
+		render(<textarea>Children text</textarea>, scratch);
+		expect(scratch.innerHTML).to.equal('<textarea>Children text</textarea>');
+	});
+
+	it('should prioritize value over defaultValue', () => {
+		render(
+			<textarea value="Value prop" defaultValue="defaultValue prop" />,
+			scratch
+		);
+		expect(scratch.innerHTML).to.equal('<textarea>Value prop</textarea>');
+	});
+
+	it('should prioritize value prop over defaultValue and children', () => {
+		render(
+			<textarea value="Value prop" defaultValue="defaultValue prop">
+				Children text
+			</textarea>,
+			scratch
+		);
+		expect(scratch.innerHTML).to.equal('<textarea>Value prop</textarea>');
 	});
 });
