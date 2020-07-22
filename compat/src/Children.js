867fd816f9b992946f3648e9e74a67da9525d62c
Martin Oppitz <github@martinoppitz.com>
2020-07-22 02:41:49 +0200

    fix(#2644): adapt the react children api

--- compat/src/Children.js
+++ compat/src/Children.js
@@ -1,9 +1,9 @@
 import { toChildArray } from 'preact';
 
-const mapFn = (children, fn) => {
+const mapFn = (children, fn, context) => {
 	if (!children) return null;
 	return toChildArray(children).reduce(
-		(acc, value, index) => acc.concat(fn(value, index)),
+		(acc, value, index) => acc.concat(fn(context, value, index)),
 		[]
 	);
 };
