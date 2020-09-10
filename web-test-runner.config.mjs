0a74b30a74451e4934c50e1027e6018d03351e43
Marvin Hagemeister <hello@marvinh.dev>
2020-09-10 15:43:00 +0200

    Add back saucelabs integration

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,8 +1,75 @@
 import { importMapsPlugin } from '@web/dev-server-import-maps';
 import { legacyPlugin } from '@web/dev-server-legacy';
+import { createSauceLabsLauncher } from '@web/test-runner-saucelabs';
 import rollupBabel from '@rollup/plugin-babel';
 import { fromRollup } from '@web/dev-server-rollup';
 import fs from 'fs';
+
+const ci = String(process.env.CI).match(/^(1|true)$/gi);
+const pullRequest = String(process.env.GITHUB_EVENT_NAME) === 'pull_request';
+const masterBranch = String(process.env.GITHUB_WORKFLOW);
+// const sauceLabs = ci && !pullRequest && masterBranch;
+const sauceLabs = true;
+let browsers;
+
+if (sauceLabs) {
+	const sauceLabsLauncher = createSauceLabsLauncher({
+		user: process.env.SAUCE_USERNAME,
+		key: process.env.SAUCE_ACCESS_KEY
+		// the Sauce Labs datacenter to run your tests on, defaults to 'us-west-1'
+		// region: 'eu-central-1',
+	});
+
+	const sharedCapabilities = {
+		'sauce:options': {
+			name: 'unit-tests',
+			// if you are running tests in a CI, the build id might be available as an
+			// environment variable. this is useful for identifying test runs
+			// this is for example the name for github actions
+			build: `CI #${process.env.GITHUB_RUN_NUMBER} (${process.env.GITHUB_RUN_ID})`,
+			tunnelIdentifier:
+				process.env.GITHUB_RUN_NUMBER ||
+				`local${require('./package.json').version}`,
+			connectLocationForSERelay: 'localhost',
+			connectPortForSERelay: 4445,
+			startConnect: !!sauceLabs
+		}
+	};
+	browsers = [
+		sauceLabsLauncher({
+			capabilities: {
+				...sharedCapabilities,
+				browserName: 'chrome',
+				browserVersion: 'latest',
+				platformName: 'Windows 10'
+			}
+		}),
+		sauceLabsLauncher({
+			capabilities: {
+				...sharedCapabilities,
+				browserName: 'firefox',
+				browserVersion: 'latest',
+				platformName: 'Windows 10'
+			}
+		}),
+		sauceLabsLauncher({
+			capabilities: {
+				...sharedCapabilities,
+				browserName: 'MicrosoftEdge',
+				browserVersion: 'latest',
+				platformName: 'Windows 10'
+			}
+		}),
+		sauceLabsLauncher({
+			capabilities: {
+				...sharedCapabilities,
+				browserName: 'internet explorer',
+				browserVersion: '11.0',
+				platformName: 'Windows 7'
+			}
+		})
+	];
+}
 
 const rename = {};
 const mangle = JSON.parse(fs.readFileSync('./mangle.json', 'utf8'));
@@ -22,6 +89,7 @@ export default {
 	mimeTypes: {
 		'**/*.jsx': 'js'
 	},
+	browsers,
 	plugins: [
 		importMapsPlugin({
 			inject: {
