ec04f3e4a9edff2ded5f5839cd07e664f57e9fdc
Andre Wiggins <andrewiggins@live.com>
2020-09-15 14:27:53 -0700

    Add some docs on using createLazy and createSuspender

--- compat/test/browser/suspense.test.js
+++ compat/test/browser/suspense.test.js
@@ -14,8 +14,32 @@ const h = React.createElement;
 /* eslint-env browser, mocha */
 
 /**
+ * Create a Lazy component whose promise is controlled by by the test. This
+ * function returns 3 values: The Lazy component to render, a `resolve`
+ * function, and a `reject` function. Call `resolve` with the component the Lazy
+ * component should resolve with. Call `reject` with the error the Lazy
+ * component should reject with
+ *
+ * @example
+ * // 1. Create and render the Lazy component
+ * const [Lazy, resolve] = createLazy();
+ * render(
+ * 	<Suspense fallback={<div>Suspended...</div>}>
+ * 		<Lazy />
+ * 	</Suspense>,
+ * 	scratch
+ * );
+ * rerender(); // Rerender is required so the fallback is displayed
+ * expect(scratch.innerHTML).to.eql(`<div>Suspended...</div>`);
+ *
+ * // 2. Resolve the Lazy with a new component to render
+ * return resolve(() => <div>Hello</div>).then(() => {
+ * 	rerender();
+ * 	expect(scratch.innerHTML).to.equal(`<div>Hello</div>`);
+ * });
+ *
  * @typedef {import('../../../src').ComponentType<any>} ComponentType
- * @returns {[typeof Component, (c: ComponentType) => Promise<void>, (c: ComponentType) => void]}
+ * @returns {[typeof Component, (c: ComponentType) => Promise<void>, (e: Error) => Promise<void>]}
  */
 function createLazy() {
 	/** @type {(c: ComponentType) => Promise<void>} */
@@ -27,8 +51,8 @@ function createLazy() {
 				return promise;
 			};
 
-			rejecter = () => {
-				reject();
+			rejecter = e => {
+				reject(e);
 				return promise;
 			};
 		});
@@ -40,6 +64,33 @@ function createLazy() {
 }
 
 /**
+ * Returns a Component and a function (named `suspend`) that will suspend the component when called.
+ * `suspend` will return two functions, `resolve` and `reject`. Call `resolve` with a Component the
+ * suspended component should resume with or reject with the Error the suspended component should
+ * reject with
+ *
+ * @example
+ * // 1. Create a suspender with initial children (argument to createSuspender) and render it
+ * const [Suspender, suspend] = createSuspender(() => <div>Hello</div>);
+ * render(
+ * 	<Suspense fallback={<div>Suspended...</div>}>
+ * 		<Suspender />
+ * 	</Suspense>,
+ * 	scratch
+ * );
+ * expect(scratch.innerHTML).to.eql(`div>Hello</div>`);
+ *
+ * // 2. Cause the component to suspend and rerender the update (i.e. the fallback)
+ * const [resolve] = suspend();
+ * rerender();
+ * expect(scratch.innerHTML).to.eql(`div>Suspended...</div>`);
+ *
+ * // 3. Resolve the suspended component with a new component and rerender
+ * return resolve(() => <div>Hello2</div>).then(() => {
+ * 	rerender();
+ * 	expect(scratch.innerHTML).to.eql(`div>Hello2</div>`);
+ * });
+ *
  * @typedef {[(c: ComponentType) => Promise<void>, (error: Error) => Promise<void>]} Resolvers
  * @param {ComponentType} DefaultComponent
  * @returns {[typeof Component, () => Resolvers]}
