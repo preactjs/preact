31f88dd011193ac655df1e858d432c8718457a4d
Jovi De Croock <decroockjovi@gmail.com>
2020-09-22 20:09:44 +0200

    fix typings, remove DevSource from options.vnode

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,6 @@ export = preact;
 export as namespace preact;
 
 import { JSXInternal } from './jsx';
-import { DevSource } from './internal';
 
 declare namespace preact {
 	export import JSX = JSXInternal;
@@ -257,7 +256,7 @@ declare namespace preact {
 	 */
 	interface Options {
 		/** Attach a hook that is invoked whenever a VNode is created. */
-		vnode?(vnode: VNode, source: DevSource, componentSelf: DevSource): void;
+		vnode?(vnode: VNode): void;
 		/** Attach a hook that is invoked immediately before a vnode is unmounted. */
 		unmount?(vnode: VNode): void;
 		/** Attach a hook that is invoked after a vnode has rendered. */
