a1279b9b57bd8d60d72c6b9643f92d59311c6dfb
Jovi De Croock <decroockjovi@gmail.com>
2020-09-22 20:08:54 +0200

    remove unused exports

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ export {
 	createElement as h,
 	Fragment,
 	createRef,
-	isValidElement,
-	jsx,
-	jsxs,
-	jsxDEV
+	isValidElement
 } from './create-element';
 export { Component } from './component';
 export { cloneElement } from './clone-element';
