432c7903babab19319a1987667ed4a7caa689626
Marvin Hagemeister <hello@marvinh.dev>
2020-09-23 11:41:08 +0200

    Add more jsx-runtime tests

--- test/browser/jsx.test.js
+++ test/browser/jsx.test.js
@@ -1,4 +1,5 @@
-import { jsx } from '../../jsx-runtime';
+import { Component, createElement } from 'preact';
+import { jsx, jsxs, jsxDEV, Fragment } from '../../jsx-runtime';
 import { setupScratch, teardown } from '../_util/helpers';
 
 describe('Babel jsx/jsxDEV', () => {
@@ -12,6 +13,13 @@ describe('Babel jsx/jsxDEV', () => {
 		teardown(scratch);
 	});
 
+	it('should have needed exports', () => {
+		expect(typeof jsx).to.equal('function');
+		expect(typeof jsxs).to.equal('function');
+		expect(typeof jsxDEV).to.equal('function');
+		expect(typeof Fragment).to.equal('function');
+	});
+
 	it('should keep ref in props', () => {
 		const ref = () => null;
 		const vnode = jsx('div', { ref });
@@ -21,5 +29,39 @@ describe('Babel jsx/jsxDEV', () => {
 	it('should add keys', () => {
 		const vnode = jsx('div', null, 'foo');
 		expect(vnode.key).to.equal('foo');
+	});
+
+	it('should apply defaultProps', () => {
+		class Foo extends Component {
+			render() {
+				return <div />;
+			}
+		}
+
+		Foo.defaultProps = {
+			foo: 'bar'
+		};
+
+		const vnode = jsx(Foo, {}, null);
+		expect(vnode.props).to.deep.equal({
+			foo: 'bar'
+		});
+	});
+
+	it('should set __source and __self', () => {
+		const vnode = jsx('div', { class: 'foo' }, 'key', 'source', 'self');
+		expect(vnode.__source).to.equal('source');
+		expect(vnode.__self).to.equal('self');
+	});
+
+	it('should return a vnode like createElement', () => {
+		const elementVNode = createElement('div', {
+			class: 'foo',
+			key: 'key'
+		});
+		const jsxVNode = jsx('div', { class: 'foo' }, 'key');
+		delete jsxVNode.__self;
+		delete jsxVNode.__source;
+		expect(jsxVNode).to.deep.equal(elementVNode);
 	});
 });
