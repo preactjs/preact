c7a7253da8fa8b447af0269217ef14a3ae02680e
Yevhenii Dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-10-09 11:51:26 +0300

    #2792 fix memo fn types definition (#2793)
    * #2792: added overloading memo fn
    
    * #2792: added test ts for memo fn
    
    * #2792: refactor test ts

--- compat/test/ts/memo.tsx
+++ compat/test/ts/memo.tsx
@@ -1,12 +1,18 @@
 import * as React from '../../src';
-
-function ExpectType<T>(v: T) {} // type assertion helper
+import { expectType } from './utils';
 
 interface MemoProps {
 	required: string;
 	optional?: string;
 	defaulted: string;
 }
+
+interface MemoPropsExceptDefaults {
+	required: string;
+	optional?: string;
+}
+
+const ComponentExceptDefaults = () => <div></div>;
 
 const ReadonlyBaseComponent = (props: Readonly<MemoProps>) => (
 	<div>{props.required + props.optional + props.defaulted}</div>
@@ -20,21 +26,31 @@ BaseComponent.defaultProps = { defaulted: '' };
 
 // memo for readonly component with default comparison
 const MemoedReadonlyComponent = React.memo(ReadonlyBaseComponent);
-ExpectType<React.FunctionComponent<MemoProps>>(MemoedReadonlyComponent);
+expectType<React.FunctionComponent<MemoProps>>(MemoedReadonlyComponent);
 export const memoedReadonlyComponent = (
 	<MemoedReadonlyComponent required="hi" />
 );
 
 // memo for non-readonly component with default comparison
 const MemoedComponent = React.memo(BaseComponent);
-ExpectType<React.FunctionComponent<MemoProps>>(MemoedComponent);
+expectType<React.FunctionComponent<MemoProps>>(MemoedComponent);
 export const memoedComponent = <MemoedComponent required="hi" />;
 
 // memo with custom comparison
 const CustomMemoedComponent = React.memo(BaseComponent, (a, b) => {
-	ExpectType<MemoProps>(a);
-	ExpectType<MemoProps>(b);
+	expectType<MemoProps>(a);
+	expectType<MemoProps>(b);
 	return a.required === b.required;
 });
-ExpectType<React.FunctionComponent<MemoProps>>(CustomMemoedComponent);
+expectType<React.FunctionComponent<MemoProps>>(CustomMemoedComponent);
 export const customMemoedComponent = <CustomMemoedComponent required="hi" />;
+
+const MemoedComponentExceptDefaults = React.memo<MemoPropsExceptDefaults>(
+	ComponentExceptDefaults
+);
+expectType<React.FunctionComponent<MemoPropsExceptDefaults>>(
+	MemoedComponentExceptDefaults
+);
+export const memoedComponentExceptDefaults = (
+	<MemoedComponentExceptDefaults required="hi" />
+);
