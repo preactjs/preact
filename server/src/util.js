28a09533bf9f964f164ac4684dd48f1f1bac21f7
Jason Miller <developit@users.noreply.github.com>
2020-10-15 17:50:48 -0400

    Update util.js

--- server/src/util.js
+++ server/src/util.js
@@ -2,6 +2,7 @@
 export const IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|^--/i;
 
 export function encodeEntities(s) {
+	if (typeof s !== 'string') s = String(s);
 	let out = '';
 	for (let i=0; i<s.length; i++) {
 		let ch = s[i];
