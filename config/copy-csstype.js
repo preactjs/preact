3c9d4996035f54d50342dedc93e9811b03766b9c
Jason Miller <developit@users.noreply.github.com>
2020-12-15 23:00:16 -0500

    Delete copy-csstype.js

--- config/copy-csstype.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, '..', 'node_modules', 'csstype', 'index.d.ts');
-const dest = path.join(__dirname, '..', 'src', 'jsx-csstype.d.ts');
-fs.copyFileSync(src, dest);
