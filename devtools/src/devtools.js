c6b58f7a8f7ce974395224de67e1828080b073e7
Marvin Hagemeister <hello@marvinh.dev>
2021-01-03 13:25:05 +0100

    Release 10.5.9

--- devtools/src/devtools.js
+++ devtools/src/devtools.js
@@ -2,7 +2,7 @@ import { options, Fragment, Component } from 'preact';
 
 export function initDevTools() {
 	if (typeof window != 'undefined' && window.__PREACT_DEVTOOLS__) {
-		window.__PREACT_DEVTOOLS__.attachPreact('10.5.8', options, {
+		window.__PREACT_DEVTOOLS__.attachPreact('10.5.9', options, {
 			Fragment,
 			Component
 		});
