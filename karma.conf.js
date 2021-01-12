a0a4beae7217c6d2bc5fdfa5d7a8ad72ab32e124
Marvin Hagemeister <hello@marvinh.dev>
2021-01-12 15:21:44 +0100

    Fix karma IE11 target containing ES2015 syntax

--- karma.conf.js
+++ karma.conf.js
@@ -281,6 +281,7 @@ module.exports = function(config) {
 		},
 
 		esbuild: {
+			target: 'es5',
 			define: {
 				COVERAGE: coverage,
 				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || ''),
