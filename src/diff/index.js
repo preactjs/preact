76fc28b9573d75ef6b789b4be9bd2dd435636904
Li Hau Tan <tanlh@seagroup.com>
2021-02-08 16:19:01 +0800

    fix reordering issue of memoized component when the component initially render null

--- src/diff/index.js
+++ src/diff/index.js
@@ -147,6 +147,9 @@ export function diff(
 					c._vnode = newVNode;
 					newVNode._dom = oldVNode._dom;
 					newVNode._children = oldVNode._children;
+					newVNode._children.forEach(vnode => {
+						if (vnode) vnode._parent = newVNode;
+					});
 					if (c._renderCallbacks.length) {
 						commitQueue.push(c);
 					}
