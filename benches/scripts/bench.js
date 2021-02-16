0f88fac0efba7bfffbdbbdee1fafabf0bcd8c791
Andre Wiggins <andrewiggins@live.com>
2021-02-15 16:59:03 -0800

    Print helpful message if no benchmarks are found matching patterns

--- benches/scripts/bench.js
+++ benches/scripts/bench.js
@@ -32,8 +32,13 @@ export const defaultBenchOptions = {
 export async function runBenches(bench1 = 'all', opts) {
 	const globs = bench1 === 'all' ? allBenches : [bench1].concat(opts._);
 	const benchesToRun = await globSrc(globs);
-	console.log('Running benchmarks:', benchesToRun.join(', '));
-	console.log();
+
+	if (benchesToRun.length == 0) {
+		console.log('No benchmarks found matching patterns:', globs);
+	} else {
+		console.log('Running benchmarks:', benchesToRun.join(', '));
+		console.log();
+	}
 
 	const configFileTasks = benchesToRun.map(async (benchPath, i) => {
 		return generateConfig(benchesRoot('src', benchPath), {
