cedd97144169bf570b292671507754e835ee6279
Jason Miller <jason@developit.ca>
2021-05-16 13:57:02 -0400

    use uninstallDebug() to remove preact/debug after testing it

--- debug/test/browser/temporaryOptionsHooks.js
+++ debug/test/browser/temporaryOptionsHooks.js
@@ -1,26 +1,10 @@
-// import { options } from 'preact';
+import { initDebug, uninstallDebug } from 'preact/debug';
 
-// const storedOptions = Object.assign({}, options);
-// console.log('storedOptions: ', storedOptions);
-// let revisedOptions;
-
-// function set(obj, newObj) {
-// 	for (let i in obj) if (!(i in newObj)) delete obj[i];
-// 	Object.assign(obj, newObj);
-// }
-
-// export function withTemporaryOptions() {
-// 	before(() => {
-// 		if (!revisedOptions) {
-// 			revisedOptions = Object.assign({}, options);
-// 		} else {
-// 			set(options, revisedOptions);
-// 		}
-// 	});
-
-// 	after(() => {
-// 		set(options, storedOptions);
-// 	});
-// }
-
-export function withTemporaryOptions() {}
+export function withTemporaryOptions() {
+	before(() => {
+		initDebug();
+	});
+	after(() => {
+		uninstallDebug();
+	});
+}
