a3e481e8b3fd24eaf3a324a64b3599d1590c3732
Jovi De Croock <decroockjovi@gmail.com>
2021-06-10 09:57:49 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -60,4 +60,4 @@ export const RESET_MODE = ~(
 export const INHERITED_MODES = MODE_HYDRATE | MODE_MUTATIVE_HYDRATE | MODE_SVG;
 
 export const EMPTY_ARR = [];
-export let UNDEFINED;
+export let UNDEFINED = undefined;
