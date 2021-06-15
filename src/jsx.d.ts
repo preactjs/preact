51d607c7e721c8bea952af3df9a52327ee85cb8c
Mario Pizzinini <hello@crafted.systems>
2021-06-15 11:34:45 +0200

    Add attribute definition autoCapitalize to HTMLAttributes

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -754,6 +754,10 @@ export namespace JSXInternal {
 		wmode?: string;
 		wrap?: string;
 
+		// Non-standard Attributes
+		autocapitalize?: string;
+		autoCapitalize?: string;
+
 		// RDFa Attributes
 		about?: string;
 		datatype?: string;
