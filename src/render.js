68ae6dce885b4b7e6466bee8af6be9d5ff53d4e0
Jovi De Croock <decroockjovi@gmail.com>
2021-10-10 17:20:02 +0200

    leverage createRoot in rendre/hydrate

--- src/render.js
+++ src/render.js
@@ -10,6 +10,7 @@ import options from './options';
 import { mount } from './diff/mount';
 import { patch } from './diff/patch';
 import { createInternal } from './tree';
+import { createRoot } from './create-root';
 
 /**
  * Render a Preact virtual node into a DOM element
@@ -18,55 +19,12 @@ import { createInternal } from './tree';
  * render into
  */
 export function render(vnode, parentDom) {
-	if (options._root) options._root(vnode, parentDom);
-
-	// List of effects that need to be called after diffing.
-	const commitQueue = [];
-
-	// To be able to support calling `render()` multiple times on the same
-	// DOM node, we need to obtain a reference to the previous tree. We do
-	// this by assigning a new `_children` property to DOM nodes which points
-	// to the last rendered tree. By default this property is not present, which
-	// means that we are mounting a new tree for the first time.
-	let rootInternal = parentDom._children;
-	vnode = createElement(Fragment, { _parentDom: parentDom }, [vnode]);
-
-	if (rootInternal) {
-		patch(
-			parentDom,
-			vnode,
-			rootInternal,
-			{},
-			commitQueue,
-			parentDom.firstChild
-		);
-	} else {
-		// Store the VDOM tree root on the DOM element in a (minified) property:
-		rootInternal = parentDom._children = createInternal(vnode, null);
-
-		if (parentDom.ownerSVGElement !== UNDEFINED) {
-			rootInternal._flags |= MODE_SVG;
-		}
-
-		// Calling `render` on a container with existing DOM elements puts the diff into mutative hydrate mode:
-		if (parentDom.firstChild) {
-			rootInternal._flags |= MODE_MUTATIVE_HYDRATE;
-		}
-
-		mount(
-			parentDom,
-			vnode,
-			rootInternal,
-			{},
-			commitQueue,
-			// Start the diff at the replaceNode or the parentDOM.firstChild if any.
-			// Will be null if the parentDom is empty
-			parentDom.firstChild
-		);
+	let root = parentDom && parentDom._root;
+	if (!root) {
+		root = createRoot(parentDom);
 	}
-
-	// Flush all queued effects
-	commitRoot(commitQueue, rootInternal);
+	root.render(vnode);
+	parentDom._root = root;
 }
 
 /**
@@ -76,18 +34,10 @@ export function render(vnode, parentDom) {
  * update
  */
 export function hydrate(vnode, parentDom) {
-	if (options._root) options._root(vnode, parentDom);
-
-	vnode = createElement(Fragment, { _parentDom: parentDom }, [vnode]);
-	const rootInternal = createInternal(vnode);
-	rootInternal._flags |= MODE_HYDRATE;
-	parentDom._children = rootInternal;
-
-	if (parentDom.ownerSVGElement !== UNDEFINED) {
-		rootInternal._flags |= MODE_SVG;
+	let root = parentDom && parentDom._root;
+	if (!root) {
+		root = createRoot(parentDom);
 	}
-
-	const commitQueue = [];
-	mount(parentDom, vnode, rootInternal, {}, commitQueue, parentDom.firstChild);
-	commitRoot(commitQueue, rootInternal);
+	root.hydrate(vnode);
+	parentDom._root = root;
 }
