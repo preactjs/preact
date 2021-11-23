preactjs/preact

commit 09c30063cb18d0a1f6e2135f5c45381566dfa22b

Fix tests in IE11 (#3264)

karma.conf.js
@@ -4,6 +4,8 @@ var coverage = String(process.env.COVERAGE) === 'true',
 	minify = String(process.env.MINIFY) === 'true',
 	ci = String(process.env.CI).match(/^(1|true)$/gi),
 	sauceLabs = ci && String(process.env.RUN_SAUCE_LABS) === 'true',
+	// always downlevel to ES5 for saucelabs:
+	downlevel = sauceLabs || String(process.env.DOWNLEVEL) === 'true',
 	performance = !coverage && String(process.env.PERFORMANCE) !== 'false',
 	path = require('path'),
 	errorstacks = require('errorstacks'),
@@ -163,7 +165,7 @@ function createEsbuildPlugin() {
 								loose: true,
 								modules: false,
 								targets: {
-									browsers: ['last 2 versions', 'IE >= 9']
+									browsers: ['last 2 versions', 'IE >= 11']
 								}
 							}
 						]
@@ -178,7 +180,7 @@ function createEsbuildPlugin() {
 			});
 
 			// Apply babel pass whenever we load a .js file
-			build.onLoad({ filter: /\.js$/ }, async args => {
+			build.onLoad({ filter: /\.[mc]?js$/ }, async args => {
 				const contents = await fs.readFile(args.path, 'utf-8');
 
 				// Using a cache is crucial as babel is 30x slower than esbuild
@@ -202,6 +204,20 @@ function createEsbuildPlugin() {
 					const tmp = await babel.transformAsync(result, {
 						filename: args.path,
 						sourceMaps: 'inline',
+						presets: downlevel
+							? [
+									[
+										'@babel/preset-env',
+										{
+											loose: true,
+											modules: false,
+											targets: {
+												browsers: ['last 2 versions', 'IE >= 11']
+											}
+										}
+									]
+							  ]
+							: [],
 						plugins: [
 							coverage && [
 								'istanbul',
@@ -326,7 +342,7 @@ module.exports = function(config) {
 			singleBundle: false,
 
 			// esbuild options
-			target: 'es2015',
+			target: 'es5',
 			define: {
 				COVERAGE: coverage,
 				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || ''),

src/diff/index.js
@@ -433,7 +433,9 @@ function diffElementNodes(
 				// despite the attribute not being present. When the attribute
 				// is missing the progress bar is treated as indeterminate.
 				// To fix that we'll always update it when it is 0 for progress elements
-				(i !== dom.value || (nodeType === 'progress' && !i))
+				(i !== oldProps.value ||
+					i !== dom.value ||
+					(nodeType === 'progress' && !i))
 			) {
 				setProperty(dom, 'value', i, oldProps.value, false);
 			}

commit 44404f3fd48f32868ab59661d4d1a7314ce0cfe5

Fix es5 warnings in local test runs
This fixes an issue I introduced in #3264.

karma.conf.js
@@ -342,7 +342,7 @@ module.exports = function(config) {
 			singleBundle: false,
 
 			// esbuild options
-			target: 'es5',
+			target: downlevel ? 'es5' : 'es2015',
 			define: {
 				COVERAGE: coverage,
 				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || ''),