876a1d8ab4044a3795bf5a24b69ffaee5b1ec09d
Jason Miller <jason@developit.ca>
2021-12-09 17:20:21 -0500

    move tree root analysis back into render() to account for tests re-using createRoot() roots

--- src/create-root.js
+++ src/create-root.js
@@ -18,24 +18,16 @@ import { createInternal } from './tree';
 export function createRoot(parentDom) {
 	let rootInternal,
 		commitQueue,
-		firstChild =
-			/** @type {import('./internal').PreactElement} */ (parentDom.firstChild),
+		firstChild,
 		flags = 0;
-
-	// If the parent of this tree is within an inline SVG, the tree should start off in SVG mode:
-	if (parentDom.ownerSVGElement !== UNDEFINED) {
-		flags |= MODE_SVG;
-	}
-
-	// Calling createRoot().render() on an Element with existing children triggers mutative hydrate mode:
-	if (firstChild) {
-		flags |= MODE_MUTATIVE_HYDRATE;
-	}
 
 	function render(vnode) {
 		if (options._root) options._root(vnode, parentDom);
 
 		vnode = createElement(Fragment, { _parentDom: parentDom }, [vnode]);
+
+		firstChild =
+			/** @type {import('./internal').PreactElement} */ (parentDom.firstChild);
 
 		// List of effects that need to be called after diffing:
 		commitQueue = [];
@@ -44,9 +36,20 @@ export function createRoot(parentDom) {
 			patch(parentDom, vnode, rootInternal, commitQueue, firstChild);
 		} else {
 			rootInternal = createInternal(vnode);
+
 			// Store the VDOM tree root on the DOM element in a (minified) property:
 			parentDom._children = rootInternal;
+
+			// Calling createRoot().render() on an Element with existing children triggers mutative hydrate mode:
+			if (firstChild) {
+				flags = flags || MODE_MUTATIVE_HYDRATE;
+			}
+			// If the parent of this tree is within an inline SVG, the tree should start off in SVG mode:
+			if (parentDom.ownerSVGElement !== UNDEFINED) {
+				flags |= MODE_SVG;
+			}
 			rootInternal.flags |= flags;
+
 			rootInternal._context = {};
 
 			mount(parentDom, vnode, rootInternal, commitQueue, firstChild);
