72735319cbadffbe1fe5f57d88346f9b64e74356
Jason Miller <jason@developit.ca>
2022-01-03 22:55:23 -0500

    Fix demo

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,10 +2,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const preact = path.join(__dirname, '..', 'src');
-const compat = path.join(__dirname, '..', 'compat', 'src');
+const preact = path.join(__dirname, '..');
+const compat = path.join(__dirname, '..', 'compat');
 
 module.exports = {
+	mode: 'development',
 	context: __dirname,
 	entry: './index',
 	output: {
@@ -13,10 +14,8 @@ module.exports = {
 	},
 	resolve: {
 		alias: {
-			['preact/debug']: path.join(__dirname, '..', 'debug'),
-			['preact/devtools']: path.join(__dirname, '..', 'devtools'),
-			['preact/hooks']: path.join(__dirname, '..', 'hooks', 'src'),
 			preact: preact,
+			preact$: preact,
 			react: compat,
 			'react-dom': compat
 		},
@@ -25,12 +24,15 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.tsx?$/,
+				test: /\.([mc]?js|ts)x?$/,
 				loader: 'babel-loader',
 				options: {
 					sourceMap: true,
 					presets: [
-						[require.resolve('@babel/preset-typescript'), { jsxPragma: 'h' }],
+						[
+							require.resolve('@babel/preset-typescript'),
+							{ jsxPragma: 'createElement', jsxPragmaFrag: 'Fragment' }
+						],
 						[
 							require.resolve('@babel/preset-env'),
 							{
@@ -48,7 +50,7 @@ module.exports = {
 						[require.resolve('@babel/plugin-transform-react-jsx-source')],
 						[
 							require.resolve('@babel/plugin-transform-react-jsx'),
-							{ pragma: 'h', pragmaFrag: 'Fragment' }
+							{ pragma: 'createElement', pragmaFrag: 'Fragment' }
 						],
 						[
 							require.resolve('@babel/plugin-proposal-decorators'),
@@ -62,51 +64,18 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				options: {
-					sourceMap: true,
-					presets: [
-						[
-							require.resolve('@babel/preset-env'),
-							{
-								targets: {
-									browsers: ['last 2 versions', 'IE >= 9']
-								},
-								modules: false,
-								loose: true
-							}
-						],
-						[require.resolve('@babel/preset-react')]
-					],
-					plugins: [
-						[require.resolve('@babel/plugin-transform-react-jsx-source')],
-						[
-							require.resolve('@babel/plugin-transform-react-jsx'),
-							{ pragma: 'createElement', pragmaFrag: 'Fragment' }
-						],
-						[require.resolve('@babel/plugin-proposal-class-properties')],
-						[
-							require.resolve('@babel/plugin-transform-react-constant-elements')
-						],
-						[require.resolve('@babel/plugin-syntax-dynamic-import')]
-					]
-				}
-			},
-			{
 				test: /\.s?css$/,
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	},
-	devtool: 'inline-source-map',
-	node: {
-		process: 'mock',
-		Buffer: false,
-		setImmediate: false
-	},
+	// devtool: 'inline-source-map',
 	devServer: {
 		historyApiFallback: true
 	},
-	plugins: [new HtmlWebpackPlugin()]
+	plugins: [
+		new HtmlWebpackPlugin({
+			showErrors: false
+		})
+	]
 };
