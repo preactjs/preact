17da4efa736f14c84cd9f36fca4420d94f0dd403
Paul Guilbert <guilbert.paul@gmail.com>
2022-02-17 18:29:10 +0100

    Fix Incorrect translation of xlink:href attribute -> hhref #3441 (#3454)
    * Fix Incorrect translation of xlink:href attribute -> hhref #3441
    
    * Test the translation of svg xlink:href attribute. (xlink:href to href)

--- src/diff/props.js
+++ src/diff/props.js
@@ -106,7 +106,7 @@ export function setProperty(dom, name, value, oldValue, isSvg) {
 			// Normalize incorrect prop usage for SVG:
 			// - xlink:href / xlinkHref --> href (xlink:href was removed from SVG and isn't needed)
 			// - className --> class
-			name = name.replace(/xlink[H:h]/, 'h').replace(/sName$/, 's');
+			name = name.replace(/xlink(H|:h)/, 'h').replace(/sName$/, 's');
 		} else if (
 			name !== 'href' &&
 			name !== 'list' &&
