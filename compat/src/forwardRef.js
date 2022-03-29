f25d189e761c7aecefa38d5bd7e756310d4b40d7
Marvin Hagemeister <hello@marvinh.dev>
2022-03-29 22:03:47 +0200

    Drop mobx-react comment
    This hack is not necessary anymore to get `mobx-react` working.

--- compat/src/forwardRef.js
+++ compat/src/forwardRef.js
@@ -24,9 +24,6 @@ export const REACT_FORWARD_SYMBOL =
  * @returns {import('./internal').FunctionComponent}
  */
 export function forwardRef(fn) {
-	// We always have ref in props.ref, except for
-	// mobx-react. It will call this function directly
-	// and always pass ref as the second argument.
 	function Forwarded(props) {
 		let clone = assign({}, props);
 		delete clone.ref;
