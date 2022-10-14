43204def7bef557f354e23d807570e6cfb50c8ce
Jovi De Croock <decroockjovi@gmail.com>
2022-10-14 06:33:37 +0200

    fix setstate order (#3763)

--- src/diff/index.js
+++ src/diff/index.js
@@ -96,6 +96,7 @@ export function diff(
 			if (c._nextState == null) {
 				c._nextState = c.state;
 			}
+
 			if (newType.getDerivedStateFromProps != null) {
 				if (c._nextState == c.state) {
 					c._nextState = assign({}, c._nextState);
@@ -109,11 +110,6 @@ export function diff(
 
 			oldProps = c.props;
 			oldState = c.state;
-
-			for (tmp = 0; tmp < c._stateCallbacks.length; tmp++) {
-				c._renderCallbacks.push(c._stateCallbacks[tmp]);
-				c._stateCallbacks = [];
-			}
 
 			// Invoke pre-render lifecycle methods
 			if (isNew) {
@@ -156,6 +152,12 @@ export function diff(
 					newVNode._children.forEach(vnode => {
 						if (vnode) vnode._parent = newVNode;
 					});
+
+					for (let i = 0; i < c._stateCallbacks.length; i++) {
+						c._renderCallbacks.push(c._stateCallbacks[i]);
+					}
+					c._stateCallbacks = [];
+
 					if (c._renderCallbacks.length) {
 						commitQueue.push(c);
 					}
@@ -188,6 +190,11 @@ export function diff(
 				if (renderHook) renderHook(newVNode);
 
 				tmp = c.render(c.props, c.state, c.context);
+
+				for (let i = 0; i < c._stateCallbacks.length; i++) {
+					c._renderCallbacks.push(c._stateCallbacks[i]);
+				}
+				c._stateCallbacks = [];
 			} else {
 				do {
 					c._dirty = false;
@@ -524,7 +531,11 @@ export function unmount(vnode, parentVNode, skipRemove) {
 	if ((r = vnode._children)) {
 		for (let i = 0; i < r.length; i++) {
 			if (r[i]) {
-				unmount(r[i], parentVNode, skipRemove || typeof vnode.type !== 'function');
+				unmount(
+					r[i],
+					parentVNode,
+					skipRemove || typeof vnode.type !== 'function'
+				);
 			}
 		}
 	}
