72e5dbad57a376655f0554f386795631e3fed151
Andre Wiggins <andrewiggins@live.com>
2022-11-03 16:40:35 -0700

    remove unused variable

--- src/diff/index.js
+++ src/diff/index.js
@@ -36,9 +36,6 @@ export function diff(
 	let tmp,
 		newType = newVNode.type;
 
-    // #2545, access owner document from the parentDom instead of using the global
-    const doc = parentDom.ownerDocument;
-
 	// When passing through createElement it assigns the object
 	// constructor as undefined. This to prevent JSON-injection.
 	if (newVNode.constructor !== undefined) return null;
@@ -267,8 +264,8 @@ export function diff(
 				isSvg,
 				excessDomChildren,
 				commitQueue,
-                		isHydrating,
-                		parentDom.ownerDocument
+				isHydrating,
+				parentDom.ownerDocument
 			);
 		}
 
@@ -322,7 +319,7 @@ export function commitRoot(commitQueue, root) {
  * @param {Array<import('../internal').Component>} commitQueue List of components
  * which have callbacks to invoke in commitRoot
  * @param {boolean} isHydrating Whether or not we are in hydration
- * @param {Document} doc The owner document of the parentNode
+ * @param {Document} doc The owner document of the parentNode #2545
  * @returns {import('../internal').PreactElement}
  */
 function diffElementNodes(
@@ -333,8 +330,8 @@ function diffElementNodes(
 	isSvg,
 	excessDomChildren,
 	commitQueue,
-    	isHydrating,
-    	doc
+	isHydrating,
+	doc
 ) {
 	let oldProps = oldVNode.props;
 	let newProps = newVNode.props;
