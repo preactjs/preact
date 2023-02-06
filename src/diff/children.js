13ade350810f10efcf29818122372ef73317b099
Andre Wiggins <andrewiggins@live.com>
2023-02-06 13:43:31 -0800

    Remove old commented out patchChildren code

--- src/diff/children.js
+++ src/diff/children.js
@@ -40,9 +40,7 @@ export function patchChildren(parentInternal, children, parentDom) {
 	let internal = parentInternal._child;
 	/** @type {Internal} */
 	let prevInternal;
-	// let newTail;
 	let oldHead = internal;
-	// let skewedOldHead = oldHead;
 
 	// Step 1. Find matches and set up _prev pointers. Identity null placeholders
 	// by also walking the old internal children at the same time
@@ -128,10 +126,6 @@ export function patchChildren(parentInternal, children, parentDom) {
 		// no match, create a new Internal:
 		if (!matchedInternal) {
 			matchedInternal = createInternal(normalizedVNode, parentInternal);
-			// console.log('creating new', internal.type);
-		} else {
-			// console.log('updating', internal.type);
-			// patch(internal, vnode, parentDom);
 		}
 
 		matchedInternal._prev = prevInternal || parentInternal;
@@ -149,7 +143,6 @@ export function patchChildren(parentInternal, children, parentDom) {
 		const next = oldHead._next;
 		if (oldHead._prev == null) {
 			if (lastMatchedInternal) lastMatchedInternal._next = next;
-			// else parentInternal._child = next;
 			unmount(oldHead, oldHead, 0);
 		} else {
 			lastMatchedInternal = oldHead;
@@ -311,217 +304,6 @@ export function patchChildren(parentInternal, children, parentDom) {
 
 	parentInternal._child = firstChild;
 }
-/*
-export function patchChildren(internal, children, parentDom) {
-	// let oldChildren =
-	// 	(internal._children && internal._children.slice()) || EMPTY_ARR;
-
-	// let oldChildrenLength = oldChildren.length;
-	// let remainingOldChildren = oldChildrenLength;
-
-
-	let skew = 0;
-	let i;
-
-	/** @type {import('../internal').Internal} *\/
-	let childInternal;
-
-	/** @type {import('../internal').ComponentChild} *\/
-	let childVNode;
-
-	/** @type {import('../internal').Internal[]} *\/
-	const newChildren = [];
-
-	for (i = 0; i < children.length; i++) {
-		childVNode = normalizeToVNode(children[i]);
-
-		// Terser removes the `continue` here and wraps the loop body
-		// in a `if (childVNode) { ... } condition
-		if (childVNode == null) {
-			// newChildren[i] = null;
-			continue;
-		}
-
-		let skewedIndex = i + skew;
-
-		/// TODO: Reconsider if we should bring back the "not moving text nodes" logic?
-		let matchingIndex = findMatchingIndex(
-			childVNode,
-			oldChildren,
-			skewedIndex,
-			remainingOldChildren
-		);
-
-		if (matchingIndex === -1) {
-			childInternal = UNDEFINED;
-		} else {
-			childInternal = oldChildren[matchingIndex];
-			oldChildren[matchingIndex] = UNDEFINED;
-			remainingOldChildren--;
-		}
-
-		let mountingChild = childInternal == null;
-
-		if (mountingChild) {
-			childInternal = createInternal(childVNode, internal);
-
-			// We are mounting a new VNode
-			mount(
-				childInternal,
-				childVNode,
-				parentDom,
-				getDomSibling(internal, skewedIndex)
-			);
-		}
-		// If this node suspended during hydration, and no other flags are set:
-		// @TODO: might be better to explicitly check for MODE_ERRORED here.
-		else if (
-			(childInternal.flags & (MODE_HYDRATE | MODE_SUSPENDED)) ===
-			(MODE_HYDRATE | MODE_SUSPENDED)
-		) {
-			// We are resuming the hydration of a VNode
-			mount(childInternal, childVNode, parentDom, childInternal.data);
-		} else {
-			// Morph the old element into the new one, but don't append it to the dom yet
-			patch(childInternal, childVNode, parentDom);
-		}
-
-		go: if (mountingChild) {
-			if (matchingIndex == -1) {
-				skew--;
-			}
-
-			// Perform insert of new dom
-			if (childInternal.flags & TYPE_DOM) {
-				parentDom.insertBefore(
-					childInternal.data,
-					getDomSibling(internal, skewedIndex)
-				);
-			}
-		} else if (matchingIndex !== skewedIndex) {
-			// Move this DOM into its correct place
-			if (matchingIndex === skewedIndex + 1) {
-				skew++;
-				break go;
-			} else if (matchingIndex > skewedIndex) {
-				if (remainingOldChildren > children.length - skewedIndex) {
-					skew += matchingIndex - skewedIndex;
-					break go;
-				} else {
-					// ### Change from keyed: I think this was missing from the algo...
-					skew--;
-				}
-			} else if (matchingIndex < skewedIndex) {
-				if (matchingIndex == skewedIndex - 1) {
-					skew = matchingIndex - skewedIndex;
-				} else {
-					skew = 0;
-				}
-			} else {
-				skew = 0;
-			}
-
-			skewedIndex = i + skew;
-
-			if (matchingIndex == i) break go;
-
-			let nextSibling = getDomSibling(internal, skewedIndex + 1);
-			if (childInternal.flags & TYPE_DOM) {
-				parentDom.insertBefore(childInternal.data, nextSibling);
-			} else {
-				insertComponentDom(childInternal, nextSibling, parentDom);
-			}
-		}
-
-		newChildren[i] = childInternal;
-	}
-
-	internal._children = newChildren;
-
-	// Remove remaining oldChildren if there are any.
-	if (remainingOldChildren > 0) {
-		for (i = oldChildrenLength; i--; ) {
-			if (oldChildren[i] != null) {
-				unmount(oldChildren[i], oldChildren[i]);
-			}
-		}
-	}
-
-	// Set refs only after unmount
-	for (i = 0; i < newChildren.length; i++) {
-		childInternal = newChildren[i];
-		if (childInternal) {
-			let oldRef = childInternal._prevRef;
-			if (childInternal.ref != oldRef) {
-				if (oldRef) applyRef(oldRef, null, childInternal);
-				if (childInternal.ref)
-					applyRef(
-						childInternal.ref,
-						childInternal._component || childInternal.data,
-						childInternal
-					);
-			}
-		}
-	}
-}
-*/
-
-/**
- * @param {import('../internal').VNode | string} childVNode
- * @param {import('../internal').Internal[]} oldChildren
- * @param {number} skewedIndex
- * @param {number} remainingOldChildren
- * @returns {number}
- */
-/*
-function findMatchingIndex(
-	childVNode,
-	oldChildren,
-	skewedIndex,
-	remainingOldChildren
-) {
-	const type = typeof childVNode == 'string' ? null : childVNode.type;
-	const key = type !== null ? childVNode.key : UNDEFINED;
-	let match = -1;
-	let x = skewedIndex - 1; // i - 1;
-	let y = skewedIndex + 1; // i + 1;
-	let oldChild = oldChildren[skewedIndex]; // i
-
-	if (
-		// ### Change from keyed: support for matching null placeholders
-		oldChild === null ||
-		(oldChild != null && oldChild.type === type && oldChild.key == key)
-	) {
-		match = skewedIndex; // i
-	}
-	// If there are any unused children left (ignoring an available in-place child which we just checked)
-	else if (remainingOldChildren > (oldChild != null ? 1 : 0)) {
-		// eslint-disable-next-line no-constant-condition
-		while (true) {
-			if (x >= 0) {
-				oldChild = oldChildren[x];
-				if (oldChild != null && oldChild.type === type && oldChild.key == key) {
-					match = x;
-					break;
-				}
-				x--;
-			}
-			if (y < oldChildren.length) {
-				oldChild = oldChildren[y];
-				if (oldChild != null && oldChild.type === type && oldChild.key == key) {
-					match = y;
-					break;
-				}
-				y++;
-			} else if (x < 0) {
-				break;
-			}
-		}
-	}
-
-	return match;
-}
-*/
 
 /**
  * @param {import('../internal').Internal} internal
