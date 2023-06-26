900e060fb58a13673b75f15fd17512f4f2d1e1f1
Cagdas Ucar <cagdas.ucar@webdigital.com>
2023-06-26 10:13:50 -0700

    temporary disable regular async rendering but keep initial async rendering - bug when state is frequently updated - they get stuck for some reason

--- async/src/index.js
+++ async/src/index.js
@@ -371,11 +371,12 @@ const initializeAsyncRendering = () => {
 		['diff']
 	);
 
-	// async implementation of processing render queue
-	// note that we are awaiting for each component render here
-	// if we had not, this would be "concurrent rendering" but that
-	// does not work at all - double rendering issues - not a good idea
-	// __v is mangled property of _vnode, __b => _depth, __d => _dirty
+	/*************************************************************************************************************************************
+	 * below is async implementation of processing render queue. disabled for now, limiting async rendering to initial render only.
+	 * we have seen some bugs in cases of frequent re-rendering, specifically in comparison control, when we update state quickly based on mouse events.
+	 * we may want to re-visit this later to fix it properly. note that we are waiting for each component render here. otherwise it would be
+	 * "concurrent rendering" but that does not work at all - double rendering issues - not a good idea.
+	 * __v is mangled property of _vnode, __b => _depth, __d => _dirty
 	window._preactAsync.processRenderQueue = new AsyncFunction(`
 		let queue;
 		while ((window._preactAsync.queueCount = window._preactAsync.queue.length)) {
@@ -393,6 +394,7 @@ const initializeAsyncRendering = () => {
 		}
 	`);
 	options.processRenderQueue = () => window._preactAsync.processRenderQueue(); // wrap it in a regular function so that caller will not wait for promise
+	 **************************************************************************************************************************************/
 };
 
 /**
