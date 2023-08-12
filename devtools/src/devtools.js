c52fa99c9b902ee1a66eaff6f188b11e2a6fb6ff
jdecroock <decroockjovi@gmail.com>
2023-08-12 09:37:21 +0200

    10.17.0

--- devtools/src/devtools.js
+++ devtools/src/devtools.js
@@ -2,7 +2,7 @@ import { options, Fragment, Component } from 'preact';
 
 export function initDevTools() {
 	if (typeof window != 'undefined' && window.__PREACT_DEVTOOLS__) {
-		window.__PREACT_DEVTOOLS__.attachPreact('10.16.0', options, {
+		window.__PREACT_DEVTOOLS__.attachPreact('10.17.0', options, {
 			Fragment,
 			Component
 		});
