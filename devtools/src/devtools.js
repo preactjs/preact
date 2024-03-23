bc7c5515599634a427334eb03236ddf19e13e618
Jovi De Croock <decroockjovi@gmail.com>
2024-03-23 07:46:25 +0100

    10.20.1 (#4319)

--- devtools/src/devtools.js
+++ devtools/src/devtools.js
@@ -2,7 +2,7 @@ import { options, Fragment, Component } from 'preact';
 
 export function initDevTools() {
 	if (typeof window != 'undefined' && window.__PREACT_DEVTOOLS__) {
-		window.__PREACT_DEVTOOLS__.attachPreact('10.20.0', options, {
+		window.__PREACT_DEVTOOLS__.attachPreact('10.20.1', options, {
 			Fragment,
 			Component
 		});
