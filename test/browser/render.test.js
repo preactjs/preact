d501beafa0ebc22f447b1ffdb6bf4d6cb7eed2af
Jovi De Croock <decroockjovi@gmail.com>
2024-04-18 16:11:23 +0200

    Update test/browser/render.test.js

--- test/browser/render.test.js
+++ test/browser/render.test.js
@@ -1,5 +1,5 @@
 import { setupRerender } from 'preact/test-utils';
-import { createElement, render, Component, options, hydrate } from 'preact';
+import { createElement, render, Component, options } from 'preact';
 import {
 	setupScratch,
 	teardown,
