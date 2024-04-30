9c4c61be1ec53ede4a22db123d28764f3f7cad9b
Ryan Christian <rchristian@ryanchristian.dev>
2024-04-30 08:05:33 -0500

    test: Remove perhaps superfluous mathml tests for the moment

--- test/browser/mathml.test.js
+++ test/browser/mathml.test.js
@@ -49,50 +49,6 @@ describe('mathml', () => {
 		expect(namespace).to.equal('http://www.w3.org/1998/Math/MathML');
 	});
 
-	it('should use attributes for className', () => {
-		const Demo = ({ c }) => (
-			<math {...(c ? { class: 'foo_' + c } : {})}>
-				<msup>
-					<mi>c</mi>
-					<mn>2</mn>
-				</msup>
-			</math>
-		);
-		render(<Demo c="1" />, scratch);
-		let root = scratch.firstChild;
-		sinon.spy(root, 'removeAttribute');
-		render(<Demo />, scratch);
-		expect(root.removeAttribute).to.have.been.calledOnce.and.calledWith(
-			'class'
-		);
-
-		root.removeAttribute.restore();
-
-		render(<div />, scratch);
-		render(<Demo />, scratch);
-		root = scratch.firstChild;
-		sinon.spy(root, 'setAttribute');
-		render(<Demo c="2" />, scratch);
-		expect(root.setAttribute).to.have.been.calledOnce.and.calledWith(
-			'class',
-			'foo_2'
-		);
-
-		root.setAttribute.restore();
-	});
-
-	it('should support class attribute', () => {
-		render(<math class="foo bar" />, scratch);
-
-		expect(scratch.innerHTML).to.contain(` class="foo bar"`);
-	});
-
-	it('should support className attribute', () => {
-		render(<math className="foo bar" />, scratch);
-
-		expect(scratch.innerHTML).to.contain(` class="foo bar"`);
-	});
-
 	it('should transition from DOM to MathML and back', () => {
 		render(
 			<div>
