b0550f541151447e1ab2b525cd35ecd1df2522f8
jdecroock <decroockjovi@gmail.com>
2024-11-10 13:35:09 +0100

    Leverage Object.assign

--- src/util.js
+++ src/util.js
@@ -1,19 +1,8 @@
 import { EMPTY_ARR } from './constants';
 
 export const isArray = Array.isArray;
-
-/**
- * Assign properties from `props` to `obj`
- * @template O, P The obj and props types
- * @param {O} obj The object to copy properties to
- * @param {P} props The object to copy properties from
- * @returns {O & P}
- */
-export function assign(obj, props) {
-	// @ts-expect-error We change the type of `obj` to be `O & P`
-	for (let i in props) obj[i] = props[i];
-	return /** @type {O & P} */ (obj);
-}
+export const assign = Object.assign;
+export const slice = EMPTY_ARR.slice;
 
 /**
  * Remove a child node from its parent if attached. This is a workaround for
@@ -24,5 +13,3 @@ export function assign(obj, props) {
 export function removeNode(node) {
 	if (node && node.parentNode) node.parentNode.removeChild(node);
 }
-
-export const slice = EMPTY_ARR.slice;
