b8c2dded67640bb601130dfdfb92e877798faf27
jdecroock <decroockjovi@gmail.com>
2024-11-10 13:54:14 +0100

    Remove component.base

--- src/diff/index.js
+++ src/diff/index.js
@@ -278,8 +278,6 @@ export function diff(
 				refQueue
 			);
 
-			c.base = newVNode._dom;
-
 			// We successfully rendered this VNode, unset any stored hydration/bailout state:
 			newVNode._flags &= RESET_MODE;
 
@@ -633,7 +631,7 @@ export function unmount(vnode, parentVNode, skipRemove) {
 			}
 		}
 
-		r.base = r._parentDom = NULL;
+		r._parentDom = NULL;
 	}
 
 	if ((r = vnode._children)) {
