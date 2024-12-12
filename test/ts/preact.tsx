32ebe36589bec8978cd5db26c5fce30d0c61bdad
Ryan Christian <rchristian@ryanchristian.dev>
2024-12-12 05:58:37 -0600

    test: Add simple test case for `h` & `createElement`

--- test/ts/preact.tsx
+++ test/ts/preact.tsx
@@ -383,6 +383,10 @@ h('form', { onSubmit: onSubmit });
 h('option', { value: 'foo' });
 createElement('option', { value: 'foo' });
 
+// Should default to correct event target element for the attribute interface
+h<JSX.InputHTMLAttributes>('input', { onClick: e => e.currentTarget.capture });
+createElement<JSX.InputHTMLAttributes>('input', { onClick: e => e.currentTarget.capture });
+
 function Checkbox({ onChange }: JSX.HTMLAttributes<HTMLInputElement>) {
 	function handleChange(
 		this: void,
