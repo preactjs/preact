da6a55b6de1522242b989a000dda39fe8b2ae869
Ryan Christian <rchristian@ryanchristian.dev>
2024-12-16 23:03:56 -0600

    revert: Bring back input type attribute

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -2050,6 +2050,30 @@ export namespace JSXInternal {
 	> &
 		ImgAriaRoles;
 
+	type HTMLInputTypeAttribute =
+		| 'button'
+		| 'checkbox'
+		| 'color'
+		| 'date'
+		| 'datetime-local'
+		| 'email'
+		| 'file'
+		| 'hidden'
+		| 'image'
+		| 'month'
+		| 'number'
+		| 'password'
+		| 'radio'
+		| 'range'
+		| 'reset'
+		| 'search'
+		| 'submit'
+		| 'tel'
+		| 'text'
+		| 'time'
+		| 'url'
+		| 'week';
+
 	interface PartialInputHTMLAttributes<T extends EventTarget>
 		extends HTMLAttributes<T> {
 		accept?: Signalish<string | undefined>;
