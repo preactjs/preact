98308a172276dfc3bed3ecd031390d954bc9515c
Ryan Christian <rchristian@ryanchristian.dev>
2024-12-19 13:06:19 -0600

    fix: Incorrect onToggle event type

--- test/ts/preact.tsx
+++ test/ts/preact.tsx
@@ -351,6 +351,10 @@ const acceptsStringAsLength = <div style={{ marginTop: '20px' }} />;
 
 const ReturnNull: FunctionalComponent = () => null;
 
+// Should accept arbitrary properties outside of JSX.HTMLAttributes
+h('option', { x: 'foo' });
+createElement('option', { value: 'foo' });
+
 // Refs should work on elements
 const ref = createRef<HTMLDivElement>();
 createElement('div', { ref: ref }, 'hi');
@@ -375,13 +379,17 @@ const onBeforeInput = (e: h.JSX.TargetedInputEvent<HTMLInputElement>) => {};
 createElement('input', { onBeforeInput: onBeforeInput });
 h('input', { onBeforeInput: onBeforeInput });
 
+// Should accept onSubmit
 const onSubmit = (e: h.JSX.TargetedSubmitEvent<HTMLFormElement>) => {};
 <form onSubmit={e => e.currentTarget.elements} />;
 createElement('form', { onSubmit: onSubmit });
 h('form', { onSubmit: onSubmit });
 
-h('option', { value: 'foo' });
-createElement('option', { value: 'foo' });
+// Should accept onToggle
+const onToggle = (e: h.JSX.TargetedToggleEvent<HTMLDetailsElement>) => {};
+<dialog onToggle={(e) => ({ newState: e.newState, oldState: e.oldState }) } />;
+createElement('dialog', { onToggle: onToggle });
+h('dialog', { onToggle: onToggle });
 
 // Should default to correct event target element for the attribute interface
 h<JSX.InputHTMLAttributes>('input', { onClick: e => e.currentTarget.capture });
