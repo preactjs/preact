055cc5b8c62326fbb0fcaccb9816504e82f121b8
Jovi De Croock <decroockjovi@gmail.com>
2024-12-28 09:25:17 +0100

    10.25.4 (#4620)

--- devtools/src/devtools.js
+++ devtools/src/devtools.js
@@ -13,7 +13,7 @@ export function initDevTools() {
 		globalVar !== undefined &&
 		globalVar.__PREACT_DEVTOOLS__
 	) {
-		globalVar.__PREACT_DEVTOOLS__.attachPreact('10.25.3', options, {
+		globalVar.__PREACT_DEVTOOLS__.attachPreact('10.25.4', options, {
 			Fragment,
 			Component
 		});
