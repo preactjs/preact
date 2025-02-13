58ad12275e4480e9c6f253dd5f427bf6fc7e143b
jdecroock <decroockjovi@gmail.com>
2025-02-13 18:58:03 +0100

    Save bytes

--- src/util.js
+++ src/util.js
@@ -2,19 +2,7 @@ import { EMPTY_ARR } from './constants';
 
 export const isArray = Array.isArray;
 export const slice = EMPTY_ARR.slice;
-
-/**
- * Assign properties from `props` to `obj`
- * @template O, P The obj and props types
- * @param {O} obj The object to copy properties to
- * @param {P} props The object to copy properties from
- * @returns {O & P}
- */
-export function assign(obj, props) {
-	// @ts-expect-error We change the type of `obj` to be `O & P`
-	for (let i in props) obj[i] = props[i];
-	return /** @type {O & P} */ (obj);
-}
+export const assign = Object.assign;
 
 /**
  * Remove a child node from its parent if attached.
