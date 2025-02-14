757823119f94318ab1d7d31e08ecc936ac1feafa
jdecroock <decroockjovi@gmail.com>
2025-02-14 09:31:40 +0100

    Remove automatic px suffix

--- jsx-runtime/src/utils.js
+++ jsx-runtime/src/utils.js
@@ -1,4 +1,6 @@
 const ENCODED_ENTITIES = /["&<]/;
+export const IS_NON_DIMENSIONAL =
+	/acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
 
 /** @param {string} str */
 export function encodeEntities(str) {
