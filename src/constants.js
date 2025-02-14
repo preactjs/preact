3314bce460a20b773847fbfea0ec30e9991113e7
jdecroock <decroockjovi@gmail.com>
2025-02-14 09:57:42 +0100

    Remove constant

--- src/constants.js
+++ src/constants.js
@@ -18,5 +18,3 @@ export const NULL = null;
 export const UNDEFINED = undefined;
 export const EMPTY_OBJ = /** @type {any} */ ({});
 export const EMPTY_ARR = [];
-export const IS_NON_DIMENSIONAL =
-	/acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
