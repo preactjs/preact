6b8bfa2f0a5c4580a9966ff5f66d7403d9ca1145
jdecroock <decroockjovi@gmail.com>
2025-02-14 09:57:42 +0100

    Remove constant

--- src/constants.js
+++ src/constants.js
@@ -17,5 +17,3 @@ export const MATH_NAMESPACE = 'http://www.w3.org/1998/Math/MathML';
 export const UNDEFINED = undefined;
 export const EMPTY_OBJ = /** @type {any} */ ({});
 export const EMPTY_ARR = [];
-export const IS_NON_DIMENSIONAL =
-	/acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
