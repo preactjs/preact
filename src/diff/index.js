77190903c58560010d8a640f746ba5b010ecb8c1
Jovi De Croock <decroockjovi@gmail.com>
2025-02-14 16:14:56 +0100

    Remove static dom bail (#4670)

--- src/diff/index.js
+++ src/diff/index.js
@@ -353,12 +353,6 @@ export function diff(
 			}
 			options._catchError(e, newVNode, oldVNode);
 		}
-	} else if (
-		excessDomChildren == NULL &&
-		newVNode._original == oldVNode._original
-	) {
-		newVNode._children = oldVNode._children;
-		newVNode._dom = oldVNode._dom;
 	} else {
 		oldDom = newVNode._dom = diffElementNodes(
 			oldVNode._dom,
