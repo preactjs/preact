14b4026bbcaffebd04e45b288dbb8e5e5aa6b198
jdecroock <decroockjovi@gmail.com>
2025-02-14 09:59:01 +0100

    Revert "Remove constant"
    This reverts commit 6b8bfa2f0a5c4580a9966ff5f66d7403d9ca1145.

--- src/constants.js
+++ src/constants.js
@@ -18,3 +18,5 @@ export const NULL = null;
 export const UNDEFINED = undefined;
 export const EMPTY_OBJ = /** @type {any} */ ({});
 export const EMPTY_ARR = [];
+export const IS_NON_DIMENSIONAL =
+	/acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
