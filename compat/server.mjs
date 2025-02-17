6c8c05ef1800eed80818b93899ecdbad18e9342d
jdecroock <decroockjovi@gmail.com>
2025-02-17 14:26:34 +0100

    Add renderToReadableStream export

--- compat/server.mjs
+++ compat/server.mjs
@@ -1,5 +1,6 @@
 import { renderToString } from 'preact-render-to-string';
 import { renderToPipeableStream } from 'preact-render-to-string/stream-node';
+import { renderToReadableStream } from 'preact-render-to-string/stream';
 
 export {
 	renderToString,
@@ -7,9 +8,10 @@ export {
 } from 'preact-render-to-string';
 
 export { renderToPipeableStream } from 'preact-render-to-string/stream-node';
-
+export { renderToReadableStream } from 'preact-render-to-string/stream';
 export default {
 	renderToString,
 	renderToStaticMarkup: renderToString,
-	renderToPipeableStream
+	renderToPipeableStream,
+	renderToReadableStream
 };
