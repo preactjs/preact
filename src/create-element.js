094c33ba025f4d330b6356715385c64d7095596a
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2025-03-08 19:25:12 -0600

    refactor: Skip dom check for inferring lower-cased event names (#4720)

--- src/create-element.js
+++ src/create-element.js
@@ -29,19 +29,6 @@ export function createElement(type, props, children) {
 			arguments.length > 3 ? slice.call(arguments, 2) : children;
 	}
 
-<<<<<<< HEAD
-	// If a Component VNode, check for and apply defaultProps
-	// Note: type may be undefined in development, must never error here.
-	if (typeof type == 'function' && type.defaultProps != NULL) {
-		for (i in type.defaultProps) {
-			if (normalizedProps[i] === UNDEFINED) {
-				normalizedProps[i] = type.defaultProps[i];
-			}
-		}
-	}
-
-=======
->>>>>>> 04457d6e (Move `defaultProps` into `preact/compat` (#4657))
 	return createVNode(type, normalizedProps, key, ref, NULL);
 }
 
--- src/diff/props.js
+++ src/diff/props.js
@@ -65,11 +65,9 @@ export function setProperty(dom, name, value, oldValue, namespace) {
 	// Benchmark for comparison: https://esbench.com/bench/574c954bdb965b9a00965ac6
 	else if (name[0] == 'o' && name[1] == 'n') {
 		useCapture = name != (name = name.replace(CAPTURE_REGEX, '$1'));
-		const lowerCaseName = name.toLowerCase();
 
 		// Infer correct casing for DOM built-in events:
-		if (lowerCaseName in dom || name == 'onFocusOut' || name == 'onFocusIn')
-			name = lowerCaseName.slice(2);
+		if (name[2].toLowerCase() != name[2]) name = name.toLowerCase().slice(2);
 		else name = name.slice(2);
 
 		if (!dom._listeners) dom._listeners = {};
