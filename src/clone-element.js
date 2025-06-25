37d531f55078bb5b37837baf3561dee7947a1f8f
James Garbutt <43081j@users.noreply.github.com>
2025-06-25 13:02:28 +0100

    perf: use createElement when cloning
    To avoid duplicating logic, we can use `createElement` with
    pre-populated props.

--- src/clone-element.js
+++ src/clone-element.js
@@ -1,38 +1,24 @@
-import { assign, slice } from './util';
-import { createVNode } from './create-element';
-import { NULL } from './constants';
+import { assign } from './util';
+import { createElement } from './create-element';
 
 /**
  * Clones the given VNode, optionally adding attributes/props and replacing its
  * children.
  * @param {import('./internal').VNode} vnode The virtual DOM element to clone
  * @param {object} props Attributes/props to add when cloning
- * @param {Array<import('./internal').ComponentChildren>} rest Any additional arguments will be used
+ * @param {...Array<import('./internal').ComponentChildren>} rest Any additional arguments will be used
  * as replacement children.
  * @returns {import('./internal').VNode}
  */
-export function cloneElement(vnode, props, children) {
-	let normalizedProps = assign({}, vnode.props),
-		key,
-		ref,
-		i;
-
-	for (i in props) {
-		if (i == 'key') key = props[i];
-		else if (i == 'ref' && typeof vnode.type != 'function') ref = props[i];
-		else normalizedProps[i] = props[i];
-	}
-
-	if (arguments.length > 2) {
-		normalizedProps.children =
-			arguments.length > 3 ? slice.call(arguments, 2) : children;
-	}
-
-	return createVNode(
+export function cloneElement(vnode, props, ...rest) {
+	return createElement(
 		vnode.type,
-		normalizedProps,
-		key || vnode.key,
-		ref || vnode.ref,
-		NULL
+		assign(
+			vnode.ref ? { ref: vnode.ref } : {},
+			vnode.key ? { key: vnode.key } : {},
+			vnode.props,
+			props
+		),
+		...rest
 	);
 }
