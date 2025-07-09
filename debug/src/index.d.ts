99fe7205fd5bc546f83e57c70c822ca50be04f31
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-07-09 10:07:06 +0800

    fix: types

--- debug/src/index.d.ts
+++ debug/src/index.d.ts
@@ -1,3 +1,22 @@
+import { VNode } from 'preact';
+
+/**
+ * Get the currently rendered `vnode`
+ */
+export function getCurrentVNode(): VNode | null;
+
+/**
+ * Return the component stack that was captured up to this point.
+ */
+export function getOwnerStack(vnode: VNode): string;
+
+/**
+ * Setup code to capture the component trace while rendering. Note that
+ * we cannot simply traverse `vnode._parent` upwards, because we have some
+ * debug messages for `this.setState` where the `vnode` is `undefined`.
+ */
+export function setupComponentStack(): void;
+
 /**
  * Reset the history of which prop type warnings have been logged.
  */
