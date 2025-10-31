7a5570d735003118801455be404ccf9009273e1e
Ali Garajian <50337133+ali-garajian@users.noreply.github.com>
2025-10-31 20:01:04 +0330

    refactor(children): remove redundant assignment to childVNode (#4951)
    * refactor(children): remove redundant assignment to childVNode
    
    * refactor(children): use key instead of childVNode.key
    
    ---------
    
    Co-authored-by: Ali Garajian <ali.garajian@snapppay.ir>

--- src/diff/children.js
+++ src/diff/children.js
@@ -218,7 +218,7 @@ function constructNewChildrenArray(
 				childVNode._original
 			);
 		} else {
-			childVNode = newParentVNode._children[i] = childVNode;
+			newParentVNode._children[i] = childVNode;
 		}
 
 		const skewedIndex = i + skew;
@@ -426,7 +426,7 @@ function findMatchingIndex(
 		remainingOldChildren > (matched ? 1 : 0);
 
 	if (
-		(oldVNode === NULL && childVNode.key == null) ||
+		(oldVNode === NULL && key == null) ||
 		(matched && key == oldVNode.key && type == oldVNode.type)
 	) {
 		return skewedIndex;
