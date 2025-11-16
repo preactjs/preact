d3b72c9bc96d07f08a9d12165e23b8bdf28c25c3
Ali Garajian <50337133+ali-garajian@users.noreply.github.com>
2025-11-16 23:08:04 +0330

    refactor(diff): set oldProps default value on declaration (#4959)

--- src/diff/index.js
+++ src/diff/index.js
@@ -415,7 +415,7 @@ function diffElementNodes(
 	isHydrating,
 	refQueue
 ) {
-	let oldProps = oldVNode.props;
+	let oldProps = oldVNode.props || EMPTY_OBJ;
 	let newProps = newVNode.props;
 	let nodeType = /** @type {string} */ (newVNode.type);
 	/** @type {any} */
@@ -484,8 +484,6 @@ function diffElementNodes(
 	} else {
 		// If excessDomChildren was not null, repopulate it with the current element's children:
 		excessDomChildren = excessDomChildren && slice.call(dom.childNodes);
-
-		oldProps = oldVNode.props || EMPTY_OBJ;
 
 		// If we are in a situation where we are not hydrating but are using
 		// existing DOM (e.g. replaceNode) we should read the existing DOM
