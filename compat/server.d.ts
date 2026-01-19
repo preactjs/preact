011aaef86c6c31decfb8435574a76510224b7871
Ryan Christian <rchristian@ryanchristian.dev>
2026-01-19 14:55:39 -0600

    chore: Fix RTS

--- compat/server.d.ts
+++ compat/server.d.ts
@@ -1,10 +1,3 @@
-// @ts-nocheck TS loses its mind over the mixed module systems here.
-// It's not ideal, but works at runtime and we're not shipping mixed type definitions.
-
-import { renderToString } from 'preact-render-to-string';
-import { renderToPipeableStream } from 'preact-render-to-string/stream-node';
-import { renderToReadableStream } from 'preact-render-to-string/stream';
-
 export {
 	renderToString,
 	renderToString as renderToStaticMarkup
@@ -12,9 +5,3 @@ export {
 
 export { renderToPipeableStream } from 'preact-render-to-string/stream-node';
 export { renderToReadableStream } from 'preact-render-to-string/stream';
-export = {
-	renderToString: typeof renderToString,
-	renderToStaticMarkup: typeof renderToString,
-	renderToPipeableStream: typeof renderToPipeableStream,
-	renderToReadableStream: typeof renderToReadableStream
-};
