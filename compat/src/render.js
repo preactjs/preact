5aa892a42d1686a45c34b599ce0bfc20df52fde5
Jovi De Croock <decroockjovi@gmail.com>
2026-08-05 17:59:49 +0200

    perf(compat): reuse synthetic event helper functions

--- compat/src/render.js
+++ compat/src/render.js
@@ -147,16 +147,19 @@ export function hydrate(vnode, parent, callback) {
 }
 
 let oldEventHook = options.event;
+const persist = () => {};
+function isPropagationStopped() {
+	return this.cancelBubble;
+}
+function isDefaultPrevented() {
+	return this.defaultPrevented;
+}
 options.event = e => {
 	if (oldEventHook) e = oldEventHook(e);
 
-	e.persist = () => {};
-	e.isPropagationStopped = function isPropagationStopped() {
-		return this.cancelBubble;
-	};
-	e.isDefaultPrevented = function isDefaultPrevented() {
-		return this.defaultPrevented;
-	};
+	e.persist = persist;
+	e.isPropagationStopped = isPropagationStopped;
+	e.isDefaultPrevented = isDefaultPrevented;
 	return (e.nativeEvent = e);
 };
 
