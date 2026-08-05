0e388585ada19efbddb67c28239c4e05d933141b
Jovi De Croock <decroockjovi@gmail.com>
2026-08-05 17:58:26 +0200

    perf: avoid slice for variadic vnode children

--- src/create-element.js
+++ src/create-element.js
@@ -1,4 +1,4 @@
-import { assign, slice } from './util';
+import { assign } from './util';
 import options from './options';
 import { NULL, UNDEFINED } from './constants';
 
@@ -26,7 +26,14 @@ export function createElement(type, props, children) {
 	}
 
 	if (length > 2) {
-		normalizedProps.children = length > 3 ? slice.call(arguments, 2) : children;
+		if (length > 3) {
+			normalizedProps.children = new Array(length - 2);
+			for (i = 2; i < length; i++) {
+				normalizedProps.children[i - 2] = arguments[i];
+			}
+		} else {
+			normalizedProps.children = children;
+		}
 	}
 
 	return createVNode(type, normalizedProps, key, ref, NULL);
@@ -55,7 +62,14 @@ export function cloneElement(vnode, props, children) {
 	}
 
 	if (length > 2) {
-		normalizedProps.children = length > 3 ? slice.call(arguments, 2) : children;
+		if (length > 3) {
+			normalizedProps.children = new Array(length - 2);
+			for (i = 2; i < length; i++) {
+				normalizedProps.children[i - 2] = arguments[i];
+			}
+		} else {
+			normalizedProps.children = children;
+		}
 	}
 
 	return createVNode(
